81b536409e6809a15a4194d13fbe761dceb6e4a4
Eli Barzilay <elbarzil@microsoft.com>
2017-06-09 03:14:08 -0400

    Minor script cleanup
    No need for `LD_STRING`, and remove some unnecessary boilerplate in
    `update_livy.py`.

--- tools/hdi/update_livy.py
+++ tools/hdi/update_livy.py
@@ -2,24 +2,18 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in project root for information.
 
-import sys
-import json
+import sys, json
 
-def main():
-    if len(sys.argv) != 4:
-        raise Exception(("Not enough" if len(sys.argv)<4 else "Too many") + " arguments.")
-    [_, config_file, maven_pkg, ld_lib_path] = sys.argv
-    with open(config_file) as conf_file:
-        conf=json.load(conf_file)
-    conf["session_configs"]["conf"]    = {}
-    conf["session_configs"]["conf"]["spark.jars.packages"] = maven_pkg
-    with open(config_file, "w") as outfile:
-        json.dump(conf, outfile, indent=2, sort_keys=True)
+if len(sys.argv) != 3:
+    raise Exception(("Not enough" if len(sys.argv) < 3 else "Too many")
+                    + " arguments.")
 
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as exn:
-        for line in str(exn).split("\n"):
-            print "[ERROR] {0}".format(line)
-        sys.exit(1)
+_, config_file, maven_pkg = sys.argv
+with open(config_file) as conf_file:
+    conf=json.load(conf_file)
+
+conf["session_configs"]["conf"] = {}
+conf["session_configs"]["conf"]["spark.jars.packages"] = maven_pkg
+
+with open(config_file, "w") as outfile:
+    json.dump(conf, outfile, indent=2, sort_keys=True)
