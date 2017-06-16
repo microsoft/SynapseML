61c37c9afa1cde50045aa61e2fd28571d4828133
Eli Barzilay <eli@barzilay.org>
2017-06-16 09:07:39 -0400

    More cosmetic improvements

--- tools/deployment/bashshelldeploy.sh
+++ tools/deployment/bashshelldeploy.sh
@@ -6,7 +6,12 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation> -t <templateFilePath> -p <parametersFilePath>" 1>&2; exit 1; }
+usage() {
+  echo "Usage: $(basename "$0") -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation> -t <templateFilePath> -p <parametersFilePath>"
+  exit
+}
+
+failwith() { echo "$*" 1>&2; exit 1; }
 
 declare subscriptionId=""
 declare resourceGroupName=""
@@ -15,118 +20,76 @@ declare resourceGroupLocation=""
 declare templateFilePath=""
 declare parametersFilePath=""
 
-# Initialize parameters specified from command line
+# initialize parameters specified from command line
 while getopts ":i:g:n:l:t:p:" arg; do
-	case "${arg}" in
-		i)
-			subscriptionId=${OPTARG}
-			;;
-		g)
-			resourceGroupName=${OPTARG}
-			;;
-		n)
-			deploymentName=${OPTARG}
-			;;
-		l)
-			resourceGroupLocation=${OPTARG}
-			;;
-	        t)
-			templateFilePath=${OPTARG}
-			;;
-		p)
-			parametersFilePath=${OPTARG}
-			;;
-		esac
+  case "${arg}" in
+    ( i ) subscriptionId="${OPTARG}"        ;;
+    ( g ) resourceGroupName="${OPTARG}"     ;;
+    ( n ) deploymentName="${OPTARG}"        ;;
+    ( l ) resourceGroupLocation="${OPTARG}" ;;
+    ( t ) templateFilePath="${OPTARG}"      ;;
+    ( p ) parametersFilePath="${OPTARG}"    ;;
+  esac
 done
 shift $((OPTIND-1))
 
-#Prompt for parameters is some required parameters are missing
-if [[ -z "$subscriptionId" ]]; then
-	echo "Subscription Id:"
-	read subscriptionId
-	[[ "${subscriptionId:?}" ]]
-fi
+# required
+if [[ -z "$subscriptionId" ]]; then; read -p "Subscription Id: " subscriptionId; fi
+if [[ -z "$subscriptionId" ]]; then failwith "subscription id required"; fi
 
-if [[ -z "$resourceGroupName" ]]; then
-	echo "ResourceGroupName:"
-	read resourceGroupName
-	[[ "${resourceGroupName:?}" ]]
-fi
+# required
+if [[ -z "$resourceGroupName" ]]; then read -p "ResourceGroupName: " resourceGroupName; fi
+if [[ -z "$resourceGroupName" ]]; then failwith "resource group name required"; fi
 
-if [[ -z "$deploymentName" ]]; then
-	echo "DeploymentName:"
-	read deploymentName
-fi
+# optional
+if [[ -z "$deploymentName" ]]; then read -p "DeploymentName: " deploymentName; fi
 
+# optional
 if [[ -z "$resourceGroupLocation" ]]; then
-	echo "Enter a location below to create a new resource group else skip this"
-	echo "ResourceGroupLocation:"
-	read resourceGroupLocation
+  echo "Enter a location to create a new resource group otherwise skip it"
+  read -p "ResourceGroupLocation: " resourceGroupLocation
 fi
 
-if [[ -z "$templateFilePath" ]]; then
-	#templateFile Path - default template file to be used
-	templateFilePath="azureDeployMainTemplate.json"
-fi
+if [[ -z "$templateFilePath" ]]; then templateFilePath="azureDeployMainTemplate.json"; fi
 
-if [ ! -f "$templateFilePath" ]; then
-	echo "$templateFilePath not found"
-	exit 1
-fi
+if [[ ! -f "$templateFilePath" ]]; then failwith "$templateFilePath not found"; fi
 
 if [[ -z "$parametersFilePath" ]]; then
-	#parameter file path - default parameter file to be used
-	parametersFilePath="azureDeployParameters.json"
+  # parameter file path - default parameter file to be used
+  parametersFilePath="azureDeployParameters.json"
 fi
 
-if [ ! -f "$parametersFilePath" ]; then 
-	echo "$parametersFilePath not found"
-	exit 1
+if [[ ! -f "$parametersFilePath" ]]; then
+  echo "$parametersFilePath not found"
+  exit 1
 fi
 
-if [ -z "$subscriptionId" ] || [ -z "$resourceGroupName" ] || [ -z "$deploymentName" ]; then
-	echo "Either one of subscriptionId, resourceGroupName, deploymentName is empty"
-	usage
-fi
-
-#login to azure using your credentials
+# login to azure using your credentials
 set +e
-az account show 1> /dev/null
+az account show > /dev/null
 
-if [ $? != 0 ]; 
-then
-	az login
-fi
+if [[ $? != 0 ]]; then az login; fi
 
-#set the default subscription id
+# set the default subscription id
 set -e
-az account set --subscription $subscriptionId
+az account set --subscription "$subscriptionId"
 
-#Check for existing RG
-set +e
-az group show -n $resourceGroupName | grep -q $resourceGroupName
+# check for existing RG
+az group show -n "$resourceGroupName" | grep -q "$resourceGroupName"
 
-if [ $? != 0 ]; then
-	echo "Resource group with name" $resourceGroupName "could not be found. Creating new resource group.."
-	set -e
-	(
-		set -x
-		az group create --name $resourceGroupName --location $resourceGroupLocation 1> /dev/null
-	)
-	else
-	echo "Using existing resource group..."
+if (( $? != 0 )); then
+  echo "Resource group with name $resourceGroupName not found,"
+  echo "Creating a new resource group."
+  az group create --name "$resourceGroupName" --location "$resourceGroupLocation" \
+     > /dev/null \
+    || { echo "Resource group creation failure" 1>&2; exit 1; }
+else
+  echo "Using existing resource group..."
 fi
 
-#set -e
-
-#Start deployment
 echo "Starting deployment..."
-(
-	set -x
-	az group deployment create --name $deploymentName --resource-group $resourceGroupName --template-file $templateFilePath --parameters @$parametersFilePath
-)
-
-if [ $?  == 0 ]; 
- then
-	echo "Template has been successfully deployed"
-fi
+az group deployment create \
+   --name "$deploymentName" --resource-group "$resourceGroupName" \
+   --template-file "$templateFilePath" --parameters "@$parametersFilePath" \
+  || failwith "Deployment failed"
+echo "Template has been successfully deployed"
