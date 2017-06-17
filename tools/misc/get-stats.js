f508b703e488c1d135c51201de1201e06cacf521
Eli Barzilay <eli@barzilay.org>
2017-06-17 05:13:04 -0400

    Add `tools/misc` and some more build additions
    * Add `tools/misc` with some maintenance scripts that should be properly
      kept.  Currently, one script that I'm using to cleanup old storage,
      and another that is used to collect stats (in bash and in JS).
    
    * Fix a bunch of scriptlets: drop the `"$@"` when calling `runme` so it
      is possible to pass arguments.  (None are actually using arguments so
      this wasn't a problem.)
    
    * Add the `PR-Build.md` to `Build.md` so that all information is kept in
      a single place instead of two VSTS uploads.  Make the post-pr report
      include that text in a `success` comment.
    
    * Streamline `Build.md` additions via `_add_to_description`.
    
    * Minor markdown tweaks.

---
+++ tools/misc/get-stats.js
@@ -0,0 +1,63 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in project root for information.
+
+let az = require("azure-storage");
+
+let httpsGet = require("https").get;
+let get = url => new Promise((resolve, reject) =>
+  httpsGet(url, r => {
+    r.setEncoding("utf8");
+    let str = "";
+    r.on("data", d => str += d);
+    r.on("end", () => resolve(JSON.parse(str)));
+  }));
+
+let ghAPI = require("github");
+let repo = {owner: "Azure", repo: "mmlspark"};
+let repo_pg = Object.assign({}, repo, {per_page: 100});
+
+module.exports = ctx => {
+  let blob = az.createBlobService(process.env.AzureWebJobsStorage);
+  let gh   = new ghAPI();
+  let timeStamp = new Date().toISOString()
+                        .replace(/:[0-9][0-9]\.[0-9]+Z/, "").replace(/T/, " ");
+
+  let getPaged = (state, combine) => r =>
+    !gh.hasNextPage(r) ? Promise.resolve(combine(state, r.data))
+    : gh.getNextPage(r).then(getPaged(combine(state, r.data), combine));
+  let getAllContributors = getPaged([], (st,d) => st.concat(d.map(x => x.login)))
+  let getAllIssues = getPaged([[],[]], (st,d) =>
+    [st[0].concat(d.filter(x => !("pull_request" in x)).map(x => "#"+x.number)),
+     st[1].concat(d.filter(x =>  ("pull_request" in x)).map(x => "#"+x.number))])
+
+  // To initialize the append blob (delete the rest when doing this to avoid races):
+  // blob.createAppendBlobFromText("stats", "log", `initial text`,
+  //   (err) => { if (err) ctx.done(err); else { ctx.log(">>> DONE!"); ctx.done(); }});
+
+  let text = ([repo, [issues, pulls], contribs, docker, dtags]) => `
+    |=========================
+    |${timeStamp}
+    |github:
+    |  stars: ${       repo.stargazers_count }
+    |  watchers: ${    repo.subscribers_count}
+    |  forks: ${       repo.forks_count      }
+    |  issues_pulls: ${repo.open_issues_count}
+    |  contributors: ${contribs.length} => ${contribs.join(", ")}
+    |  pulls: ${       pulls   .length} => ${pulls   .join(", ")}
+    |  issues: ${      issues  .length} => ${issues  .join(", ")}
+    |docker:
+    |  pulls: ${docker.pull_count}
+    |  stars: ${docker.star_count}
+    |  tags: ${dtags.length} => ${dtags.map(x => x.name).join(", ")}
+    `.replace(/(?:^|\r?(\n))\s*(?:\||$)/g, "$1");
+
+  Promise.all([gh.repos.get(repo).then(r => Promise.resolve(r.data)),
+               gh.issues.getForRepo(repo_pg).then(getAllIssues),
+               gh.repos.getContributors(repo_pg).then(getAllContributors),
+               get("https://hub.docker.com/v2/repositories/microsoft/mmlspark/"),
+               get("https://hub.docker.com/v2/repositories/microsoft/mmlspark/tags/")
+                 .then(r => Promise.resolve(r.results))])
+    .then(r => blob.appendFromText("stats", "log", text(r),
+                                   (err) => err ? ctx.done(err) : ctx.done()));
+
+};
--- tools/pip/generate-pip.sh
+++ tools/pip/generate-pip.sh
@@ -2,7 +2,7 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in project root for information.
 
-. "$(dirname "${BASH_SOURCE[0]}")/../../runme" "$@"
+. "$(dirname "${BASH_SOURCE[0]}")/../../runme"
 main() {
 
 local srcdir="$TOOLSDIR/pip"
