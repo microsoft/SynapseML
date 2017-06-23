9e3d299930d261981ef12d2f420a5dafe082f3c0
Eli Barzilay <eli@barzilay.org>
2017-06-23 13:40:43 -0400

    Text cleanup and other cosmetic improvements

--- tools/deployment/bashshelldeploy.sh
+++ tools/deployment/bashshelldeploy.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+
 set -euo pipefail
 IFS=$'\n\t'
 
@@ -6,127 +9,79 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation> -t <templateFilePath> -p <parametersFilePath>" 1>&2; exit 1; }
+usage() {
+  echo "Usage: $(basename "$0") -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation> -t <templateFilePath> -p <parametersFilePath>"
+  exit
+}
 
-declare subscriptionId=""
-declare resourceGroupName=""
-declare deploymentName=""
-declare resourceGroupLocation=""
-declare templateFilePath=""
-declare parametersFilePath=""
+failwith() { echo "$*" 1>&2; exit 1; }
 
-# Initialize parameters specified from command line
+subscriptionId=""
+resourceGroupName=""
+deploymentName=""
+resourceGroupLocation=""
+templateFilePath=""
+parametersFilePath=""
+
+# initialize parameters specified from command line
 while getopts ":i:g:n:l:t:p:" arg; do
-	case "${arg}" in
-		i)
-			subscriptionId=${OPTARG}
-			;;
-		g)
-			resourceGroupName=${OPTARG}
-			;;
-		n)
-			deploymentName=${OPTARG}
-			;;
-		l)
-			resourceGroupLocation=${OPTARG}
-			;;
-	        t)
-			templateFilePath=${OPTARG}
-			;;
-		p)
-			parametersFilePath=${OPTARG}
-			;;
-		esac
+  case "${arg}" in
+    ( i ) subscriptionId="${OPTARG}"        ;;
+    ( g ) resourceGroupName="${OPTARG}"     ;;
+    ( n ) deploymentName="${OPTARG}"        ;;
+    ( l ) resourceGroupLocation="${OPTARG}" ;;
+    ( t ) templateFilePath="${OPTARG}"      ;;
+    ( p ) parametersFilePath="${OPTARG}"    ;;
+  esac
 done
 shift $((OPTIND-1))
 
-#Prompt for parameters is some required parameters are missing
-if [[ -z "$subscriptionId" ]]; then
-	echo "Subscription Id:"
-	read subscriptionId
-	[[ "${subscriptionId:?}" ]]
-fi
+# required
+if [[ -z "$subscriptionId" ]]; then read -p "Subscription Id: " subscriptionId; fi
+if [[ -z "$subscriptionId" ]]; then failwith "subscription id required"; fi
 
-if [[ -z "$resourceGroupName" ]]; then
-	echo "ResourceGroupName:"
-	read resourceGroupName
-	[[ "${resourceGroupName:?}" ]]
-fi
+# required
+if [[ -z "$resourceGroupName" ]]; then read -p "ResourceGroupName: " resourceGroupName; fi
+if [[ -z "$resourceGroupName" ]]; then failwith "resource group name required"; fi
 
-if [[ -z "$deploymentName" ]]; then
-	echo "DeploymentName:"
-	read deploymentName
-fi
+# optional
+if [[ -z "$deploymentName" ]]; then read -p "DeploymentName: " deploymentName; fi
 
+# optional
 if [[ -z "$resourceGroupLocation" ]]; then
-	echo "Enter a location below to create a new resource group else skip this"
-	echo "ResourceGroupLocation:"
-	read resourceGroupLocation
+  echo "Enter a location to create a new resource group otherwise skip it"
+  read -p "ResourceGroupLocation: " resourceGroupLocation
 fi
 
-if [[ -z "$templateFilePath" ]]; then
-	#templateFile Path - default template file to be used
-	templateFilePath="azureDeployMainTemplate.json"
+if [[ -z "$templateFilePath" ]]; then templateFilePath="azureDeployMainTemplate.json"; fi
+
+if [[ ! -f "$templateFilePath" ]]; then failwith "$templateFilePath not found"; fi
+
+if [[ -z "$parametersFilePath" ]]; then parametersFilePath="azureDeployParameters.json"; fi
+
+if [[ ! -f "$parametersFilePath" ]]; then failwith "$parametersFilePath not found"; fi
+
+# login to azure using your credentials
+az account show > /dev/null || az login
+
+# set the default subscription id
+az account set --subscription "$subscriptionId"
+
+# check for existing RG
+if az group show -n "$resourceGroupName" | grep -q "$resourceGroupName"; then
+  echo "Using existing resource group..."
+else
+  echo "Resource group with name $resourceGroupName not found,"
+  echo "Creating a new resource group."
+  if [[ -z "$resourceGroupLocation" ]]; then failwith "resource group location required"; fi
+  az group create --name "$resourceGroupName" --location "$resourceGroupLocation" \
+     > /dev/null \
+    || { echo "Resource group creation failure" 1>&2; exit 1; }
 fi
 
-if [ ! -f "$templateFilePath" ]; then
-	echo "$templateFilePath not found"
-	exit 1
-fi
-
-if [[ -z "$parametersFilePath" ]]; then
-	#parameter file path - default parameter file to be used
-	parametersFilePath="azureDeployParameters.json"
-fi
-
-if [ ! -f "$parametersFilePath" ]; then 
-	echo "$parametersFilePath not found"
-	exit 1
-fi
-
-if [ -z "$subscriptionId" ] || [ -z "$resourceGroupName" ] || [ -z "$deploymentName" ]; then
-	echo "Either one of subscriptionId, resourceGroupName, deploymentName is empty"
-	usage
-fi
-
-#login to azure using your credentials
-set +e
-az account show 1> /dev/null
-
-if [ $? != 0 ]; 
-then
-	az login
-fi
-
-#set the default subscription id
-set -e
-az account set --subscription $subscriptionId
-
-#Check for existing RG
-set +e
-az group show -n $resourceGroupName | grep -q $resourceGroupName
-
-if [ $? != 0 ]; then
-	echo "Resource group with name" $resourceGroupName "could not be found. Creating new resource group.."
-	set -e
-	(
-		set -x
-		az group create --name $resourceGroupName --location $resourceGroupLocation 1> /dev/null
-	)
-	else
-	echo "Using existing resource group..."
-fi
-
-#set -e
-
-#Start deployment
 echo "Starting deployment..."
-(
-	set -x
-	az group deployment create --name $deploymentName --resource-group $resourceGroupName --template-file $templateFilePath --parameters @$parametersFilePath
-)
-
-if [ $?  == 0 ]; 
- then
-	echo "Template has been successfully deployed"
-fi
+az group deployment create \
+   --name "$deploymentName" --resource-group "$resourceGroupName" \
+   --template-file "$templateFilePath" --parameters "@$parametersFilePath" \
+  || failwith "Deployment failed"
+echo "Template has been successfully deployed"
--- tools/deployment/powershelldeploy.ps1
+++ tools/deployment/powershelldeploy.ps1
@@ -55,7 +55,6 @@ Function RegisterRP {
     Param(
         [string]$ResourceProviderNamespace
     )
-
     Write-Host "Registering resource provider '$ResourceProviderNamespace'";
     Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
 }
@@ -76,31 +75,29 @@ Select-AzureRmSubscription -SubscriptionID $subscriptionId;
 
 # Register RPs
 $resourceProviders = @("microsoft.hdinsight");
-if($resourceProviders.length) {
+if ($resourceProviders.length) {
     Write-Host "Registering resource providers"
-    foreach($resourceProvider in $resourceProviders) {
+    foreach ($resourceProvider in $resourceProviders) {
         RegisterRP($resourceProvider);
     }
 }
 
 #Create or check for existing resource group
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
-if(!$resourceGroup)
-{
+if (!$resourceGroup) {
     Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
-    if(!$resourceGroupLocation) {
+    if (!$resourceGroupLocation) {
         $resourceGroupLocation = Read-Host "resourceGroupLocation";
     }
     Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
     New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
-}
-else{
+} else {
     Write-Host "Using existing resource group '$resourceGroupName'";
 }
 
 # Start the deployment
 Write-Host "Starting deployment...";
-if(Test-Path $parametersFilePath) {
+if (Test-Path $parametersFilePath) {
     New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
 } else {
     New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
