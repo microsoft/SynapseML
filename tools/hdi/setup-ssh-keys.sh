d1746a9cca2c9f3e369d6e68a5a6019e75684e79
Tong Wen <towen@microsoft.com>
2017-07-25 12:43:51 -0400

    Rename file: `setup-ssh-keys` -> `setup-ssh-keys.sh`
    Also add copyright.

--- tools/hdi/setup-ssh-keys.sh
+++ tools/hdi/setup-ssh-keys.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
 
 usage() { echo "Usage: $(basename "$0") <vm-name> [<username>] "; exit 0; }
 
