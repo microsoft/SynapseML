f56d4ce82b30ccdb13051e924a02e0275bfa318b
Ilya Matiach <ilmat@microsoft.com>
2017-07-26 18:29:33 -0400

    Fixing code to load native shared object files from OpenCV

--- src/core/schema/src/main/scala/ImageSchema.scala
+++ src/core/schema/src/main/scala/ImageSchema.scala
@@ -9,6 +9,7 @@ import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.types._
 
 import scala.reflect.ClassTag
+import scala.collection.JavaConverters._
 
 object ImageSchema {
 
@@ -35,9 +36,24 @@ object ImageSchema {
   def isImage(df: DataFrame, column: String): Boolean =
     df.schema(column).dataType == columnSchema
 
+  // TODO: Figure out a better way to list all dependencies to be extracted in opencv jar
+  val dependencies = List("libgomp.so.1", "libjnicvkernels.so", "libjniopencv_bioinspired.so",
+    "libjniopencv_calib3d.so", "libjniopencv_core.so", "libjniopencv_dnn.so", "libjniopencv_face.so",
+    "libjniopencv_features2d.so", "libjniopencv_flann.so", "libjniopencv_highgui.so", "libjniopencv_imgcodecs.so",
+    "libjniopencv_imgproc.so", "libjniopencv_ml.so", "libjniopencv_objdetect.so", "libjniopencv_optflow.so",
+    "libjniopencv_photo.so", "libjniopencv_shape.so", "libjniopencv_stitching.so", "libjniopencv_superres.so",
+    "libjniopencv_text.so", "libjniopencv_video.so", "libjniopencv_videoio.so", "libjniopencv_videostab.so",
+    "libjniopencv_xfeatures2d.so", "libjniopencv_ximgproc.so", "libopencv_bioinspired.so.3.2",
+    "libopencv_calib3d.so.3.2", "libopencv_core.so.3.2", "libopencv_dnn.so.3.2", "libopencv_face.so.3.2",
+    "libopencv_features2d.so.3.2", "libopencv_flann.so.3.2", "libopencv_highgui.so.3.2", "libopencv_imgcodecs.so.3.2",
+    "libopencv_imgproc.so.3.2", "libopencv_ml.so.3.2", "libopencv_objdetect.so.3.2", "libopencv_optflow.so.3.2",
+    "libopencv_photo.so.3.2", "libopencv_shape.so.3.2", "libopencv_stitching.so.3.2", "libopencv_superres.so.3.2",
+    "libopencv_text.so.3.2", "libopencv_video.so.3.2", "libopencv_videoio.so.3.2", "libopencv_videostab.so.3.2",
+    "libopencv_xfeatures2d.so.3.2", "libopencv_ximgproc.so.3.2")
+
   private[spark] def loadLibraryForAllPartitions[T:ClassTag](rdd: RDD[T], lib: String):RDD[T] = {
     def perPartition(it: Iterator[T]):Iterator[T] = {
-      new NativeLoader("/org/bytedeco/javacpp").loadLibraryByName(lib); it }
+      new NativeLoader("/org/bytedeco/javacpp").loadLibraryWithDepsByName(lib, dependencies.asJava); it }
     rdd.mapPartitions(perPartition, preservesPartitioning = true)
   }
 }
