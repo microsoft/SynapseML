4135d54cbcf5a578e3bca32177beef2b93c5d531
Mark Hamilton <marhamil@microsoft.com>
2017-08-04 19:00:12 +0000

    Fix wrapper generation for complex params
    (And get rid of the bogus "registration" printout.)

--- src/core/schema/src/main/python/TypeConversionUtils.py
+++ src/core/schema/src/main/python/TypeConversionUtils.py
@@ -1,6 +1,8 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in project root for information.
 
+from py4j.protocol import Py4JError
+
 def generateTypeConverter(name, cache, typeConverter):
     """
     Type converter
@@ -28,16 +30,20 @@ def complexTypeConverter(name, value, cache):
         _java_obj:
     """
     cache[name]=value
-    if isinstance(value, list):
-        java_value=[]
-        for v in value:
-            if hasattr(v, "_transfer_params_to_java"):
-                v._transfer_params_to_java()
-            java_value.append(v._java_obj)
-        return java_value
-    if hasattr(value, "_transfer_params_to_java"):
-        value._transfer_params_to_java()
-    if hasattr(value, "_java_obj"):
-        return value._java_obj
-    else:
-        return value._to_java()
+    try:
+        if isinstance(value, list):
+            java_value=[]
+            for v in value:
+                if hasattr(v, "_transfer_params_to_java"):
+                    v._transfer_params_to_java()
+                java_value.append(v._java_obj)
+            return java_value
+        if hasattr(value, "_transfer_params_to_java"):
+            value._transfer_params_to_java()
+        if hasattr(value, "_java_obj"):
+            return value._java_obj
+        else:
+            return value._to_java()
+    except Py4JError as e:
+        return value
+
