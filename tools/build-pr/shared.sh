microsoft/SynapseML

commit f1704d37b5baaef971274b99d98f8f83712c4e78

Script fixes and improvements
* When `CNTK_WHEELS` isn't specified for an environment, ignore it
  instead of failing.

* `defvar` has a new `-X` flag for variables that are all injected into
  generated scripts (currently only the HDI scripts).

* `defvar` also has `-e` which adds the setting to
  "$HOME/.mmlspark_profile", replacing `setenv` -- and also `-E` is a
  combination of `-xXe`.

* Use the above features to generate the HDI scripts instead of listing
  each value that is to be replaced.  (It also makes it easier to
  maintain since they use the same variable names as the rest of the
  runme scripts.)

* Use `$CNTK_VERSION` in the HDI scripts, which is now inherited from
  the same runme environment as everything else.

* Fix the version sorting in container-gc, so no versions are
  dropped (using 1,2 includes the second key (after the `#`) so it gets
  considered for uniqueness too).

tools/config.sh
@@ -55,7 +55,7 @@ _main_url() { echo "https://$MAIN_STORAGE.azureedge.net/$1"; }
 # The base URL for our installables
 defvar INSTALLER_URL "$(_main_url "installers")"
 # Directory for caching installers; if it is empty then no caching is used
-defvar INSTALLER_CACHE_DIR "$HOME/.mmlspark_cache"
+defvar -p INSTALLER_CACHE_DIR "$HOME/.mmlspark_cache"
 
 INSTALLATIONS=(
 
@@ -155,8 +155,8 @@ SBT.setup() {
 defvar SCALA_VERSION "2.11"
 defvar SCALA_FULL_VERSION "$SCALA_VERSION.8"
 SBT.init() {
-  setenv SCALA_VERSION "$SCALA_VERSION"
-  setenv SCALA_FULL_VERSION "$SCALA_FULL_VERSION"
+  defvar -E SCALA_VERSION "$SCALA_VERSION"
+  defvar -E SCALA_FULL_VERSION "$SCALA_FULL_VERSION"
 }
 
 Spark.setup() {
@@ -238,24 +238,24 @@ CNTK.init() {
 
 # Storage for build artifacts
 defvar STORAGE_CONTAINER "buildartifacts"
-defvar STORAGE_URL "$(_main_url "$STORAGE_CONTAINER")"
+defvar -X STORAGE_URL    "$(_main_url "$STORAGE_CONTAINER")"
 
 # Container for docs and maven/pip packages
-defvar DOCS_CONTAINER  "docs"
-defvar DOCS_URL        "$(_main_url "$DOCS_CONTAINER")"
-defvar MAVEN_CONTAINER  "maven"
-defvar -x MAVEN_URL     "$(_main_url "$MAVEN_CONTAINER")"
-defvar -d MAVEN_PACKAGE "com.microsoft.ml.spark:mmlspark_$SCALA_VERSION:<{MML_VERSION}>"
-defvar PIP_CONTAINER    "pip"
-defvar -x PIP_URL       "$(_main_url "$PIP_CONTAINER")"
-defvar -d PIP_PACKAGE   "mmlspark-<{MML_VERSION}>-py2.py3-none-any.whl"
+defvar DOCS_CONTAINER    "docs"
+defvar DOCS_URL          "$(_main_url "$DOCS_CONTAINER")"
+defvar MAVEN_CONTAINER   "maven"
+defvar -xX MAVEN_URL     "$(_main_url "$MAVEN_CONTAINER")"
+defvar -dX MAVEN_PACKAGE "com.microsoft.ml.spark:mmlspark_$SCALA_VERSION:<{MML_VERSION}>"
+defvar PIP_CONTAINER     "pip"
+defvar -xX PIP_URL       "$(_main_url "$PIP_CONTAINER")"
+defvar -dX PIP_PACKAGE   "mmlspark-<{MML_VERSION}>-py2.py3-none-any.whl"
 
 # E2E test cluster information
 defvar E2E_CLUSTER_NAME   "mmlsparktest"
 defvar E2E_RESOURCE_GROUP "mmlsparktest"
 defvar E2E_CLUSTER_SSH    "spark@${E2E_CLUSTER_NAME}-ssh.azurehdinsight.net"
 defvar E2E_PARALLEL_RUNS  "2"
-defvar CLUSTER_SDK_DIR    "/mml-sdk" # this is for all clusters
+defvar -X CLUSTER_SDK_DIR "/mml-sdk" # this is for all clusters
 
 # Demo cluster information
 defvar DEMO_CLUSTER_NAME   "mmlsparkdemo"

tools/misc/container-gc
@@ -26,7 +26,7 @@ GH_API="https://api.github.com/repos/Azure/mmlspark"
 github_prs="$(curl --silent --show-error "$GH_API/pulls" \
               | jq -r 'map(.number | tostring) | " " + join(" ") + " "')"
 
-vsort() { sort -t "+" -k 1,1 -V "$@"; }
+vsort() { sort -t "+" -k 1,2 -V "$@"; }
 
 now=0; _minute=$((60)); _hour=$((60*60)); _day=$((_hour*24))
 show-time() (

tools/runme/install.sh
@@ -46,7 +46,7 @@ _do_envinits() {
     if [[ "$cmd" = "export "*"="* ]]; then
       var="${cmd#export }"; var="${var%%=*}"; val="$(qstr "${!var}")"
     fi
-    # print only commands and setenvs that change values
+    # print only commands and var settings that change values
     if [[ "$var" = "" || "$cmd" != "export $var=$val" ]]; then show command "$cmd"; fi
   done
   eval "$script"
@@ -143,8 +143,8 @@ _install() { # libname
      || (                             " $where " != *" devel "*   ) ]]; then return
   fi
   local dir="$HOME/lib/$lib"
-  setenv "${envvar}_VERSION" "$ver"
-  setenv "${envvar}_HOME"    "$dir"
+  defvar -E "${envvar}_VERSION" "$ver"
+  defvar -xe "${envvar}_HOME"    "$dir"
   if [[ "x$prereq" != "x" ]] && ! eval "${prereq%|*}" > /dev/null 2>&1; then
     failwith "$libname: prerequisite failure: ${prereq##*|}"
   fi
@@ -157,8 +157,8 @@ _install() { # libname
   else Op="Installing"; _note_work "Install"; fi
   # avoid output up to here, so there's nothing unless we actually do something
   show section "$Op $libname v$ver in $dir"
-  show command setenv "${envvar}_VERSION" "$ver"
-  show command setenv "${envvar}_HOME"    "$dir"
+  show command export "${envvar}_VERSION=$ver"
+  show command export "${envvar}_HOME=$dir"
   if [[ "$update" = "Y" && "$(_verify_version "$libname")" = "" ]]; then
     show warning "Looks like $libname was already updated, noting new version"
     _ cd "$dir"

tools/runme/utils.sh
@@ -7,12 +7,19 @@
 # ---< defvar [opts] var val >--------------------------------------------------
 # Use this to define customize-able variables (no effect if it exists).
 # Additional arguments are concatenated (wihtout spaces) to make long value
-# settings look nice.  Use "-x" to export the variable, "-p" to resolve the
-# value to an absolute path from where we are, "-f" to set the value even if
-# it's already set.  You can also use "-d" to define values with delayed
-# references to other variables using "...<{var}>..." -- these will be replaced
-# at the end of processing the config file.
+# settings look nice.  Uses the following flags:
+# * `-x`: export the variable,
+# * `-X`: mark the variable to be included in generated script resources
+#         like the HDI script actions,
+# * `-e`: set in the user environment (in "$HOME/.mmlspark_profile"),
+# * `-E`: implies `-xXe`,
+# * `-p`: resolve the value to an absolute path from where we are,
+# * `-f`: set the value even if it's already set,
+# * `-d`: define values with delayed references to other variables using
+#         `...<{var}>...` -- these will be replaced at the end of
+#         processing the config file.
 _delayed_vars=()
+_gen_vars=()
 defvar() {
   local opts=""; while [[ "x$1" == "x-"* ]]; do opts+="${1:1}"; shift; done
   local var="$1" val v; shift
@@ -20,8 +27,17 @@ defvar() {
     val=""; for v; do val+="$v"; done; printf -v "$var" "%s" "$val"; fi
   if [[ "$opts" == *"p"* && "x${!var}" != "/"* ]]; then
     printf -v "$var" "%s" "$(realpath -m "${!var}")"; fi
-  if [[ "$opts" == *"x"* ]]; then export "$var"; fi
-  if [[ "$opts" == *"d"* ]]; then _delayed_vars+=( "$var" ); fi
+  if [[ "$opts" == *[xE]* ]]; then export "$var"; fi
+  if [[ "$opts" == *[XE]* && " ${_gen_vars[*]} " != *" $var "* ]]; then
+    _gen_vars+=( "$var" ); fi
+  if [[ "$opts" == *"d"* ]]; then
+    _delayed_vars+=( "$var" ); fi
+  if [[ "$opts" == *[eE]* ]]; then
+    envinit_commands+=("export $var=$(qstr "${!var}")"); fi
+}
+_show_gen_vars() {
+  local var
+  for var in "${_gen_vars[@]}"; do printf '%s=%s\n' "$var" "$(qstr "${!var}")"; done
 }
 _replace_delayed_vars() {
   local var val pfx sfx change=1
@@ -322,10 +338,6 @@ set_install_info_vars() {
 envinit_commands=('export MMLSPARK_PROFILE="yes"')
 envinit_eval() { envinit_commands+=("$*"); eval "$*"; }
 
-# ---< setenv var val >---------------------------------------------------------
-# Set an environment variable; include the setting in the user environment too.
-setenv() { envinit_eval "export $1=$(qstr "$2")"; }
-
 # ---< get_runtime_hash >-------------------------------------------------------
 # Prints out a hash of the currently configured runtime environment.  The hash
 # depends on the relevant bits of configuration, including a .setup and .init
@@ -463,9 +475,9 @@ _set_build_info() {
     #
     cd "$owd"
   fi
-  defvar -x MML_VERSION    "$version"
-  defvar -x MML_BUILD_INFO "$info"
-  defvar -x MML_LATEST     "$is_latest"
+  defvar -xX MML_VERSION    "$version"
+  defvar -xX MML_BUILD_INFO "$info"
+  defvar -xX MML_LATEST     "$is_latest"
 }
 # To be called when re-creating $BUILD_ARTIFACTS
 _reset_build_info() {

tools/hdi/install-mmlspark.sh
@@ -6,22 +6,18 @@
 # Configurations for installing mmlspark + dependencies on an HDI
 # cluster, from a specific storage blob (which is created by the build).
 
-# These are replaced by the build process.
-DOWNLOAD_URL="<=<=fill-in-url=>=>"
-MAVEN_PACKAGE="<=<=fill-in-maven-package=>=>"
-MAVEN_URL="<=<=fill-in-maven-url=>=>"
-PIP_PACKAGE="<=<=fill-in-pip-package=>=>"
-SDK_DIR="<=<=fill-in-sdk-dir=>=>"
+# <=<= this line is replaced with variables defined with `defvar -X` =>=>
+DOWNLOAD_URL="$STORAGE_URL/$MML_VERSION"
+
 HDFS_NOTEBOOKS_FOLDER="/HdiNotebooks/Microsoft ML Spark Examples"
 
 CPATH="/usr/bin/anaconda/bin"
 CONDA_ENVS=( $("$CPATH/conda" info --envs | grep "^[^#]" | sed -e "s/ .*//") )
 
-CNTK_VER="2.0.beta12.0"
 CNTK_BASE_URL="https://cntk.ai/PythonWheel/CPU-Only"
 declare -A CNTK_WHEELS=(
-  [root]="$CNTK_BASE_URL/cntk-$CNTK_VER-cp27-cp27mu-linux_x86_64.whl"
-  [py35]="$CNTK_BASE_URL/cntk-$CNTK_VER-cp35-cp35m-linux_x86_64.whl")
+  [root]="$CNTK_BASE_URL/cntk-$CNTK_VERSION-cp27-cp27mu-linux_x86_64.whl"
+  [py35]="$CNTK_BASE_URL/cntk-$CNTK_VERSION-cp35-cp35m-linux_x86_64.whl")
 
 get_headnodes() {
   hdfssite="$(< "/etc/hadoop/conf/hdfs-site.xml")"
@@ -46,12 +42,14 @@ apt-get install -y openmpi-bin libunwind8
 
 # Install CNTK in all environments
 for env in "${CONDA_ENVS[@]}"; do
+  # wheel="${CNTK_WHEELS[$env]:?"Unknown conda env for CNTK: $env"}"
+  # ... but hdi will have additional environments, created by vienna
+  if [[ -z "${CNTK_WHEELS[$env]}" ]]; then continue; fi
   echo -n "[$env] "
-  wheel="${CNTK_WHEELS[$env]:?"Unknown conda env for CNTK: $env"}"
   . "$CPATH/activate" "$env"
   pkg="$(pip freeze | grep "^cntk")"
   if [[ "$pkg" != "cntk"* ]]; then echo "Installing CNTK..."; pip install "$wheel"
-  elif [[ "$pkg" = *"$CNTK_VER" ]]; then echo "CNTK is already installed."
+  elif [[ "$pkg" = *"$CNTK_VERSION" ]]; then echo "CNTK is already installed."
   else echo "Updating CNTK..."; pip install --upgrade --no-deps "$wheel"
   fi
   . "$CPATH/deactivate"
@@ -64,8 +62,8 @@ mkdir "$tmp"
 echo "Downloading materials..."
 curl $curlflags -o "$tmp/BuildArifacts.zip" "$DOWNLOAD_URL/BuildArtifacts.zip"
 curl $curlflags -o "$tmp/update_livy.py" "$DOWNLOAD_URL/update_livy.py"
-rm -rf "$SDK_DIR"; mkdir -p "$SDK_DIR"
-cd "$SDK_DIR"; unzip "$tmp/BuildArifacts.zip"; rm "$tmp/BuildArifacts.zip"
+rm -rf "$CLUSTER_SDK_DIR"; mkdir -p "$CLUSTER_SDK_DIR"
+cd "$CLUSTER_SDK_DIR"; unzip "$tmp/BuildArifacts.zip"; rm "$tmp/BuildArifacts.zip"
 
 # Change the Livy configuration
 echo "Updating the Livy configuration..."
@@ -77,7 +75,7 @@ for env in "${CONDA_ENVS[@]}"; do
   # first uninstall it, since otherwise an existing "1.2.dev3+4" version makes
   # pip consider "1.2.dev3" as "already satisfied"
   pip uninstall -y "mmlspark"
-  pip install "$PIP_PACKAGE"
+  pip install "$PIP_URL/$PIP_PACKAGE"
   . "$CPATH/deactivate"
 done
 
@@ -110,7 +108,7 @@ urlencode() {
   printf '\n'
 }
 
-for f in "$SDK_DIR/notebooks/hdinsight/"*.ipynb; do
+for f in "$CLUSTER_SDK_DIR/notebooks/hdinsight/"*.ipynb; do
   hdfs dfs -copyFromLocal "$(urlencode "$f")" "$HDFS_NOTEBOOKS_FOLDER"
 done
 

tools/hdi/setup-test-authkey.sh
@@ -2,8 +2,9 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in project root for information.
 
-SDK_DIR="<=<=fill-in-sdk-dir=>=>"
-NB_DIR="$SDK_DIR/notebooks" # This gets created as root, need to chown to spark
+# <=<= this line is replaced with variables defined with `defvar -X` =>=>
+
+NB_DIR="$CLUSTER_SDK_DIR/notebooks" # gets created as root, need to chown to spark
 
 # This is the public key used by the build to access the test cluster
 PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0FUryXQloryZQGXVP9vOqBVsuUWihHs"

tools/runme/build.sh
@@ -251,13 +251,12 @@ _upload_artifacts_to_storage() {
   ( cd "$BUILD_ARTIFACTS"
     _ zip -qr9 "$tmp/$(basename "$BUILD_ARTIFACTS.zip")" * )
   local f txt
+  local varlinerx="^(.*)# +<=<= .*? =>=>(.*)\$"
   for f in "$TOOLSDIR/hdi/"*; do
     txt="$(< "$f")"
-    txt="${txt//<=<=fill-in-maven-package=>=>/com.microsoft.ml.spark:mmlspark_$SCALA_VERSION:$MML_VERSION}"
-    txt="${txt//<=<=fill-in-maven-url=>=>/$MAVEN_URL}"
-    txt="${txt//<=<=fill-in-pip-package=>=>/$PIP_URL/$PIP_PACKAGE}"
-    txt="${txt//<=<=fill-in-sdk-dir=>=>/$CLUSTER_SDK_DIR}"
-    txt="${txt//<=<=fill-in-url=>=>/$STORAGE_URL/$MML_VERSION}"
+    if [[ "$txt" =~ $varlinerx ]]; then
+      txt="${BASH_REMATCH[1]}$(_show_gen_vars)${BASH_REMATCH[2]}"
+    fi
     echo "$txt" > "$tmp/$(basename "$f")"
   done
   _ azblob upload-batch --source "$tmp" --destination "$STORAGE_CONTAINER/$MML_VERSION"

commit f6f7fffbbfe727d3466cd3f4091d37c0dbd87531

A few more build/script fixes
* Fix a bug that was introduced in f1704d3: translations of the old
  `setenv` into `defvar -e` should have used the -f flag to force the
  values.  This bug would lead to the problem of "sticky" versions that
  don't change.  Make `-e` imply `-f`, which also makes sense since if
  we're changing a user's profile then we need to force a change of
  whatever is currently there.

* `container-gc`: use a file for collecting info, to avoid getting
  errors about going over the shell limits.

* Some more bash-isms.

tools/bin/mml-exec
@@ -20,7 +20,7 @@ else echo "Could not find \"runme\"" 1>&2; exit 1; fi
 # If we let spark guess a driver, it can find "python2.7" in the path (eg, the
 # system's installation) and use that; so do this to force it to use "python" in
 # our path, which is a symlink to the conda python.
-if [[ "x$PYSPARK_PYTHON" = "x" ]]; then export PYSPARK_PYTHON="python"; fi
+if [[ -z "$PYSPARK_PYTHON" ]]; then export PYSPARK_PYTHON="python"; fi
 
 if [[ "$exe" == "jupyter-notebook" ]]; then
   if [[ "${#args[@]}" = 0 ]]; then args=""

tools/build-pr/checkout
@@ -24,7 +24,7 @@ printf 'PR BUILD for #%s\n  repo: %s\n  ref: %s\n  sha1: %s\n' \
 
 git checkout "master" > /dev/null 2>&1
 oldbr="$(git for-each-ref --format="%(refname:short)" "refs/heads/pr-*")"
-if [[ "x$oldbr" != "x" ]]; then git branch -D $oldbr; fi
+if [[ -n "$oldbr" ]]; then git branch -D "$oldbr"; fi
 
 text="A build has started."
 post_status "pending" "$text"

tools/build-pr/shared.sh
@@ -13,7 +13,7 @@ fi
 
 T=""
 _get_T() {
-  if [[ "x$T" = "x" ]]; then
+  if [[ -z "$T" ]]; then
     T="$(__ az keyvault secret show --vault-name mmlspark-keys --name github-auth \
          | jq -r ".value" | base64 -d)"
   fi

tools/config.sh
@@ -149,9 +149,9 @@ defvar -p TEST_RESULTS    "$BASEDIR/TestResults"
 # Specific installation functions
 
 SBT.setup() {
-  local f="$SRCDIR/project/build.properties" txt="sbt.version = $SBT_VERSION"
+  local f="$SRCDIR/project/build.properties" txt="sbt.version=$SBT_VERSION"
   if [[ ! -e "$f" ]]; then echo "$txt" > "$f"; return; fi
-  if [[ "x$(< "$f")" != "x$txt" ]]; then failwith "$f exists"; fi
+  if [[ "$(< "$f")" != "$txt" ]]; then failwith "$f exists with unexpected contents"; fi
 }
 defvar SCALA_VERSION "2.11"
 defvar SCALA_FULL_VERSION "$SCALA_VERSION.8"

tools/docker/bin/eula
@@ -9,5 +9,5 @@ echo ""
 echo "(Note: you can also use \"-e ACCEPT_EULA=Y\" to indicate agreement.)"
 echo ""
 read -ep "Do you agree to the EULA? " R
-if [[ "x${R,,}" != @("xy"|"xyes") ]]; then echo "Bye."
+if [[ "${R,,}" != @("y"|"yes") ]]; then echo "Bye."
 else ACCEPT_EULA=Y launcher; fi

tools/misc/container-gc
@@ -4,7 +4,7 @@
 
 . "$(dirname "${BASH_SOURCE[0]}")/../../runme"
 
-types=(S M P D R)
+types=(S M P R D)
 declare -A S=([container]="$STORAGE_CONTAINER"
               [path]=""
               [suffix]="/")
@@ -15,13 +15,13 @@ declare -A P=([container]="$PIP_CONTAINER"
               [path]=""
               [prefix]="mmlspark-"
               [suffix]="-py2.py3-none-any.whl")
-declare -A D=([container]="$DOCS_CONTAINER"
-              [path]=""
-              [suffix]="")
 declare -A R=([container]="$R_CONTAINER"
               [path]=""
               [prefix]="mmlspark-"
               [suffix]=".zip")
+declare -A D=([container]="$DOCS_CONTAINER"
+              [path]=""
+              [suffix]="")
 
 set -e
 shopt -s nullglob
@@ -116,47 +116,53 @@ cachedir="$HOME/.$(basename "$0")"; mkdir -p "$cachedir"
  done)
 
 get-ver-info() (
-  v="$1" info="{}"
+  v="$1" info="$tmp-info"
+  echo "{}" > "$info"
   if [[ -r "$cachedir/$v" ]]; then return; fi
   for t in "${types[@]}"; do
     declare -n X="$t"
     pfx="${X[path]}${X[path]:+/}${X[prefix]}$v${X[suffix]}"
-    info="$(az storage blob list -c "${X[container]}" --prefix "$pfx" \
-            | jq --argjson i "$info" --arg pfx "$pfx" '$i + {"'"$t"'":
-                map(select((.name == $pfx)
-                           or ((.name | startswith($pfx))
-                               and (($pfx | endswith("/"))
-                                    or (.name | .[($pfx | length):]
-                                        | startswith("/"))))))}')"
+    az storage blob list -c "${X[container]}" --prefix "$pfx" \
+    | jq --slurpfile i "$info" --arg pfx "$pfx" '$i[0] + {"'"$t"'":
+        map(select((.name == $pfx)
+                   or ((.name | startswith($pfx))
+                       and (($pfx | endswith("/"))
+                            or (.name | .[($pfx | length):]
+                                | startswith("/"))))))}' \
+    > "$info-new"; mv "$info-new" "$info"
   done
   #
   label="$(jq -r '.S | map(select(.name | endswith("/.label")) | .name) | .[]' \
-                 <<<"$info")"
+                 < "$info")"
   if [[ "$label" = *$'\n'* ]]; then failwith "found multiple .label blobs"; fi
   if [[ -n "$label" ]]; then
     az storage blob download -c "${S[container]}" -n "$label" -f "$tmp" > /dev/null
     label="$(< "$tmp")"
     rm -f "$tmp"
   fi
-  info="$(jq --arg l "$label" '. + {label: $l}' <<<"$info")"
+  jq --arg l "$label" '. + {label: $l}' \
+     < "$info" > "$info-new"; mv "$info-new" "$info"
   #
   az storage blob download -c "${S[container]}" -n "$v/Build.md" -f "$tmp.md" > /dev/null
   binfo="$(< "$tmp.md")"; rm -f "$tmp.md"
   if [[ -z "$binfo" ]]; then binfo="(No info: \"Build.md\" file not found)"; fi
-  info="$(jq --arg b "$binfo" '. + {buildinfo: $b}' <<<"$info")"
+  jq --arg b "$binfo" '. + {buildinfo: $b}' \
+     < "$info" > "$info-new"; mv "$info-new" "$info"
   #
   rx="\n#+ [^\n]* build [^\n]*github PR #([0-9]+)"
   rx=$'\n'"#+ [^"$'\n'"]* build [^"$'\n'"]*github PR #([0-9]+)"
   if [[ "$binfo" =~ $rx ]]; then pr="${BASH_REMATCH[1]}"; else pr=""; fi
-  info="$(jq --arg pr "$pr" '. + {github_pr: $pr}' <<<"$info")"
+  jq --arg pr "$pr" '. + {github_pr: $pr}' \
+     < "$info" > "$info-new"; mv "$info-new" "$info"
   #
-  info="$(jq --argjson now "$(date +"%s")" \
+  jq --argjson now "$(date +"%s")" \
              '. + (to_entries
                    | map(select((.value | type) == "array") | .value) | add
                    | map(.properties.lastModified | sub("\\+00:00$"; "Z") | fromdate)
-                   | sort | {created: $now, mintime: .[0], maxtime: .[-1]})' <<<"$info")"
+                   | sort | {created: $now, mintime: .[0], maxtime: .[-1]})' \
+     < "$info" > "$info-new"; mv "$info-new" "$info"
   #
-  echo "$info" > "$cachedir/$v"
+  mv "$info" "$cachedir/$v"
 )
 
 show-all-oneline() {

tools/runme/install.sh
@@ -106,7 +106,7 @@ _unpack_zip() {
 }
 
 _unpack_sh() {
-  if [[ "x$instcmd" != "x" ]]; then eval "_ $instcmd"
+  if [[ -n "$instcmd" ]]; then eval "_ $instcmd"
   else failwith "sh package without instcmd: $1"; fi
 }
 
@@ -123,7 +123,7 @@ _retrieve_file() { # url file sha256
   fi
   _curl --output "$target" "$url"
   local sha256sum="$(__ sha256sum "$target")"; sha256sum="${sha256sum%% *}"
-  if [[ "x$sha256sum" = "x" ]]; then failwith "could not get sha256 checksum"; fi
+  if [[ -z "$sha256sum" ]]; then failwith "could not get sha256 checksum"; fi
   if [[ "$sha256sum" != "$sha256" ]]; then
     failwith "sha256 checksum failed for $target (retrieved from $url)"
   fi
@@ -143,9 +143,9 @@ _install() { # libname
      || (                             " $where " != *" devel "*   ) ]]; then return
   fi
   local dir="$HOME/lib/$lib"
-  defvar -E "${envvar}_VERSION" "$ver"
+  defvar -E  "${envvar}_VERSION" "$ver"
   defvar -xe "${envvar}_HOME"    "$dir"
-  if [[ "x$prereq" != "x" ]] && ! eval "${prereq%|*}" > /dev/null 2>&1; then
+  if [[ -n "$prereq" ]] && ! eval "${prereq%|*}" > /dev/null 2>&1; then
     failwith "$libname: prerequisite failure: ${prereq##*|}"
   fi
   if [[ "$(_verify_version -q "$libname")" = "" ]]; then

tools/runme/utils.sh
@@ -11,8 +11,9 @@
 # * `-x`: export the variable,
 # * `-X`: mark the variable to be included in generated script resources
 #         like the HDI script actions,
-# * `-e`: set in the user environment (in "$HOME/.mmlspark_profile"),
-# * `-E`: implies `-xXe`,
+# * `-e`: set in the user environment in "$HOME/.mmlspark_profile"
+#         (implies -f to avoid "sticky values"),
+# * `-E`: shorthand for `-xXef`,
 # * `-p`: resolve the value to an absolute path from where we are,
 # * `-f`: set the value even if it's already set,
 # * `-d`: define values with delayed references to other variables using
@@ -23,7 +24,7 @@ _gen_vars=()
 defvar() {
   local opts=""; while [[ "x$1" == "x-"* ]]; do opts+="${1:1}"; shift; done
   local var="$1" val v; shift
-  if [[ "$opts" == *"f"* || -z "${!var+x}" ]]; then
+  if [[ "$opts" == *[feE]* || -z "${!var+x}" ]]; then
     val=""; for v; do val+="$v"; done; printf -v "$var" "%s" "$val"; fi
   if [[ "$opts" == *"p"* && "x${!var}" != "/"* ]]; then
     printf -v "$var" "%s" "$(realpath -m "${!var}")"; fi
@@ -92,13 +93,10 @@ esac; shift; done
 # generic output.  $hide_in_log can be set to a string holding sensitive
 # information that should be hidden in the output.  The display uses "$HOME"
 # instead of the actual value whenever it appears.
-first_show="Y"
 hide_in_log=""
 show() {
   local tag="$1"; shift
-  if [[ "$first_show" = "Y" ]]; then first_show="N";
-  elif [[ "x$tag" = "xsection" ]]; then echo ""
-  fi
+  if [[ "x$tag" = "xsection" ]]; then echo ""; fi
   if [[ "x$tag" = "x-" ]]; then tag=""
   elif [[ "$BUILDMODE" = "server" ]]; then tag="##[$tag]"
   else case "$tag" in