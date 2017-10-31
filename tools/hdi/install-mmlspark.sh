microsoft/SynapseML

commit f1704d37b5baaef971274b99d98f8f83712c4e78

Script fixes and improvements
* When `CNTK_WHEELS` isn't specified for an environment, ignore it
  instead of failing.

* `defvar` has a new `-X` flag for variables that are all injected into
  generated scripts (currently only the HDI scripts).

* `defvar` also has `-e` which adds the setting to
  "$HOME/.mmlspark_profile", replacing `setenv` -- and also `-E` is a
  combination of `-xXe`.

* Use the above features to generate the HDI scripts instead of listing
  each value that is to be replaced.  (It also makes it easier to
  maintain since they use the same variable names as the rest of the
  runme scripts.)

* Use `$CNTK_VERSION` in the HDI scripts, which is now inherited from
  the same runme environment as everything else.

* Fix the version sorting in container-gc, so no versions are
  dropped (using 1,2 includes the second key (after the `#`) so it gets
  considered for uniqueness too).

tools/hdi/install-mmlspark.sh
@@ -6,22 +6,18 @@
 # Configurations for installing mmlspark + dependencies on an HDI
 # cluster, from a specific storage blob (which is created by the build).
 
-# These are replaced by the build process.
-DOWNLOAD_URL="<=<=fill-in-url=>=>"
-MAVEN_PACKAGE="<=<=fill-in-maven-package=>=>"
-MAVEN_URL="<=<=fill-in-maven-url=>=>"
-PIP_PACKAGE="<=<=fill-in-pip-package=>=>"
-SDK_DIR="<=<=fill-in-sdk-dir=>=>"
+# <=<= this line is replaced with variables defined with `defvar -X` =>=>
+DOWNLOAD_URL="$STORAGE_URL/$MML_VERSION"
+
 HDFS_NOTEBOOKS_FOLDER="/HdiNotebooks/Microsoft ML Spark Examples"
 
 CPATH="/usr/bin/anaconda/bin"
 CONDA_ENVS=( $("$CPATH/conda" info --envs | grep "^[^#]" | sed -e "s/ .*//") )
 
-CNTK_VER="2.0.beta12.0"
 CNTK_BASE_URL="https://cntk.ai/PythonWheel/CPU-Only"
 declare -A CNTK_WHEELS=(
-  [root]="$CNTK_BASE_URL/cntk-$CNTK_VER-cp27-cp27mu-linux_x86_64.whl"
-  [py35]="$CNTK_BASE_URL/cntk-$CNTK_VER-cp35-cp35m-linux_x86_64.whl")
+  [root]="$CNTK_BASE_URL/cntk-$CNTK_VERSION-cp27-cp27mu-linux_x86_64.whl"
+  [py35]="$CNTK_BASE_URL/cntk-$CNTK_VERSION-cp35-cp35m-linux_x86_64.whl")
 
 get_headnodes() {
   hdfssite="$(< "/etc/hadoop/conf/hdfs-site.xml")"
@@ -46,12 +42,14 @@ apt-get install -y openmpi-bin libunwind8
 
 # Install CNTK in all environments
 for env in "${CONDA_ENVS[@]}"; do
+  # wheel="${CNTK_WHEELS[$env]:?"Unknown conda env for CNTK: $env"}"
+  # ... but hdi will have additional environments, created by vienna
+  if [[ -z "${CNTK_WHEELS[$env]}" ]]; then continue; fi
   echo -n "[$env] "
-  wheel="${CNTK_WHEELS[$env]:?"Unknown conda env for CNTK: $env"}"
   . "$CPATH/activate" "$env"
   pkg="$(pip freeze | grep "^cntk")"
   if [[ "$pkg" != "cntk"* ]]; then echo "Installing CNTK..."; pip install "$wheel"
-  elif [[ "$pkg" = *"$CNTK_VER" ]]; then echo "CNTK is already installed."
+  elif [[ "$pkg" = *"$CNTK_VERSION" ]]; then echo "CNTK is already installed."
   else echo "Updating CNTK..."; pip install --upgrade --no-deps "$wheel"
   fi
   . "$CPATH/deactivate"
@@ -64,8 +62,8 @@ mkdir "$tmp"
 echo "Downloading materials..."
 curl $curlflags -o "$tmp/BuildArifacts.zip" "$DOWNLOAD_URL/BuildArtifacts.zip"
 curl $curlflags -o "$tmp/update_livy.py" "$DOWNLOAD_URL/update_livy.py"
-rm -rf "$SDK_DIR"; mkdir -p "$SDK_DIR"
-cd "$SDK_DIR"; unzip "$tmp/BuildArifacts.zip"; rm "$tmp/BuildArifacts.zip"
+rm -rf "$CLUSTER_SDK_DIR"; mkdir -p "$CLUSTER_SDK_DIR"
+cd "$CLUSTER_SDK_DIR"; unzip "$tmp/BuildArifacts.zip"; rm "$tmp/BuildArifacts.zip"
 
 # Change the Livy configuration
 echo "Updating the Livy configuration..."
@@ -77,7 +75,7 @@ for env in "${CONDA_ENVS[@]}"; do
   # first uninstall it, since otherwise an existing "1.2.dev3+4" version makes
   # pip consider "1.2.dev3" as "already satisfied"
   pip uninstall -y "mmlspark"
-  pip install "$PIP_PACKAGE"
+  pip install "$PIP_URL/$PIP_PACKAGE"
   . "$CPATH/deactivate"
 done
 
@@ -110,7 +108,7 @@ urlencode() {
   printf '\n'
 }
 
-for f in "$SDK_DIR/notebooks/hdinsight/"*.ipynb; do
+for f in "$CLUSTER_SDK_DIR/notebooks/hdinsight/"*.ipynb; do
   hdfs dfs -copyFromLocal "$(urlencode "$f")" "$HDFS_NOTEBOOKS_FOLDER"
 done
 

tools/config.sh
@@ -55,7 +55,7 @@ _main_url() { echo "https://$MAIN_STORAGE.azureedge.net/$1"; }
 # The base URL for our installables
 defvar INSTALLER_URL "$(_main_url "installers")"
 # Directory for caching installers; if it is empty then no caching is used
-defvar INSTALLER_CACHE_DIR "$HOME/.mmlspark_cache"
+defvar -p INSTALLER_CACHE_DIR "$HOME/.mmlspark_cache"
 
 INSTALLATIONS=(
 
@@ -155,8 +155,8 @@ SBT.setup() {
 defvar SCALA_VERSION "2.11"
 defvar SCALA_FULL_VERSION "$SCALA_VERSION.8"
 SBT.init() {
-  setenv SCALA_VERSION "$SCALA_VERSION"
-  setenv SCALA_FULL_VERSION "$SCALA_FULL_VERSION"
+  defvar -E SCALA_VERSION "$SCALA_VERSION"
+  defvar -E SCALA_FULL_VERSION "$SCALA_FULL_VERSION"
 }
 
 Spark.setup() {
@@ -238,24 +238,24 @@ CNTK.init() {
 
 # Storage for build artifacts
 defvar STORAGE_CONTAINER "buildartifacts"
-defvar STORAGE_URL "$(_main_url "$STORAGE_CONTAINER")"
+defvar -X STORAGE_URL    "$(_main_url "$STORAGE_CONTAINER")"
 
 # Container for docs and maven/pip packages
-defvar DOCS_CONTAINER  "docs"
-defvar DOCS_URL        "$(_main_url "$DOCS_CONTAINER")"
-defvar MAVEN_CONTAINER  "maven"
-defvar -x MAVEN_URL     "$(_main_url "$MAVEN_CONTAINER")"
-defvar -d MAVEN_PACKAGE "com.microsoft.ml.spark:mmlspark_$SCALA_VERSION:<{MML_VERSION}>"
-defvar PIP_CONTAINER    "pip"
-defvar -x PIP_URL       "$(_main_url "$PIP_CONTAINER")"
-defvar -d PIP_PACKAGE   "mmlspark-<{MML_VERSION}>-py2.py3-none-any.whl"
+defvar DOCS_CONTAINER    "docs"
+defvar DOCS_URL          "$(_main_url "$DOCS_CONTAINER")"
+defvar MAVEN_CONTAINER   "maven"
+defvar -xX MAVEN_URL     "$(_main_url "$MAVEN_CONTAINER")"
+defvar -dX MAVEN_PACKAGE "com.microsoft.ml.spark:mmlspark_$SCALA_VERSION:<{MML_VERSION}>"
+defvar PIP_CONTAINER     "pip"
+defvar -xX PIP_URL       "$(_main_url "$PIP_CONTAINER")"
+defvar -dX PIP_PACKAGE   "mmlspark-<{MML_VERSION}>-py2.py3-none-any.whl"
 
 # E2E test cluster information
 defvar E2E_CLUSTER_NAME   "mmlsparktest"
 defvar E2E_RESOURCE_GROUP "mmlsparktest"
 defvar E2E_CLUSTER_SSH    "spark@${E2E_CLUSTER_NAME}-ssh.azurehdinsight.net"
 defvar E2E_PARALLEL_RUNS  "2"
-defvar CLUSTER_SDK_DIR    "/mml-sdk" # this is for all clusters
+defvar -X CLUSTER_SDK_DIR "/mml-sdk" # this is for all clusters
 
 # Demo cluster information
 defvar DEMO_CLUSTER_NAME   "mmlsparkdemo"

tools/hdi/setup-test-authkey.sh
@@ -2,8 +2,9 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in project root for information.
 
-SDK_DIR="<=<=fill-in-sdk-dir=>=>"
-NB_DIR="$SDK_DIR/notebooks" # This gets created as root, need to chown to spark
+# <=<= this line is replaced with variables defined with `defvar -X` =>=>
+
+NB_DIR="$CLUSTER_SDK_DIR/notebooks" # gets created as root, need to chown to spark
 
 # This is the public key used by the build to access the test cluster
 PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0FUryXQloryZQGXVP9vOqBVsuUWihHs"

tools/misc/container-gc
@@ -26,7 +26,7 @@ GH_API="https://api.github.com/repos/Azure/mmlspark"
 github_prs="$(curl --silent --show-error "$GH_API/pulls" \
               | jq -r 'map(.number | tostring) | " " + join(" ") + " "')"
 
-vsort() { sort -t "+" -k 1,1 -V "$@"; }
+vsort() { sort -t "+" -k 1,2 -V "$@"; }
 
 now=0; _minute=$((60)); _hour=$((60*60)); _day=$((_hour*24))
 show-time() (

tools/runme/build.sh
@@ -251,13 +251,12 @@ _upload_artifacts_to_storage() {
   ( cd "$BUILD_ARTIFACTS"
     _ zip -qr9 "$tmp/$(basename "$BUILD_ARTIFACTS.zip")" * )
   local f txt
+  local varlinerx="^(.*)# +<=<= .*? =>=>(.*)\$"
   for f in "$TOOLSDIR/hdi/"*; do
     txt="$(< "$f")"
-    txt="${txt//<=<=fill-in-maven-package=>=>/com.microsoft.ml.spark:mmlspark_$SCALA_VERSION:$MML_VERSION}"
-    txt="${txt//<=<=fill-in-maven-url=>=>/$MAVEN_URL}"
-    txt="${txt//<=<=fill-in-pip-package=>=>/$PIP_URL/$PIP_PACKAGE}"
-    txt="${txt//<=<=fill-in-sdk-dir=>=>/$CLUSTER_SDK_DIR}"
-    txt="${txt//<=<=fill-in-url=>=>/$STORAGE_URL/$MML_VERSION}"
+    if [[ "$txt" =~ $varlinerx ]]; then
+      txt="${BASH_REMATCH[1]}$(_show_gen_vars)${BASH_REMATCH[2]}"
+    fi
     echo "$txt" > "$tmp/$(basename "$f")"
   done
   _ azblob upload-batch --source "$tmp" --destination "$STORAGE_CONTAINER/$MML_VERSION"

tools/runme/install.sh
@@ -46,7 +46,7 @@ _do_envinits() {
     if [[ "$cmd" = "export "*"="* ]]; then
       var="${cmd#export }"; var="${var%%=*}"; val="$(qstr "${!var}")"
     fi
-    # print only commands and setenvs that change values
+    # print only commands and var settings that change values
     if [[ "$var" = "" || "$cmd" != "export $var=$val" ]]; then show command "$cmd"; fi
   done
   eval "$script"
@@ -143,8 +143,8 @@ _install() { # libname
      || (                             " $where " != *" devel "*   ) ]]; then return
   fi
   local dir="$HOME/lib/$lib"
-  setenv "${envvar}_VERSION" "$ver"
-  setenv "${envvar}_HOME"    "$dir"
+  defvar -E "${envvar}_VERSION" "$ver"
+  defvar -xe "${envvar}_HOME"    "$dir"
   if [[ "x$prereq" != "x" ]] && ! eval "${prereq%|*}" > /dev/null 2>&1; then
     failwith "$libname: prerequisite failure: ${prereq##*|}"
   fi
@@ -157,8 +157,8 @@ _install() { # libname
   else Op="Installing"; _note_work "Install"; fi
   # avoid output up to here, so there's nothing unless we actually do something
   show section "$Op $libname v$ver in $dir"
-  show command setenv "${envvar}_VERSION" "$ver"
-  show command setenv "${envvar}_HOME"    "$dir"
+  show command export "${envvar}_VERSION=$ver"
+  show command export "${envvar}_HOME=$dir"
   if [[ "$update" = "Y" && "$(_verify_version "$libname")" = "" ]]; then
     show warning "Looks like $libname was already updated, noting new version"
     _ cd "$dir"

tools/runme/utils.sh
@@ -7,12 +7,19 @@
 # ---< defvar [opts] var val >--------------------------------------------------
 # Use this to define customize-able variables (no effect if it exists).
 # Additional arguments are concatenated (wihtout spaces) to make long value
-# settings look nice.  Use "-x" to export the variable, "-p" to resolve the
-# value to an absolute path from where we are, "-f" to set the value even if
-# it's already set.  You can also use "-d" to define values with delayed
-# references to other variables using "...<{var}>..." -- these will be replaced
-# at the end of processing the config file.
+# settings look nice.  Uses the following flags:
+# * `-x`: export the variable,
+# * `-X`: mark the variable to be included in generated script resources
+#         like the HDI script actions,
+# * `-e`: set in the user environment (in "$HOME/.mmlspark_profile"),
+# * `-E`: implies `-xXe`,
+# * `-p`: resolve the value to an absolute path from where we are,
+# * `-f`: set the value even if it's already set,
+# * `-d`: define values with delayed references to other variables using
+#         `...<{var}>...` -- these will be replaced at the end of
+#         processing the config file.
 _delayed_vars=()
+_gen_vars=()
 defvar() {
   local opts=""; while [[ "x$1" == "x-"* ]]; do opts+="${1:1}"; shift; done
   local var="$1" val v; shift
@@ -20,8 +27,17 @@ defvar() {
     val=""; for v; do val+="$v"; done; printf -v "$var" "%s" "$val"; fi
   if [[ "$opts" == *"p"* && "x${!var}" != "/"* ]]; then
     printf -v "$var" "%s" "$(realpath -m "${!var}")"; fi
-  if [[ "$opts" == *"x"* ]]; then export "$var"; fi
-  if [[ "$opts" == *"d"* ]]; then _delayed_vars+=( "$var" ); fi
+  if [[ "$opts" == *[xE]* ]]; then export "$var"; fi
+  if [[ "$opts" == *[XE]* && " ${_gen_vars[*]} " != *" $var "* ]]; then
+    _gen_vars+=( "$var" ); fi
+  if [[ "$opts" == *"d"* ]]; then
+    _delayed_vars+=( "$var" ); fi
+  if [[ "$opts" == *[eE]* ]]; then
+    envinit_commands+=("export $var=$(qstr "${!var}")"); fi
+}
+_show_gen_vars() {
+  local var
+  for var in "${_gen_vars[@]}"; do printf '%s=%s\n' "$var" "$(qstr "${!var}")"; done
 }
 _replace_delayed_vars() {
   local var val pfx sfx change=1
@@ -322,10 +338,6 @@ set_install_info_vars() {
 envinit_commands=('export MMLSPARK_PROFILE="yes"')
 envinit_eval() { envinit_commands+=("$*"); eval "$*"; }
 
-# ---< setenv var val >---------------------------------------------------------
-# Set an environment variable; include the setting in the user environment too.
-setenv() { envinit_eval "export $1=$(qstr "$2")"; }
-
 # ---< get_runtime_hash >-------------------------------------------------------
 # Prints out a hash of the currently configured runtime environment.  The hash
 # depends on the relevant bits of configuration, including a .setup and .init
@@ -463,9 +475,9 @@ _set_build_info() {
     #
     cd "$owd"
   fi
-  defvar -x MML_VERSION    "$version"
-  defvar -x MML_BUILD_INFO "$info"
-  defvar -x MML_LATEST     "$is_latest"
+  defvar -xX MML_VERSION    "$version"
+  defvar -xX MML_BUILD_INFO "$info"
+  defvar -xX MML_LATEST     "$is_latest"
 }
 # To be called when re-creating $BUILD_ARTIFACTS
 _reset_build_info() {

commit 1e2dde02cb50377ed6bc882e54f29811472055d7

Fix an issue introduced in f1704d3
Forgot to add the `$wheel` assignment; the problem was not reached since
the test cluster already has CNTK installed.

tools/hdi/install-mmlspark.sh
@@ -42,9 +42,10 @@ apt-get install -y openmpi-bin libunwind8 libpng12-0 libjasper1
 
 # Install CNTK in all environments
 for env in "${CONDA_ENVS[@]}"; do
-  # wheel="${CNTK_WHEELS[$env]:?"Unknown conda env for CNTK: $env"}"
+  # local wheel="${CNTK_WHEELS[$env]:?"Unknown conda env for CNTK: $env"}"
   # ... but hdi will have additional environments, created by vienna
-  if [[ -z "${CNTK_WHEELS[$env]}" ]]; then continue; fi
+  local wheel="${CNTK_WHEELS[$env]}"
+  if [[ -z "$wheel" ]]; then continue; fi
   echo -n "[$env] "
   . "$CPATH/activate" "$env"
   pkg="$(pip freeze | grep "^cntk")"