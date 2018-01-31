4c3e22a59bc4b54b1c34b5989872508c4698ef5e
dciborow <dciborow@microsoft.com>
2018-01-31 22:13:50 +0000

    Adding DataFrame Param Type

--- src/msft-recommendation/src/main/scala/MsftRecomendationModel.scala
+++ src/msft-recommendation/src/main/scala/MsftRecomendationModel.scala
@@ -5,7 +5,7 @@ package com.microsoft.ml.spark
 
 import org.apache.spark.ml.Model
 import org.apache.spark.ml.param.ParamMap
-import org.apache.spark.ml.recommendation.{ALSModel, MsftRecHelper, MsftRecommendationModelParams}
+import org.apache.spark.ml.recommendation.{ALSModel, MsftRecommendationModelParams}
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, Dataset}
@@ -39,7 +39,7 @@ class MsftRecommendationModel(
     *         stored as an array of (itemCol: Int, rating: Float) Rows.
     */
   override def recommendForAllUsers(numItems: Int): DataFrame = {
-    MsftRecHelper.recommendForAllUsers(alsModel, numItems)
+    recommendForAllUsers(alsModel, numItems)
   }
 
   /**
@@ -50,7 +50,7 @@ class MsftRecommendationModel(
     *         stored as an array of (userCol: Int, rating: Float) Rows.
     */
   override def recommendForAllItems(numUsers: Int): DataFrame = {
-    MsftRecHelper.recommendForAllItems(alsModel, numUsers)
+    recommendForAllItems(alsModel, numUsers)
   }
 
   override def copy(extra: ParamMap): MsftRecommendationModel = {
