ba29087aed3fba7416f33c06b48820c5b7fc8055
Eli Barzilay <eli@barzilay.org>
2018-02-10 08:10:31 -0500

    Fix a minor copyright divernce

--- src/codegen/src/main/scala/Config.scala
+++ src/codegen/src/main/scala/Config.scala
@@ -36,7 +36,7 @@ object Config {
 
   val copyrightLines =
     s"""|# Copyright (C) Microsoft Corporation. All rights reserved.
-        |# Licensed under the MIT License. See LICENSE in the project root for information.
+        |# Licensed under the MIT License. See LICENSE in project root for information.
         |""".stripMargin
 
 }
