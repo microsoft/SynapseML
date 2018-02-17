fbd02d77b8524f1ebb2b1ced068d05bfbfa60634
Mark Hamilton <marhamil@microsoft.com>
2018-02-16 22:42:06 -0500

    Add a DataType Param

---
+++ src/core/serialize/src/main/scala/params/DataTypeParam.scala
@@ -0,0 +1,15 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in project root for information.
+
+package org.apache.spark.ml.param
+
+import org.apache.spark.sql.types.DataType
+
+/** Param for DataType */
+class DataTypeParam(parent: Params, name: String, doc: String, isValid: DataType => Boolean)
+    extends ComplexParam[DataType](parent, name, doc, isValid) {
+
+  def this(parent: Params, name: String, doc: String) =
+    this(parent, name, doc, ParamValidators.alwaysTrue)
+
+}
