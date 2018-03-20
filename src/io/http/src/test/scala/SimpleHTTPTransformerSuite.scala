e3b754598a5a46c32dc3fb524d63d28082112c4a
Eli Barzilay <eli@barzilay.org>
2018-03-20 17:17:35 -0400

    Improve the HTTPTransformerSuite.scala tests

--- src/io/http/src/test/scala/SimpleHTTPTransformerSuite.scala
+++ src/io/http/src/test/scala/SimpleHTTPTransformerSuite.scala
@@ -7,36 +7,40 @@ import org.apache.spark.ml.util.MLReadable
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.{StringType, StructType}
 
-class SimpleHTTPTransformerSuite extends TransformerFuzzing[SimpleHTTPTransformer] with WithServer {
+class SimpleHTTPTransformerSuite
+    extends TransformerFuzzing[SimpleHTTPTransformer] with WithServer {
 
-  import ServerUtils._
   import session.implicits._
   val df: DataFrame = sc.parallelize((1 to 10).map(Tuple1(_))).toDF("data")
 
-  val simpleTransformer: SimpleHTTPTransformer = new SimpleHTTPTransformer()
-    .setInputCol("data")
-    .setOutputParser(new JSONOutputParser().setDataType(new StructType().add("foo", StringType)))
-    .setUrl(url)
-    .setOutputCol("results")
+  def simpleTransformer: SimpleHTTPTransformer =
+    new SimpleHTTPTransformer()
+      .setInputCol("data")
+      .setOutputParser(new JSONOutputParser()
+                         .setDataType(new StructType().add("foo", StringType)))
+      .setUrl(getUrl)
+      .setOutputCol("results")
 
   test("HttpTransformerTest") {
     val results = simpleTransformer.transform(df).collect
     assert(results.length==10)
-    assert(results.forall(_.getStruct(1).getString(0)=="here"))
+    assert(results.forall(_.getStruct(1).getString(0) == "more blah"))
     assert(results(0).schema.fields.length==2)
   }
 
   test("Concurrent HttpTransformerTest") {
-    val results =  new SimpleHTTPTransformer()
-      .setInputCol("data")
-      .setOutputParser(new JSONOutputParser().setDataType(new StructType().add("foo", StringType)))
-      .setUrl(url)
-      .setOutputCol("results")
-      .setConcurrency(3)
-      .transform(df)
-      .collect
+    val results =
+      new SimpleHTTPTransformer()
+        .setInputCol("data")
+        .setOutputParser(new JSONOutputParser()
+                           .setDataType(new StructType().add("foo", StringType)))
+        .setUrl(getUrl)
+        .setOutputCol("results")
+        .setConcurrency(3)
+        .transform(df)
+        .collect
     assert(results.length==10)
-    assert(results.forall(_.getStruct(1).getString(0)=="here"))
+    assert(results.forall(_.getStruct(1).getString(0) == "more blah"))
     assert(results(0).schema.fields.length==2)
   }
 
