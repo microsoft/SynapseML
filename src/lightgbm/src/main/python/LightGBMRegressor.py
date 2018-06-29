d43deeeb302b0c0d3d9aa5e3a289b5aaadf118b2
Ilya Matiach <ilmat@microsoft.com>
2018-06-29 18:39:03 -0400

    Adding feature importances to LightGBM

---
+++ src/lightgbm/src/main/python/LightGBMRegressor.py
@@ -0,0 +1,38 @@
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+
+import sys
+from pyspark import SQLContext
+from pyspark import SparkContext
+
+if sys.version >= '3':
+    basestring = str
+
+from mmlspark._LightGBMRegressor import _LightGBMRegressor
+from mmlspark._LightGBMRegressor import M
+from pyspark.ml.common import inherit_doc
+
+@inherit_doc
+class LightGBMRegressor(_LightGBMRegressor):
+    def _create_model(self, java_model):
+        model = LightGBMRegressionModel()
+        model._java_obj = java_model
+        model._transfer_params_from_java()
+        return model
+
+@inherit_doc
+class LightGBMRegressionModel(M):
+    def saveNativeModel(self, sparkSession, filename):
+        """
+        Save the booster as string format to a local or WASB remote location.
+        """
+        ctx = SparkContext.getOrCreate()
+        sql_ctx = SQLContext.getOrCreate(ctx)
+        jsession = sql_ctx.sparkSession._jsparkSession
+        self._java_obj.saveNativeModel(jsession, filename)
+
+    def getFeatureImportances(self, importance_type="split"):
+        """
+        Get the feature importances.  The importance_type can be "split" or "gain".
+        """
+        self._java_obj.getFeatureImportances(importance_type)
