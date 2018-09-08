bbf32d293eb1e6562f2e48a9e69854606e8caf06
Mark Hamilton <marhamil@microsoft.com>
2018-09-08 21:30:05 +0000

    Finish Bing Image Search and add tests

--- src/io/http/src/main/python/BingImageSearch.py
+++ src/io/http/src/main/python/BingImageSearch.py
@@ -13,40 +13,20 @@ from pyspark.ml.common import inherit_doc
 @inherit_doc
 class BingImageSearch(_BingImageSearch):
 
-    def setMiniBatchSize(self, n):
-        self._java_obj = self._java_obj.setMiniBatchSize(n)
+    def setQuery(self, value):
+        self._java_obj = self._java_obj.setQuery(value)
         return self
 
-    def setOffsetCol(self, v):
-        self._java_obj = self._java_obj.setOffsetCol(v)
+    def setQueryCol(self, value):
+        self._java_obj = self._java_obj.setQueryCol(value)
         return self
 
-    def setOffset(self, v):
-        self._java_obj = self._java_obj.setOffset(v)
+    def setMarket(self, value):
+        self._java_obj = self._java_obj.setMarket(value)
         return self
 
-    def setQuery(self, v):
-        self._java_obj = self._java_obj.setQuery(v)
-        return self
-
-    def setQueryCol(self, v):
-        self._java_obj = self._java_obj.setQueryCol(v)
-        return self
-
-    def setCount(self, v):
-        self._java_obj = self._java_obj.setCount(v)
-        return self
-
-    def setCountCol(self, v):
-        self._java_obj = self._java_obj.setCountCol(v)
-        return self
-
-    def setImageType(self, v):
-        self._java_obj = self._java_obj.setImageType(v)
-        return self
-
-    def setImageTypeCol(self, v):
-        self._java_obj = self._java_obj.setImageTypeCol(v)
+    def setMarketCol(self, value):
+        self._java_obj = self._java_obj.setMarketCol(value)
         return self
 
     @staticmethod
@@ -55,3 +35,10 @@ class BingImageSearch(_BingImageSearch):
         from pyspark.sql import SparkSession
         bis = SparkSession.builder.getOrCreate()._jvm.com.microsoft.ml.spark.BingImageSearch
         return Lambda._from_java(bis.getUrlTransformer(imageCol,urlCol))
+
+    @staticmethod
+    def downloadFromUrls(cls, pathCol, bytesCol, concurrency, timeout):
+        from mmlspark import BingImageSearch
+        from pyspark.sql import SparkSession
+        bis = SparkSession.builder.getOrCreate()._jvm.com.microsoft.ml.spark.BingImageSearch
+        return Lambda._from_java(bis.downloadFromUrls(pathCol, bytesCol, concurrency, timeout))
