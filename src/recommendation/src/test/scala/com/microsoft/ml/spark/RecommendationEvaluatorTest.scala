78ebb97294e03e72243c5164710c4435149d1626
Daniel Ciborowski <dciborow@microsoft.com>
2018-09-24 18:34:57 -0400

    Update RecommendationEvaluatorTest.scala

--- src/recommendation/src/test/scala/com/microsoft/ml/spark/RecommendationEvaluatorTest.scala
+++ src/recommendation/src/test/scala/com/microsoft/ml/spark/RecommendationEvaluatorTest.scala
@@ -89,22 +89,4 @@ class RecommendationEvaluatorTest extends TestBase {
     assert(map("ndcgAt") == 1.0)
     assert(map("precisionAtk") == 0.5)
   }
-
-//  override def serializationTestObjects(): Seq[TestObject[RecommendationEvaluator]] = {
-//    val seq = Seq((Array(1, 2, 3, 4, 5, 6), Array(1, 2, 3)))
-//    import session.implicits._
-//    val df = session.createDataset(seq).toDF("prediction", "label")
-//
-//    val evaluator = new RecommendationEvaluator()
-//      .setK(6)
-//      .setSaveAll(true)
-//
-//    evaluator.setNItems(6)
-//
-//    List(new TestObject(evaluator, df))
-//  }
-
-//  override def reader: MLReadable[_] = RecommendationEvaluator
-
-//  override def modelReader: MLReadable[_] = RecommendationEvaluator
 }
