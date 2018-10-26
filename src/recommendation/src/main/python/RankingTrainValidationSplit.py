eee977529336848cbc9de9bce557216fa996965c
dciborow <dciborow@microsoft.com>
2018-10-26 17:42:03 +0000

    Added subMetrics in Python

--- src/recommendation/src/main/python/RankingTrainValidationSplit.py
+++ src/recommendation/src/main/python/RankingTrainValidationSplit.py
@@ -199,6 +199,7 @@ class RankingTrainValidationSplit(Estimator, ValidatorParams, HasCollectSubModel
         super(RankingTrainValidationSplit, self).__init__()
         kwargs = self._input_kwargs
         self._set(**kwargs)
+        self.java = java
 
     @keyword_only
     def setParams(self, estimator=None, estimatorParamMaps=None, evaluator=None, seed=None):
