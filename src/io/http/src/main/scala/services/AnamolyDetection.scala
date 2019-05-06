a91316c4263d21e7225b9f8278ec73dae06e16a0
Mark Hamilton <marhamil@microsoft.com>
2019-05-06 17:03:32 +0000

    Add simple Anamoly Detector

--- src/io/http/src/main/scala/services/AnamolyDetection.scala
+++ src/io/http/src/main/scala/services/AnamolyDetection.scala
@@ -5,33 +5,20 @@ package com.microsoft.ml.spark
 
 import com.microsoft.ml.spark.cognitive._
 import org.apache.http.entity.{AbstractHttpEntity, StringEntity}
-import org.apache.spark.ml.param.ServiceParam
+import org.apache.spark.ml.param.{Param, ServiceParam}
 import org.apache.spark.ml.util._
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.sql.types._
 import AnomalyDetectorProtocol._
-import org.apache.spark.sql.functions.udf
+import com.microsoft.ml.spark.schema.DatasetExtensions
+import org.apache.spark.sql.functions.{col, collect_list, explode, struct, udf, arrays_zip, size}
 import spray.json._
 import spray.json.DefaultJsonProtocol._
 
 import scala.language.existentials
 
 abstract class AnomalyDetectorBase(override val uid: String) extends CognitiveServicesBase(uid)
-with HasCognitiveServiceInput with HasInternalJsonOutputParser {
-  val series = new ServiceParam[Seq[TimeSeriesPoint]](this, "series",
-    """
-      |Time series data points. Points should be sorted by timestamp in ascending order
-      |to match the anomaly detection result. If the data is not sorted correctly or
-      |there is duplicated timestamp, the API will not work.
-      |In such case, an error message will be returned.
-    """.stripMargin.replace("\n", ""),
-    { _ => true },
-    isRequired = true
-  )
-
-  def setSeries(v: Seq[TimeSeriesPoint]): this.type = setScalarParam(series, v)
-
-  def setSeriesCol(v: String): this.type = setVectorParam(series, v)
+  with HasCognitiveServiceInput with HasInternalJsonOutputParser {
 
   val granularity = new ServiceParam[String](this, "granularity",
     """
@@ -97,6 +84,17 @@ with HasCognitiveServiceInput with HasInternalJsonOutputParser {
 
   def setPeriodCol(v: String): this.type = setVectorParam(period, v)
 
+  val series = new ServiceParam[Seq[TimeSeriesPoint]](this, "series",
+    """
+      |Time series data points. Points should be sorted by timestamp in ascending order
+      |to match the anomaly detection result. If the data is not sorted correctly or
+      |there is duplicated timestamp, the API will not work.
+      |In such case, an error message will be returned.
+    """.stripMargin.replace("\n", ""),
+    { _ => true },
+    isRequired = true
+  )
+
   override protected def prepareEntity: Row => Option[AbstractHttpEntity] = { row =>
     Some(new StringEntity(ADRequest(
       getValueAny(row, series).asInstanceOf[Seq[Any]].map {
@@ -119,6 +117,10 @@ class DetectLastAnomaly(override val uid: String) extends AnomalyDetectorBase(ui
 
   def this() = this(Identifiable.randomUID("DetectLastAnomoly"))
 
+  def setSeries(v: Seq[TimeSeriesPoint]): this.type = setScalarParam(series, v)
+
+  def setSeriesCol(v: String): this.type = setVectorParam(series, v)
+
   def setLocation(v: String): this.type =
     setUrl(s"https://$v.api.cognitive.microsoft.com/anomalydetector/v1.0/timeseries/last/detect")
 
@@ -132,9 +134,111 @@ class DetectAnomalies(override val uid: String) extends AnomalyDetectorBase(uid)
 
   def this() = this(Identifiable.randomUID("DetectAnomalies"))
 
+  def setSeries(v: Seq[TimeSeriesPoint]): this.type = setScalarParam(series, v)
+
+  def setSeriesCol(v: String): this.type = setVectorParam(series, v)
+
   def setLocation(v: String): this.type =
     setUrl(s"https://$v.api.cognitive.microsoft.com/anomalydetector/v1.0/timeseries/entire/detect")
 
   override def responseDataType: DataType = ADEntireResponse.schema
 
+}
+
+object SimpleDetectAnomalies extends ComplexParamsReadable[SimpleDetectAnomalies] with Serializable
+
+class SimpleDetectAnomalies(override val uid: String) extends AnomalyDetectorBase(uid)
+  with HasOutputCol {
+
+  def this() = this(Identifiable.randomUID("SimpleDetectAnomalies"))
+
+  val timestampCol = new Param[String](this, "timestampCol", "column representing the time of the series")
+
+  def setTimestampCol(v: String): this.type = set(timestampCol, v)
+
+  def getTimestampCol: String = $(timestampCol)
+
+  val valueCol = new Param[String](this, "valueCol", "column representing the value of the series")
+
+  def setValueCol(v: String): this.type = set(valueCol, v)
+
+  def getValueCol: String = $(valueCol)
+
+  val groupbyCol = new Param[String](this, "groupbyCol", "column that groups the series")
+
+  def setGroupbyCol(v: String): this.type = set(groupbyCol, v)
+
+  def getGroupbyCol: String = $(groupbyCol)
+
+  setDefault(
+    timestampCol -> "timestamp",
+    valueCol -> "value",
+    outputCol -> s"${uid}_output")
+
+  private def sortWithContext(timeSeries: Seq[Row], context: Seq[Row]): Row = {
+    val s1 = timeSeries.zipWithIndex.sortBy(r => r._1.getString(0))
+    val s2 = s1.map(s => context(s._2))
+    Row(s1.map(_._1), s2)
+  }
+
+  private def formatResultsFunc(): (Row, Int) => Seq[Row] = {
+    val fromRow = ADEntireResponse.makeFromRowConverter
+    val toRow = ADSingleResponse.makeToRowConverter;
+    { case (result, count) =>
+      Option(result)
+        .map(res => fromRow(res).explode.map(toRow))
+        .getOrElse(Seq.fill[Row](count)(null))
+    }
+  }
+
+  override def transform(dataset: Dataset[_]): DataFrame = {
+    val contextCol = DatasetExtensions.findUnusedColumnName("context", dataset.schema)
+    val inputsCol = DatasetExtensions.findUnusedColumnName("inputs", dataset.schema)
+    setVectorParam(series, inputsCol)
+
+    val inputDF = dataset.toDF()
+      .withColumn(contextCol, struct("*"))
+      .withColumn(inputsCol, struct(
+        col(getTimestampCol).alias("timestamp"),
+        col(getValueCol).alias("value")))
+    val sortUDF = udf(sortWithContext _,
+      new StructType()
+        .add(inputsCol, ArrayType(TimeSeriesPoint.schema))
+        .add(contextCol, ArrayType(inputDF.schema(contextCol).dataType))
+    )
+
+    val groupedDF = inputDF
+      .groupBy(getGroupbyCol)
+      .agg(
+        collect_list(inputsCol).alias(inputsCol),
+        collect_list(contextCol).alias(contextCol))
+      .select(sortUDF(col(inputsCol), col(contextCol)).alias("sorted"))
+      .select("sorted.*")
+
+    val outputDF = super.transform(groupedDF)
+
+    outputDF.select(
+      col(getErrorCol),
+      explode(arrays_zip(
+        col(contextCol),
+        udf(formatResultsFunc(), ArrayType(ADSingleResponse.schema))(
+          col(getOutputCol), size(col(contextCol))).alias(getOutputCol)
+      )).alias(getOutputCol)
+    ).select(
+      s"$getOutputCol.$contextCol.*",
+      getErrorCol,
+      s"$getOutputCol.1"
+    ).withColumnRenamed("1", getOutputCol)
+
+  }
+
+  def setLocation(v: String): this.type =
+    setUrl(s"https://$v.api.cognitive.microsoft.com/anomalydetector/v1.0/timeseries/entire/detect")
+
+  override def responseDataType: DataType = ADEntireResponse.schema
+
+  override def transformSchema(schema: StructType): StructType = {
+    schema.add(getErrorCol, ErrorUtils.errorSchema)
+      .add(getOutputCol, ADSingleResponse.schema)
+  }
 }
