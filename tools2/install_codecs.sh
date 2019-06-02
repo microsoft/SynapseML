555378d4efdb256085bbd049b79823b4fb76bbb3
marhamil723 <mhamilton723@gmail.com>
2019-06-02 19:31:04 -0400

    attempt to install codecs

---
+++ tools2/install_codecs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y debconf-utils
+
+# create answer file
+sudo debconf-set-selections <<\EOF
+msttcorefonts msttcorefonts/defoma note
+ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula boolean true
+ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note
+EOF
+
+sudo apt-get install -y ubuntu-restricted-extras
