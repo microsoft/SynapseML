c26828bec7e94bfd608c34c692ac2a64d35eb701
Markus Cozowicz <marcozo@microsoft.com>
2019-09-16 14:47:14 +0200

    fix python test

--- src/test/python/mmlspark/vw/test_vw.py
+++ src/test/python/mmlspark/vw/test_vw.py
@@ -5,6 +5,7 @@ import tempfile
 import pyspark
 
 from mmlspark.vw.VowpalWabbitClassifier import VowpalWabbitClassifier
+from mmlspark.vw.VowpalWabbitRegressor import VowpalWabbitRegressor
 from mmlspark.vw.VowpalWabbitFeaturizer import VowpalWabbitFeaturizer
 
 from pyspark.sql.types import *
@@ -21,7 +22,7 @@ sc = spark.sparkContext
 
 class VowpalWabbitClassificationSpec(unittest.TestCase):
 
-    def test_save_model_classification(self):
+    def save_model(self, estimator):
         # create sample data
         schema = StructType([StructField("label", DoubleType()),
                               StructField("text", StringType())])
@@ -35,8 +36,7 @@ class VowpalWabbitClassificationSpec(unittest.TestCase):
         featurized_data = featurizer.transform(data)
 
         # train model
-        vw = VowpalWabbitClassifier()
-        model = vw.fit(featurized_data)
+        model = estimator.fit(featurized_data)
 
         # write model to file and validate it's there
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -44,32 +44,13 @@ class VowpalWabbitClassificationSpec(unittest.TestCase):
 
             model.saveNativeModel(modelFile)
 
-            self.assertTrue(os.stat(modelFile) > 0)
+            self.assertTrue(os.stat(modelFile).st_size > 0)
+
+    def test_save_model_classification(self):
+        self.save_model(VowpalWabbitClassifier())
 
     def test_save_model_regression(self):
-        # create sample data
-        schema = StructType([StructField("label", DoubleType()),
-                              StructField("text", StringType())])
-
-        data = pyspark.sql.SparkSession.builder.getOrCreate().createDataFrame([
-            (-1.0, "mountains are nice"),
-            ( 1.0, "do you have the TPS reports ready?")], schema)
-
-        # featurize data
-        featurizer = VowpalWabbitFeaturizer(stringSplitInputCols=['text'])
-        featurized_data = featurizer.transform(data)
-
-        # train model
-        vw = VowpalWabbitRegressor()
-        model = vw.fit(featurized_data)
-
-        # write model to file and validate it's there
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            modelFile = '{}/model'.format(tmpdirname)
-
-            model.saveNativeModel(modelFile)
-
-            self.assertTrue(os.stat(modelFile) > 0)
+        self.save_model(VowpalWabbitRegressor())
 
 if __name__ == "__main__":
     result = unittest.main()
