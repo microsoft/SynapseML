dbb281821542dff82386e6855e807d2e906c11bf
Joan Fontanals <jfontanalsmartinez@gmail.com>
2020-01-28 17:08:41 +0100

    feat: Expose parameter bin_construct_sample_cnt in spark for LightGBM (#780)
    * Expose parameter bin_construct_sample_cnt in spark for LightGBM
    
    * Correct merge issues
    
    Co-authored-by: Ilya Matiach <ilmat@microsoft.com>

--- src/main/scala/com/microsoft/ml/spark/lightgbm/LightGBMParams.scala
+++ src/main/scala/com/microsoft/ml/spark/lightgbm/LightGBMParams.scala
@@ -55,6 +55,22 @@ trait LightGBMExecutionParams extends Wrappable {
   def setNumBatches(value: Int): this.type = set(numBatches, value)
 }
 
+/** Defines common parameters across all LightGBM learners related to histogram bin construction.
+  */
+trait LightGBMBinParams extends Wrappable {
+  val maxBin = new IntParam(this, "maxBin", "Max bin")
+  setDefault(maxBin -> 255)
+
+  def getMaxBin: Int = $(maxBin)
+  def setMaxBin(value: Int): this.type = set(maxBin, value)
+
+  val binSampleCount = new IntParam(this, "binSampleCount", "Number of samples considered at computing histogram bins")
+  setDefault(binSampleCount -> 200000)
+
+  def getBinSampleCount: Int = $(binSampleCount)
+  def setBinSampleCount(value: Int): this.type = set(binSampleCount, value)
+}
+
 /** Defines parameters for slots across all LightGBM learners.
  */
 trait LightGBMSlotParams extends Wrappable {
@@ -115,7 +131,7 @@ trait LightGBMFractionParams extends Wrappable {
   */
 trait LightGBMParams extends Wrappable with DefaultParamsWritable with HasWeightCol
   with HasValidationIndicatorCol with HasInitScoreCol with LightGBMExecutionParams
-  with LightGBMSlotParams with LightGBMFractionParams {
+  with LightGBMSlotParams with LightGBMFractionParams with LightGBMBinParams{
   val numIterations = new IntParam(this, "numIterations",
     "Number of iterations, LightGBM constructs num_class * num_iterations trees")
   setDefault(numIterations->100)
@@ -143,12 +159,6 @@ trait LightGBMParams extends Wrappable with DefaultParamsWritable with HasWeight
 
   def getObjective: String = $(objective)
   def setObjective(value: String): this.type = set(objective, value)
-
-  val maxBin = new IntParam(this, "maxBin", "Max bin")
-  setDefault(maxBin -> 255)
-
-  def getMaxBin: Int = $(maxBin)
-  def setMaxBin(value: Int): this.type = set(maxBin, value)
 
   val baggingFreq = new IntParam(this, "baggingFreq", "Bagging frequency")
   setDefault(baggingFreq->0)
