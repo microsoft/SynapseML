7cfbbf56c92d1146756aefa19b307c34bee24554
Lucy Zhang <zhangly@mit.edu>
2020-01-31 13:20:46 -0500

    Delete make_config.py

--- amltest/deployment_test/make_config.py
+++
@@ -1,6 +0,0 @@
-from azureml.core.compute import ComputeTarget, AksCompute
-
-cluster_name = 'cpu-cluster'
-compute_config = AksCompute.provisioning_configuration(location='eastus')
-production_cluster = ComputeTarget.create(ws, cluster_name, compute_config)
-production_cluster.wait_for_completion(show_output=True)
