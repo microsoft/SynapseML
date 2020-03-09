ced6463d0f45ac3d8c8da0c2b215d900fc62c2b0
rolevin <rolevin@microsoft.com>
2020-03-09 20:37:54 +0200

    adding a cyber and cyberml pillar to MMLSpark as discussed

---
+++ src/test/python/mmlspark/cyber/ml/feature_engineering/test_indexers.py
@@ -0,0 +1,146 @@
+__author__ = 'rolevin'
+
+from typing import Type
+
+from pyspark import SQLContext
+
+from pyspark.sql import functions as f, types as t
+from mmlspark.cyber.ml.feature_engineering import indexers
+
+from ..explain_tester import ExplainTester
+
+
+class TestIndexers:
+    def create_sample_dataframe(self, spark_context: SQLContext):
+        schema = t.StructType(
+            [
+                t.StructField("tenant", t.StringType(), nullable=True),
+                t.StructField("user", t.StringType(), nullable=True),
+                t.StructField("res", t.StringType(), nullable=True),
+                t.StructField("expected_uid", t.IntegerType(), nullable=True),
+                t.StructField("expected_rid", t.IntegerType(), nullable=True)
+            ]
+        )
+
+        return spark_context.createDataFrame(
+            [
+                ('1', 'a', 'A', 1, 1),
+                ('1', 'b', 'A', 2, 1),
+                ('1', 'a', 'B', 1, 2),
+
+                ('2', 'aa', 'AA', 1, 1),
+                ('2', 'bb', 'AA', 2, 1),
+
+                ('3', 'b', 'B', 1, 1)
+            ],
+            schema
+        )
+
+    def test_id_indexer(self, spark_context: SQLContext):
+        indexer = indexers.IdIndexer('user', 'tenant', 'actual_uid', True)
+
+        df = self.create_sample_dataframe(spark_context)
+        model = indexer.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.count() == df.count()
+
+        assert 0 == new_df.filter(
+            f.col('expected_uid') != f.col('actual_uid')
+        ).count()
+
+    def test_multi_indexer(self, spark_context: SQLContext):
+        multi_indexer = indexers.MultiIndexer([
+            indexers.IdIndexer('user', 'tenant', 'actual_uid', True),
+            indexers.IdIndexer('res', 'tenant', 'actual_rid', True),
+        ])
+
+        df = self.create_sample_dataframe(spark_context)
+        model = multi_indexer.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.count() == df.count()
+        assert new_df.filter(f.col('actual_uid') <= 0).count() == 0
+        assert new_df.filter(f.col('actual_rid') <= 0).count() == 0
+
+        assert 0 == new_df.filter(
+            f.col('expected_uid') != f.col('actual_uid')
+        ).count()
+
+        assert 0 == new_df.filter(
+            f.col('expected_rid') != f.col('actual_rid')
+        ).count()
+
+    def test_multi_indexer_undo_transform(self, spark_context: SQLContext):
+        multi_indexer = indexers.MultiIndexer([
+            indexers.IdIndexer('user', 'tenant', 'actual_uid', True),
+            indexers.IdIndexer('res', 'tenant', 'actual_rid', True),
+        ])
+
+        df = self.create_sample_dataframe(spark_context)
+        model = multi_indexer.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.filter(f.col('actual_uid') <= 0).count() == 0
+        assert new_df.filter(f.col('actual_rid') <= 0).count() == 0
+
+        orig_df = model.undo_transform(new_df.select(
+            'tenant', 'actual_uid', 'actual_rid'
+        ))
+
+        assert orig_df.select(
+            'tenant', 'user'
+        ).distinct().orderBy('tenant', 'user').collect() == df.select(
+            'tenant', 'user'
+        ).distinct().orderBy('tenant', 'user').collect()
+
+        assert orig_df.select(
+            'tenant', 'res'
+        ).distinct().orderBy('tenant', 'res').collect() == df.select(
+            'tenant', 'res'
+        ).distinct().orderBy('tenant', 'res').collect()
+
+    def test_multi_indexer_non_per_tenant(self, spark_context: SQLContext):
+        multi_indexer = indexers.MultiIndexer([
+            indexers.IdIndexer('user', 'tenant', 'actual_uid', False),
+            indexers.IdIndexer('res', 'tenant', 'actual_rid', False)
+        ])
+
+        df = self.create_sample_dataframe(spark_context)
+        model = multi_indexer.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.count() == df.count()
+        assert new_df.filter(f.col('actual_uid') <= 0).count() == 0
+        assert new_df.filter(f.col('actual_rid') <= 0).count() == 0
+
+        user_count = df.select('tenant', 'user').distinct().count()
+        res_count = df.select('tenant', 'res').distinct().count()
+
+        assert new_df.select('actual_uid').distinct().count() == user_count
+        assert new_df.select('actual_rid').distinct().count() == res_count
+
+        stats_row = new_df.select(
+            f.min('actual_uid').alias('min_uid'),
+            f.max('actual_uid').alias('max_uid'),
+            f.min('actual_rid').alias('min_rid'),
+            f.max('actual_rid').alias('max_rid')
+        ).first()
+
+        assert (stats_row['min_uid'] == 1) and (stats_row['max_uid'] == user_count)
+        assert (stats_row['min_rid'] == 1) and (stats_row['max_rid'] == res_count)
+
+        orig_df = model.undo_transform(new_df).select('tenant', 'user', 'res').orderBy('tenant', 'user', 'res')
+
+        assert df.select('tenant', 'user', 'res').orderBy('tenant', 'user', 'res').collect() == orig_df.collect()
+
+
+class TestIdIndexerExplain(ExplainTester):
+    def test_explain(self):
+        types = [str, bool]
+
+        def counts(c: int, tt: Type):
+            return tt not in types or c > 0
+
+        params = ['inputCol', 'partitionKey', 'outputCol', 'resetPerPartition']
+        self.check_explain(indexers.IdIndexer('input', 'tenant', 'output', True), params, counts)
---
+++ src/test/python/mmlspark/cyber/ml/feature_engineering/test_scalers.py
@@ -0,0 +1,135 @@
+__author__ = 'rolevin'
+
+from typing import Type
+
+from pyspark import SQLContext
+
+from pyspark.sql import functions as f, types as t
+from mmlspark.cyber.ml.feature_engineering import scalers
+
+from ..explain_tester import ExplainTester
+
+
+class TestScalers:
+    def create_sample_dataframe(self, spark_context: SQLContext):
+        schema = t.StructType(
+            [
+                t.StructField("tenant", t.StringType(), nullable=True),
+                t.StructField("name", t.StringType(), nullable=True),
+                t.StructField("score", t.FloatType(), nullable=True)
+            ]
+        )
+
+        return spark_context.createDataFrame(
+            [
+                ("t1", "5", 500.0),
+                ("t1", "6", 600.0),
+                ("t2", "7", 700.0),
+                ("t2", "8", 800.0),
+                ("t3", "9", 900.0)
+            ],
+            schema
+        )
+
+    def test_unpartitioned_min_max_scaler(self, spark_context: SQLContext):
+        ls = scalers.LinearScalarScaler('score', None, 'new_score', 5, 9, use_pandas=False)
+
+        df = self.create_sample_dataframe(spark_context)
+        model = ls.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.count() == df.count()
+
+        assert 0 == new_df.filter(
+            f.col('name').cast(t.IntegerType()) != f.col('new_score').cast(t.IntegerType())
+        ).count()
+
+    def test_partitioned_min_max_scaler(self, spark_context: SQLContext):
+        ls = scalers.LinearScalarScaler('score', 'tenant', 'new_score', 1, 2, use_pandas=False)
+
+        df = self.create_sample_dataframe(spark_context)
+        model = ls.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.count() == df.count()
+
+        t1_arr = new_df.filter(f.col('tenant') == 't1').orderBy('new_score').collect()
+        assert len(t1_arr) == 2
+        assert t1_arr[0]['new_score'] == 1.0
+        assert t1_arr[1]['new_score'] == 2.0
+
+        t2_arr = new_df.filter(f.col('tenant') == 't2').orderBy('new_score').collect()
+        assert len(t2_arr) == 2
+        assert t2_arr[0]['new_score'] == 1.0
+        assert t2_arr[1]['new_score'] == 2.0
+
+        t3_arr = new_df.filter(f.col('tenant') == 't3').orderBy('new_score').collect()
+        assert len(t3_arr) == 1
+        # this is the average between min and max
+        assert t3_arr[0]['new_score'] == 1.5
+
+    def test_unpartitioned_standard_scaler(self, spark_context: SQLContext):
+        ls = scalers.StandardScalarScaler('score', None, 'new_score', 1.0, use_pandas=False)
+
+        df = self.create_sample_dataframe(spark_context)
+        model = ls.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.count() == df.count()
+
+        new_scores = new_df.toPandas()['new_score']
+
+        assert new_scores.to_numpy().mean() == 0.0
+        assert abs(new_scores.to_numpy().std() - 1.0) < 0.0001
+
+    def test_partitioned_standard_scaler(self, spark_context: SQLContext):
+        ls = scalers.StandardScalarScaler('score', 'tenant', 'new_score', 1.0, use_pandas=False)
+
+        df = self.create_sample_dataframe(spark_context)
+        model = ls.fit(df)
+        new_df = model.transform(df)
+
+        assert new_df.count() == df.count()
+
+        for tenant in ['t1', 't2', 't3']:
+            new_scores = new_df.filter(f.col('tenant') == tenant).toPandas()['new_score']
+
+            assert new_scores is not None
+
+            the_mean = new_scores.to_numpy().mean()
+            the_std = new_scores.to_numpy().std()
+            tenant_scores = [s for _, s in new_scores.items()]
+
+            assert the_mean == 0.0
+
+            if tenant != 't3':
+                assert abs(the_std - 1.0) < 0.0001, str(the_std)
+                assert len(tenant_scores) == 2
+                assert tenant_scores[0] == -1.0
+                assert tenant_scores[1] == 1.0
+            else:
+                assert the_std == 0.0
+                assert len(tenant_scores) == 1
+                assert tenant_scores[0] == 0.0
+
+
+class TestStandardScalarScalerExplain(ExplainTester):
+    def test_explain(self):
+        types = [str, float]
+
+        def counts(c: int, tt: Type):
+            return tt not in types or c > 0
+
+        params = ['inputCol', 'partitionKey', 'outputCol', 'coefficientFactor']
+        self.check_explain(scalers.StandardScalarScaler('input', 'tenant', 'output'), params, counts)
+
+
+class TestLinearScalarScalerExplain(ExplainTester):
+    def test_explain(self):
+        types = [str, float]
+
+        def counts(c: int, tt: Type):
+            return tt not in types or c > 0
+
+        params = ['inputCol', 'partitionKey', 'outputCol', 'minRequiredValue', 'maxRequiredValue']
+        self.check_explain(scalers.LinearScalarScaler('input', 'tenant', 'output'), params, counts)
