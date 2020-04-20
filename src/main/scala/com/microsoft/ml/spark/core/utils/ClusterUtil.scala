c23417b3a6933e5707fdbf7844a16df198a84071
Keunhyun Oh <keunhyun.oh@ahnlab.com>
2020-04-20 21:10:09 +0900

    fix default values based on master - https://spark.apache.org/docs/latest/configuration.html

--- src/main/scala/com/microsoft/ml/spark/core/utils/ClusterUtil.scala
+++ src/main/scala/com/microsoft/ml/spark/core/utils/ClusterUtil.scala
@@ -19,19 +19,7 @@ object ClusterUtil {
     */
   def getNumCoresPerExecutor(dataset: Dataset[_], log: Logger): Int = {
     val spark = dataset.sparkSession
-    val confTaskCpus =
-      try {
-        val taskCpusConfig = spark.sparkContext.getConf.getOption("spark.task.cpus")
-        if (taskCpusConfig.isEmpty) {
-          log.info("ClusterUtils did not detect spark.task.cpus config set, using default 1 instead")
-        }
-        taskCpusConfig.getOrElse("1").toInt
-      } catch {
-        case _: NoSuchElementException => {
-          log.info("spark.task.cpus config not set, using default 1 instead")
-          1
-        }
-      }
+    val confTaskCpus = getTaskCpus(dataset, log)
     try {
       val confCores = spark.sparkContext.getConf
         .get("spark.executor.cores").toInt
@@ -41,12 +29,71 @@ object ClusterUtil {
       coresPerExec
     } catch {
       case _: NoSuchElementException =>
-        // If spark.executor.cores is not defined, get the cores per JVM
-        val numMachineCores = getJVMCPUs(spark)
-        val coresPerExec = numMachineCores / confTaskCpus
+        // If spark.executor.cores is not defined, get the cores based on master
+        val defaultNumCores = getDefaultNumExecutorCores(dataset, log)
+        val coresPerExec = defaultNumCores / confTaskCpus
         log.info(s"ClusterUtils calculated num cores per executor as $coresPerExec from " +
-          s"$numMachineCores machine cores from JVM and $confTaskCpus task CPUs")
+          s"default num cores($defaultNumCores) from master and $confTaskCpus task CPUs")
         coresPerExec
+    }
+  }
+
+  def getDefaultNumExecutorCores(dataset: Dataset[_], log: Logger): Int = {
+    val spark = dataset.sparkSession
+    val master = try {
+      val masterConf = spark.sparkContext.getConf.getOption("spark.master")
+      if (masterConf.isDefined) {
+        log.info(s"ClusterUtils detected spark.master config (spark.master: ${masterConf.get})")
+      } else {
+        log.info("ClusterUtils did not detect spark.master config set")
+      }
+
+      masterConf
+    } catch {
+      case _: NoSuchElementException => {
+        log.info("spark.master config not set")
+        None
+      }
+    }
+
+    // ref: https://spark.apache.org/docs/latest/configuration.html
+    if (master.isEmpty) {
+      val numMachineCores = getJVMCPUs(spark)
+      log.info("ClusterUtils did not detect spark.master config set" +
+        s"So, the number of machine cores($numMachineCores) from JVM is used")
+      numMachineCores
+    } else if (master.get.startsWith("spark://") || master.get.startsWith("mesos://")) {
+      // all the available cores on the worker in standalone and Mesos coarse-grained modes
+      val numMachineCores = getJVMCPUs(spark)
+      log.info(s"ClusterUtils detected the number of executor cores from $numMachineCores machine cores from JVM" +
+        s"based on master address")
+      numMachineCores
+    } else if (master.get.startsWith("yarn") || master.get.startsWith("k8s://")) {
+      // 1 in YARN mode
+      log.info(s"ClusterUtils detected 1 as the number of executor cores based on master address")
+      1
+    } else {
+      val numMachineCores = getJVMCPUs(spark)
+      log.info(s"ClusterUtils did not detect master that has known default value." +
+          s"So, the number of machine cores($numMachineCores) from JVM is used")
+      numMachineCores
+    }
+
+  }
+
+  def getTaskCpus(dataset: Dataset[_], log: Logger): Int = {
+    val spark = dataset.sparkSession
+    try {
+      val taskCpusConfig = spark.sparkContext.getConf.getOption("spark.task.cpus")
+      if (taskCpusConfig.isEmpty) {
+        log.info("ClusterUtils did not detect spark.task.cpus config set, using default 1 instead")
+      }
+      taskCpusConfig.getOrElse("1").toInt
+    } catch {
+      case _: NoSuchElementException => {
+        log.info("spark.task.cpus config not set, using default 1 instead")
+        1
+      }
     }
   }
 
