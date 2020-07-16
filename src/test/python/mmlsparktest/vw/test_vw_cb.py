a5a1a18ecfb55cc0ba6a62aaa3f293a8feda3e19
Jack Gerrits <jack@jackgerrits.com>
2020-07-16 11:10:44 -0400

    Add Python tests

--- src/test/python/mmlsparktest/vw/test_vw_cb.py
+++ src/test/python/mmlsparktest/vw/test_vw_cb.py
@@ -4,38 +4,38 @@ import unittest
 import tempfile
 import pyspark
 
-from mmlspark.vw.VowpalWabbitClassifier import VowpalWabbitContextualBandit
-from mmlspark.vw.VowpalWabbitRegressor import VowpalWabbitRegressor
+from mmlsparktest.spark import *
+
+from mmlspark.vw.VowpalWabbitContextualBandit import VowpalWabbitContextualBandit
 from mmlspark.vw.VowpalWabbitFeaturizer import VowpalWabbitFeaturizer
 from mmlspark.vw.ColumnVectorSequencer import ColumnVectorSequencer
 
 from pyspark.sql.types import *
-from mmlsparktest.spark import *
 from pyspark.ml import Pipeline
 
 class VowpalWabbitSpec(unittest.TestCase):
 
     def get_data(self):
         # create sample data
-        schema = StructType([StructField("shared", StringType()),
+        schema = StructType([StructField("shared_text", StringType()),
             StructField("action1", StringType()),
             StructField("action2_feat1", StringType()),
             StructField("action2_feat2", StringType()),
-            StructField("action2_feat2", StringType()),
-            StructField("cost", DoubleType()),
-            StructField("prob", DoubleType())])
+            StructField("chosenAction", IntegerType()),
+            StructField("label", DoubleType()),
+            StructField("probability", DoubleType())])
 
         data = pyspark.sql.SparkSession.builder.getOrCreate().createDataFrame([
-            ("shared_f", "action1_f", "action2_f", "action2_f2=0", 1, 1, 0.8),
-            ("shared_f", "action1_f", "action2_f", "action2_f2=1", 2, 1, 0.8),
-            ("shared_f", "action1_f", "action2_f", "action2_f2=1", 2, 4, 0.8),
-            ("shared_f", "action1_f", "action2_f", "action2_f2=0", 1, 0, 0.8)], schema)
+            ("shared_f", "action1_f", "action2_f", "action2_f2=0", 1, 1.0, 0.8),
+            ("shared_f", "action1_f", "action2_f", "action2_f2=1", 2, 1.0, 0.8),
+            ("shared_f", "action1_f", "action2_f", "action2_f2=1", 2, 4.0, 0.8),
+            ("shared_f", "action1_f", "action2_f", "action2_f2=0", 1, 0.0, 0.8)], schema)
 
         # featurize data
-        shared_featurizer = VowpalWabbitFeaturizer(inputCols=['shared'], outputCol='shared_features')
+        shared_featurizer = VowpalWabbitFeaturizer(inputCols=['shared_text'], outputCol='shared')
         action_one_featurizer = VowpalWabbitFeaturizer(inputCols=['action1'], outputCol='action1_features')
         action_two_featurizer = VowpalWabbitFeaturizer(inputCols=['action2_feat1','action2_feat2'], outputCol='action2_features')
-        action_merger = ColumnVectorSequencer(inputCols=['action1_features','action2_features'], outputCol='action_features')
+        action_merger = ColumnVectorSequencer(inputCols=['action1_features','action2_features'], outputCol='features')
         pipeline = Pipeline(stages=[shared_featurizer, action_one_featurizer, action_two_featurizer, action_merger])
         tranformation_pipeline = pipeline.fit(data)
 
@@ -53,18 +53,36 @@ class VowpalWabbitSpec(unittest.TestCase):
             self.assertTrue(os.stat(modelFile).st_size > 0)
 
     def test_save_model(self):
-        self.save_model(VowpalWabbitContextualBandit())
+        pass
+        self.save_model(VowpalWabbitContextualBandit()
+                        .setArgs("--cb_explore_adf --epsilon 0.2 --quiet")
+                        .setUseBarrierExecutionMode(False))
 
     def test_initial_model(self):
         featurized_data = self.get_data()
-        estimator1 = VowpalWabbitContextualBandit()
+        estimator1 = VowpalWabbitContextualBandit()\
+            .setArgs("--cb_explore_adf --epsilon 0.2 --quiet")\
+            .setUseBarrierExecutionMode(False)
 
         model1 = estimator1.fit(featurized_data)
 
-        estimator2 = VowpalWabbitContextualBandit()
+        estimator2 = VowpalWabbitContextualBandit()\
+            .setArgs("--cb_explore_adf --epsilon 0.2 --quiet")\
+            .setUseBarrierExecutionMode(False)
         estimator2.setInitialModel(model1)
 
         model2 = estimator2.fit(featurized_data)
+
+    def test_performance_statistics(self):
+        featurized_data = self.get_data()
+        estimator1 = VowpalWabbitContextualBandit()\
+            .setArgs("--cb_explore_adf --epsilon 0.2 --quiet")\
+            .setUseBarrierExecutionMode(False)
+        model1 = estimator1.fit(featurized_data)
+        # TODO: Uncomment and convert to proper Python once getPerformanceStatistics is exposed
+        # stats = model1.getPerformanceStatistics()
+        # assert stats.select("ipsEstimate").first.getDouble(0) > 0
+        # assert stats.select("snipsEstimate").first.getDouble(0) > 0
 
 if __name__ == "__main__":
     result = unittest.main()
