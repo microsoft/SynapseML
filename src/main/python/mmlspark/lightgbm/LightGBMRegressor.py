f66e45516d3fa32f936935d9826afc13d438b6be
Jean Lescut-Muller <Jean.lescut@gmail.com>
2020-10-21 12:21:51 +0200

    LightGBMRegressor.py : updating imports
    SparkContext was imported twice. Also SQLContext is outdated, and was not used anyways

--- src/main/python/mmlspark/lightgbm/LightGBMRegressor.py
+++ src/main/python/mmlspark/lightgbm/LightGBMRegressor.py
@@ -2,9 +2,6 @@
 # Licensed under the MIT License. See LICENSE in project root for information.
 
 import sys
-from pyspark import SQLContext
-from pyspark import SparkContext
-
 if sys.version >= '3':
     basestring = str
 
