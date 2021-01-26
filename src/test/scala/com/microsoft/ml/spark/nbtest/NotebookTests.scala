7a1dd3feb3c512ca1e42f647d0b0b6449f8b425c
Mark Hamilton <marhamil@microsoft.com>
2021-01-26 11:08:49 -0500

    fix: change notebook run order

--- src/test/scala/com/microsoft/ml/spark/nbtest/NotebookTests.scala
+++ src/test/scala/com/microsoft/ml/spark/nbtest/NotebookTests.scala
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit
 import com.microsoft.ml.spark.core.test.base.TestBase
 import com.microsoft.ml.spark.nbtest.DatabricksUtilities._
 
+import scala.collection.mutable
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.language.existentials
@@ -17,6 +18,7 @@ class NotebookTests extends TestBase {
 
   test("Databricks Notebooks") {
     val clusterId = createClusterInPool(ClusterName, PoolId)
+    val jobIdsToCancel = mutable.ListBuffer[Int]()
     try {
       println("Checking if cluster is active")
       tryWithRetries(Seq.fill(60 * 15)(1000).toArray) { () =>
@@ -29,42 +31,35 @@ class NotebookTests extends TestBase {
       }
       println(s"Creating folder $Folder")
       workspaceMkDir(Folder)
-      println(s"Submitting nonparallelizable job...")
-      val nonParFailutes = NonParallizableNotebooks.toIterator.map { nb =>
-        val jid = uploadAndSubmitNotebook(clusterId, nb)
-        try {
-          val monitor = monitorJob(jid, TimeoutInMillis, logLevel = 2)
-          Await.ready(monitor, Duration(TimeoutInMillis.toLong, TimeUnit.MILLISECONDS)).value.get
-        } catch {
-          case t: Throwable =>
-            println(s"Cancelling job $jid")
-            cancelRun(jid)
-            throw t
-        }
-      }.filter(_.isFailure).toArray
 
       println(s"Submitting jobs")
       val parJobIds = ParallizableNotebooks.map(uploadAndSubmitNotebook(clusterId, _))
+      parJobIds.foreach(jobIdsToCancel.append(_))
+
       println(s"Submitted ${parJobIds.length} for execution: ${parJobIds.toList}")
-      try {
-        println(s"Monitoring Parallel Jobs...")
-        val monitors = parJobIds.map((runId: Int) => monitorJob(runId, TimeoutInMillis, logLevel = 2))
 
-        println(s"Awaiting parallelizable jobs...")
-        val parFailures = monitors
-          .map(Await.ready(_, Duration(TimeoutInMillis.toLong, TimeUnit.MILLISECONDS)).value.get)
-          .filter(_.isFailure)
+      println(s"Monitoring Parallel Jobs...")
+      val monitors = parJobIds.map((runId: Int) => monitorJob(runId, TimeoutInMillis, logLevel = 2))
 
-        assert(parFailures.isEmpty && nonParFailutes.isEmpty)
-      } catch {
-        case t: Throwable =>
-          parJobIds.foreach { jid =>
-            println(s"Cancelling job $jid")
-            cancelRun(jid)
-          }
-          throw t
-      }
+      println(s"Awaiting parallelizable jobs...")
+      val parFailures = monitors
+        .map(Await.ready(_, Duration(TimeoutInMillis.toLong, TimeUnit.MILLISECONDS)).value.get)
+        .filter(_.isFailure)
+
+      println(s"Submitting nonparallelizable job...")
+      val nonParFailutes = NonParallizableNotebooks.toIterator.map { nb =>
+        val jid = uploadAndSubmitNotebook(clusterId, nb)
+        jobIdsToCancel.append(jid)
+        val monitor = monitorJob(jid, TimeoutInMillis, logLevel = 2)
+        Await.ready(monitor, Duration(TimeoutInMillis.toLong, TimeUnit.MILLISECONDS)).value.get
+      }.filter(_.isFailure).toArray
+
+      assert(parFailures.isEmpty && nonParFailutes.isEmpty)
     } finally {
+      jobIdsToCancel.foreach { jid =>
+        println(s"Cancelling job $jid")
+        cancelRun(jid)
+      }
       deleteCluster(clusterId)
     }
   }
