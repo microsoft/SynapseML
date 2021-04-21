a7799526f5e6c0cb2c61b3224ab7ff751f57fcc0
Serena Ruan <serenaruan@microsoft.com>
2021-04-21 16:15:52 +0800

    change log message to JSON format

--- src/main/scala/com/microsoft/ml/spark/logging/BasicLogging.scala
+++ src/main/scala/com/microsoft/ml/spark/logging/BasicLogging.scala
@@ -5,29 +5,43 @@ package com.microsoft.ml.spark.logging
 
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.Dataset
+import spray.json.DefaultJsonProtocol
+
+case class BasicLogInfo(
+                       uid: String,
+                       className: String,
+                       method: String,
+                       )
+
+object LogJsonProtocol extends DefaultJsonProtocol {
+  implicit val logFormat = jsonFormat3(BasicLogInfo)
+}
+
+import LogJsonProtocol._
+import spray.json._
 
 trait BasicLogging extends Logging {
 
   val uid: String
 
   def logClass(): Unit = {
-    logInfo(s"metrics/ {uid: $uid, class: $getClass}")
+    logInfo("metrics/ " + BasicLogInfo(uid, getClass.toString, "constructor").toJson.compactPrint)
   }
 
   def logFit(): Unit = {
-      logInfo(s"metrics/ {uid: $uid, method: fit}")
+      logInfo(s"metrics/ " + BasicLogInfo(uid, getClass.toString, "fit").toJson.compactPrint)
   }
 
   def logTrain(): Unit = {
-    logInfo(s"metrics/ {uid: $uid, method: train}")
+    logInfo(s"metrics/ " + BasicLogInfo(uid, getClass.toString, "train").toJson.compactPrint)
   }
 
   def logTransform(dataset: Dataset[_]): Unit = {
-     logInfo(s"metrics/ {uid: $uid, method: transform, dataset: {columns: ${dataset.columns.mkString}}}")
+     logInfo(s"metrics/ " + BasicLogInfo(uid, getClass.toString, "transform").toJson.compactPrint)
   }
 
   def logPredict(): Unit = {
-    logInfo(s"metrics/ {uid: $uid, method: predict}")
+    logInfo(s"metrics/ " + BasicLogInfo(uid, getClass.toString, "predict").toJson.compactPrint)
   }
 
 }
