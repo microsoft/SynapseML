1941be5aad796102e31dec450241b5fa32bb8065
Mark Hamilton <mhamilton723@gmail.com>
2021-05-28 19:50:07 -0400

    fix: avoid  strange issue with databricks json parser

--- src/test/scala/com/microsoft/ml/spark/cognitive/split2/SearchWriterSuite.scala
+++ src/test/scala/com/microsoft/ml/spark/cognitive/split2/SearchWriterSuite.scala
@@ -14,7 +14,7 @@ import com.microsoft.ml.spark.core.test.fuzzing.{TestObject, TransformerFuzzing}
 import org.apache.http.client.methods.HttpDelete
 import org.apache.spark.ml.util.MLReadable
 import org.apache.spark.sql.DataFrame
-
+import org.apache.spark.sql.functions.{lit, udf, col, split}
 import scala.collection.mutable
 import scala.concurrent.blocking
 
@@ -73,7 +73,7 @@ class SearchWriterSuite extends TestBase with AzureSearchKey with IndexLister
     name
   }
 
-  val indexName: String = generateIndexName()
+  lazy val indexName: String = generateIndexName()
 
   override def beforeAll(): Unit = {
     print("WARNING CREATING SEARCH ENGINE!")
@@ -106,7 +106,9 @@ class SearchWriterSuite extends TestBase with AzureSearchKey with IndexLister
     } catch {
       case _: Exception if timeouts.nonEmpty =>
         println(s"Sleeping for ${timeouts.head}")
-        blocking {Thread.sleep(timeouts.head)}
+        blocking {
+          Thread.sleep(timeouts.head)
+        }
         retryWithBackoff(f, timeouts.tail)
     }
   }
@@ -238,32 +240,33 @@ class SearchWriterSuite extends TestBase with AzureSearchKey with IndexLister
     */
   test("Handle null values for Collection(Edm.String) fields") {
     val in = generateIndexName()
-    val phraseIndex = s"""
-           |{
-           |    "name": "$in",
-           |    "fields": [
-           |      {
-           |        "name": "id",
-           |        "type": "Edm.String",
-           |        "key": true,
-           |        "facetable": false
-           |      },
-           |    {
-           |      "name": "fileName",
-           |      "type": "Edm.String",
-           |      "searchable": false,
-           |      "sortable": false,
-           |      "facetable": false
-           |    },
-           |    {
-           |      "name": "phrases",
-           |      "type": "Collection(Edm.String)",
-           |      "filterable": false,
-           |      "sortable": false,
-           |      "facetable": false
-           |    }
-           |    ]
-           |  }
+    val phraseIndex =
+      s"""
+         |{
+         |    "name": "$in",
+         |    "fields": [
+         |      {
+         |        "name": "id",
+         |        "type": "Edm.String",
+         |        "key": true,
+         |        "facetable": false
+         |      },
+         |    {
+         |      "name": "fileName",
+         |      "type": "Edm.String",
+         |      "searchable": false,
+         |      "sortable": false,
+         |      "facetable": false
+         |    },
+         |    {
+         |      "name": "phrases",
+         |      "type": "Collection(Edm.String)",
+         |      "filterable": false,
+         |      "sortable": false,
+         |      "facetable": false
+         |    }
+         |    ]
+         |  }
         """.stripMargin
     val phraseDF = Seq(
       ("upload", "0", "file0", Array("p1", "p2", "p3")),
@@ -299,12 +302,12 @@ class SearchWriterSuite extends TestBase with AzureSearchKey with IndexLister
         "filterNulls" -> "true",
         "indexName" -> in,
         "keyCol" -> "id"
-    ))
+      ))
 
     retryWithBackoff(assertSize(in, 2))
   }
 
-  test("pipeline with analyze image"){
+  test("pipeline with analyze image") {
     val in = generateIndexName()
 
     val df = Seq(
