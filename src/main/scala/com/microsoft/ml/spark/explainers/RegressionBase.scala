c4a9c43c8748d18b5866da1fd7565624b005a6ae
Jason Wang <jasonwang_83@hotmail.com>
2021-06-14 20:17:37 -0700

    Change regression base to support sparse vector as well

--- src/main/scala/com/microsoft/ml/spark/explainers/RegressionBase.scala
+++ src/main/scala/com/microsoft/ml/spark/explainers/RegressionBase.scala
@@ -3,7 +3,7 @@
 
 package com.microsoft.ml.spark.explainers
 
-import breeze.linalg.{*, sum, DenseMatrix => BDM, DenseVector => BDV}
+import breeze.linalg.{*, sum, DenseMatrix => BDM, DenseVector => BDV, Vector => BV, Matrix => BM}
 import breeze.numerics.sqrt
 
 case class RegressionResult(coefficients: BDV[Double], intercept: Double, rSquared: Double, loss: Double)
@@ -18,49 +18,55 @@ case class RegressionResult(coefficients: BDV[Double], intercept: Double, rSquar
   */
 //noinspection ScalaStyle
 abstract class RegressionBase {
-  def fit(data: BDM[Double], outputs: BDV[Double], fitIntercept: Boolean): RegressionResult = {
+  def fit(data: BM[Double], outputs: BV[Double], fitIntercept: Boolean): RegressionResult = {
     val weights = BDV.ones[Double](data.rows)
     fit(data, outputs, weights, fitIntercept)
   }
 
-  def fit(data: BDM[Double], outputs: BDV[Double], sampleWeights: BDV[Double], fitIntercept: Boolean)
+  def fit(data: BM[Double], outputs: BV[Double], sampleWeights: BV[Double], fitIntercept: Boolean)
   : RegressionResult = {
     require(data.rows == sampleWeights.size, s"data.rows != sampleWeights.size: ${data.rows} != ${sampleWeights.size}")
     require(sampleWeights.forall(_ >= 0), "Weights must be non-negative")
 
-    val normalizedWeights = normalizeSampleWeights(sampleWeights)
+    val (dataDense, outputsDense, sampleWeightsDense) = (
+      data.toDenseMatrix,
+      outputs.toDenseVector,
+      sampleWeights.toDenseVector
+    )
+
+    val normalizedWeights = normalizeSampleWeights(sampleWeightsDense)
 
     val (x, y, xOffset, yOffset) = if (fitIntercept) {
       // step 1: center x and y.
-      val x_weighted = BDM.tabulate(data.rows, data.cols) {
+      val xWeighted = BDM.tabulate(dataDense.rows, dataDense.cols) {
         (i, j) =>
-          normalizedWeights(i) * data(i, j)
+          normalizedWeights(i) * dataDense(i, j)
       }
 
-      val x_offset = (sum(x_weighted(::, *)) /:/ sum(normalizedWeights)).t
-      val x_centered = data(*, ::).map {
-        row => row - x_offset
+      val xOffset = (sum(xWeighted(::, *)) /:/ sum(normalizedWeights)).t
+      val xCentered = dataDense(*, ::).map {
+        row => row - xOffset
       }
 
-      val y_weighted = BDV.tabulate(outputs.size) {
-        i => normalizedWeights(i) * outputs(i)
+      val yWeighted = BDV.tabulate(outputsDense.size) {
+        i => normalizedWeights(i) * outputsDense(i)
       }
 
-      val y_offset = sum(y_weighted) / sum(normalizedWeights)
-      val y_centered = outputs - y_offset
+      val yOffset = sum(yWeighted) / sum(normalizedWeights)
+      val yCentered = outputsDense - yOffset
 
-      (x_centered, y_centered, x_offset, y_offset)
+      (xCentered, yCentered, xOffset, yOffset)
     } else {
-      (data, outputs, BDV.zeros[Double](data.cols), 0d)
+      (dataDense, outputsDense, BDV.zeros[Double](dataDense.cols), 0d)
     }
 
     // step 2: rescale x, y by sqrt(weights)
-    val xRescaled = BDM.tabulate(data.rows, data.cols) {
+    val xRescaled = BDM.tabulate(dataDense.rows, dataDense.cols) {
       (i, j) =>
         sqrt(normalizedWeights(i)) * x(i, j)
     }
 
-    val yRescaled = BDV.tabulate(outputs.size) {
+    val yRescaled = BDV.tabulate(outputsDense.size) {
       i => sqrt(normalizedWeights(i)) * y(i)
     }
 
@@ -70,8 +76,8 @@ abstract class RegressionBase {
     // step 4: compute intercept
     val intercept = if (fitIntercept) yOffset - (xOffset dot coefficients) else 0d
 
-    val loss = computeLoss(coefficients, intercept)(data, outputs, sampleWeights)
-    val rSquared = computeRSquared(coefficients, intercept)(data, outputs, sampleWeights)
+    val loss = computeLoss(coefficients, intercept)(dataDense, outputsDense, sampleWeightsDense)
+    val rSquared = computeRSquared(coefficients, intercept)(dataDense, outputsDense, sampleWeightsDense)
 
     RegressionResult(coefficients, intercept, rSquared, loss)
   }
