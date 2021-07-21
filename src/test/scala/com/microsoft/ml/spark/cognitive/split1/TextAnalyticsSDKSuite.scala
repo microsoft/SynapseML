3883725e5e33dcf6a5b6a6ea3848fe0f087fc8b5
victoriajmicrosoft <t-vjohnston@microsoft.com>
2021-07-21 18:55:00 -0400

    fix: confidence tests, model-version format

--- src/test/scala/com/microsoft/ml/spark/cognitive/split1/TextAnalyticsSDKSuite.scala
+++ src/test/scala/com/microsoft/ml/spark/cognitive/split1/TextAnalyticsSDKSuite.scala
@@ -51,29 +51,39 @@ class DetectedLanguageSuitev4 extends TestBase with DataFrameEquality with TextK
     assert(iso(0).get(0).toString == "en" && iso(0).get(1).toString == "es" &&
       iso(1).get(0).toString == "fr" && iso(1).get(1).toString == "zh")
   }
-
-  test("Language Detection - Assert Confidence Score") {
+  test("Language Detection - Check Confidence Score") {
     val replies = detector.transform(df)
-      .select("output.result.name", "output.result.confidenceScore")
+      .select("output", "output.result.confidenceScore")
       .collect()
-
-    val confidence = replies.map(row => row.getList(1))
-    assert(confidence(0).get(0).toString > "0.50")
-    assert(confidence(0).get(1).toString > "0.50")
-    assert(confidence(1).get(0).toString > "0.50")
-    assert(confidence(1).get(1).toString > "0.50")
-    assert(confidence(2).get(0).toString < "0.50")
+    assert(replies(0).schema(0).name == "output", "output.result.confidenceScore")
+      val firstRow = replies(0)
+      val secondRow = replies(1)
+    assert(replies(0).schema(0).name == "output")
+    val fromRow = DetectLanguageResponseV4.makeFromRowConverter
+    replies.foreach(row => {
+        val resFirstRow = fromRow(firstRow.getAs[GenericRowWithSchema]("output"))
+        val resSecondRow = fromRow(secondRow.getAs[GenericRowWithSchema]("output"))
+        val modelVersionFirstRow = resFirstRow.modelVersion.get
+        val modelVersionSecondRow = resSecondRow.modelVersion.get
+        val positiveScoreFirstRow = resFirstRow.result.head.get.confidenceScore
+        val negativeScoreFirstRow = resFirstRow.result.head.get.confidenceScore
+        val neutralScoreFirstRow = resFirstRow.result.head.get.confidenceScore
+        assert(modelVersionFirstRow.matches("\\d{4}-\\d{2}-\\d{2}"))
+        assert(modelVersionFirstRow.matches("\\d{4}-\\d{2}-\\d{2}"))
+        assert(positiveScoreFirstRow > 0.5)
+        assert(negativeScoreFirstRow > 0.5)
+        assert(neutralScoreFirstRow > 0.5)
+      })
   }
-  test("Language Detection - Check Model Version") {
-    val outputCol = detector.transform(df)
+  test("Language Detection - Assert Model Version") {
+    val replies = detector.transform(df)
       .select("output")
       .collect()
-    assert(outputCol(0).schema(0).name == "output")
+    assert(replies(0).schema(0).name == "output")
     val fromRow = DetectLanguageResponseV4.makeFromRowConverter
-    outputCol.foreach(row => {
+    replies.foreach(row => {
       val outResponse = fromRow(row.getAs[GenericRowWithSchema]("output"))
-      val modelCheck = outResponse.modelVersion.get == "2021-06-01"
-      modelCheck.toString.matches("\\d{4}-\\d{2}-\\d{2}")
+      val modelCheck = outResponse.result.head.get
     })
   }
 }
@@ -112,17 +122,18 @@ class TextSentimentSuiteV4 extends TestBase with DataFrameEquality with TextKey 
     }
   };
   test("Sentiment Analysis - Check Model Version") {
-    val outputCol = detector.transform(batchedDF)
+    val replies = detector.transform(batchedDF)
       .select("output")
       .collect()
-    assert(outputCol(0).schema(0).name == "output")
+    assert(replies(0).schema(0).name == "output")
     val fromRow = SentimentResponseV4.makeFromRowConverter
-    outputCol.foreach(row => {
+    replies.foreach(row => {
       val outResponse = fromRow(row.getAs[GenericRowWithSchema]("output"))
-      val modelCheck = outResponse.modelVersion.get == "2020-04-01"
+      val modelCheck = outResponse.result.head.get
       modelCheck.toString.matches("\\d{4}-\\d{2}-\\d{2}")
     })
   }
+
   lazy val detector2: TextSentimentV4 = new TextSentimentV4(options)
     .setSubscriptionKey(textKey)
     .setEndpoint("https://eastus.api.cognitive.microsoft.com/")
@@ -141,13 +152,32 @@ class TextSentimentSuiteV4 extends TestBase with DataFrameEquality with TextKey 
 
   test("Sentiment Analysis - Assert Confidence Score") {
     val replies = detector.transform(batchedDF)
-      .select("output.result.sentiment", "output.result.confidenceScores")
+      .select("output")
       .collect()
-    val confidence = replies.map(row => row.getList(1))
-    assert(confidence(0).get(0).toString > "[0.50, 0.0, 0.0]")
-    assert(confidence(0).get(1).toString > "[0.0, 0.0, 0.50]")
-    assert(confidence(0).get(2).toString > "[0.50, 0.0, 0.0]")
-    assert(confidence(1).get(0).toString > "[0.0, 0.0, 0.50]")
+    val firstRow = replies(0)
+    val secondRow = replies(1)
+    assert(replies(0).schema(0).name == "output")
+    val fromRow = SentimentResponseV4.makeFromRowConverter
+    replies.foreach(row => {
+      val resFirstRow = fromRow(firstRow.getAs[GenericRowWithSchema]("output"))
+      val resSecondRow = fromRow(secondRow.getAs[GenericRowWithSchema]("output"))
+      val modelVersionFirstRow = resFirstRow.modelVersion.get
+      val modelVersionSecondRow = resSecondRow.modelVersion.get
+      val positiveScoreFirstRow = resFirstRow.result.head.get.confidenceScores.positive
+      val negativeScoreFirstRow = resFirstRow.result.head.get.confidenceScores.negative
+      val neutralScoreFirstRow = resFirstRow.result.head.get.confidenceScores.neutral
+      val positiveScoreSecondRow = resSecondRow.result.head.get.confidenceScores.positive
+      val negativeScoreSecondRow = resSecondRow.result.head.get.confidenceScores.negative
+      val neutralScoreSecondRow = resSecondRow.result.head.get.confidenceScores.neutral
+      assert(modelVersionFirstRow.matches("\\d{4}-\\d{2}-\\d{2}"))
+      assert(modelVersionFirstRow.matches("\\d{4}-\\d{2}-\\d{2}"))
+      assert(positiveScoreFirstRow < 0.5)
+      assert(negativeScoreFirstRow > 0.5)
+      assert(neutralScoreFirstRow < 0.5)
+      assert(positiveScoreSecondRow > 0.5)
+      assert(negativeScoreSecondRow < 0.5)
+      assert(neutralScoreSecondRow < 0.5)
+    })
   }
 }
 class KeyPhraseExtractionSuiteV4 extends TestBase with DataFrameEquality with TextKey {
@@ -194,14 +224,14 @@ class KeyPhraseExtractionSuiteV4 extends TestBase with DataFrameEquality with Te
     }
   }
   test("KPE - Check Model Version") {
-    val outputCol = extractor.transform(df2)
+    val replies = extractor.transform(df2)
       .select("output")
       .collect()
-    assert(outputCol(0).schema(0).name == "output")
+    assert(replies(0).schema(0).name == "output")
     val fromRow = KeyPhraseResponseV4.makeFromRowConverter
-    outputCol.foreach(row => {
+    replies.foreach(row => {
       val outResponse = fromRow(row.getAs[GenericRowWithSchema]("output"))
-      val modelCheck = outResponse.modelVersion.get == "2021-06-01"
+      val modelCheck = outResponse.result.head.get
       modelCheck.toString.matches("\\d{4}-\\d{2}-\\d{2}")
     })
   }
