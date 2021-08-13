dbe7ba79a6f08c80a924dcc65eae41ee01ac9dc4
serena-ruan <82044803+serena-ruan@users.noreply.github.com>
2021-08-14 03:18:01 +0800

    feat: rename Read to ReadImage (#1163)

--- cognitive/src/main/scala/com/microsoft/ml/spark/cognitive/ComputerVision.scala
+++ cognitive/src/main/scala/com/microsoft/ml/spark/cognitive/ComputerVision.scala
@@ -329,7 +329,7 @@ class RecognizeText(override val uid: String)
   override protected def responseDataType: DataType = RTResponse.schema
 }
 
-object Read extends ComplexParamsReadable[Read] {
+object ReadImage extends ComplexParamsReadable[ReadImage] {
   def flatten(inputCol: String, outputCol: String): UDFTransformer = {
     val fromRow = ReadResponse.makeFromRowConverter
     new UDFTransformer()
@@ -344,14 +344,14 @@ object Read extends ComplexParamsReadable[Read] {
   }
 }
 
-class Read(override val uid: String)
+class ReadImage(override val uid: String)
   extends CognitiveServicesBaseNoHandler(uid)
     with BasicAsyncReply
     with HasImageInput with HasCognitiveServiceInput
     with HasInternalJsonOutputParser with HasSetLocation with BasicLogging with HasSetLinkedService {
   logClass()
 
-  def this() = this(Identifiable.randomUID("Read"))
+  def this() = this(Identifiable.randomUID("ReadImage"))
 
   val language = new ServiceParam[String](this, "language",
     "IThe BCP-47 language code of the text in the document. Currently," +
