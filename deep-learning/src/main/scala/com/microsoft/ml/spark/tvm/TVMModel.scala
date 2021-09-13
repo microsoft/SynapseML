7e0db7035693bfc6b6206532bfe92523e8608c43
xuwq1993 <wenqx@microsoft.com>
2021-09-13 18:41:14 +0800

    feat: add TVM component

--- deep-learning/src/main/scala/com/microsoft/ml/spark/tvm/TVMModel.scala
+++ deep-learning/src/main/scala/com/microsoft/ml/spark/tvm/TVMModel.scala
@@ -1,3 +1,23 @@
 package com.microsoft.ml.spark.tvm
 
-class TVMModel
+import org.apache.tvm.{Function, TVMValue}
+
+class TVMModel {
+  def funcDefExample(): Unit ={
+    val func: Function = Function.convertFunc(
+      new Function.Callback {
+        override def invoke(args: TVMValue*): String = {
+          val res: StringBuilder = new StringBuilder
+          res.toString
+        }
+      }
+    )
+    val res: TVMValue = func.pushArg("Hello").pushArg(" ").pushArg("World!").invoke
+    res.release()
+    func.release()
+  }
+
+  def loadModel(args: Array[String]): Unit = {
+
+  }
+}
