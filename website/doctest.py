94ff70aea8cbbbff2e27dc61abb5a1c357e711b6
Serena <serena.rxy@gmail.com>
2021-10-16 00:20:28 +0800

    add job to test website samples

--- website/doctest.py
+++ website/doctest.py
@@ -4,7 +4,7 @@ def main():
     cur_path = os.getcwd()
     folder = os.path.join(cur_path, "website", "docs", "documentation")
     os.chdir(folder)
-    os.system('pytest --codeblocks')
+    os.system("pytest --codeblocks --junit-xml={}".format(os.path.join(cur_path, "target", "website-test-result.xml")))
 
 if __name__ == '__main__':
     main()
