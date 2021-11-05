3b3593b3a0ec7a95132f33bf57f0417d3a1d6b0b
Kashyap Patel <kapat@microsoft.com>
2021-11-05 14:32:18 -0400

    Replace ModelInterpretability-SnowLeopardDetection with Interpretability-Image Explainers as outstanding Responsible AI notebook

--- website/src/plugins/examples/index.js
+++ website/src/plugins/examples/index.js
@@ -31,7 +31,7 @@ function all_examples() {
     `examples/ConditionalKNN - Exploring Art Across Cultures.md`,
     `examples/CyberML - Anomalous Access Detection.md`,
     `examples/responsible_ai/DataBalanceAnalysis - Adult Census Income.md`,
-    `examples/responsible_ai/ModelInterpretability - Snow Leopard Detection.md`,
+    `features/responsible_ai/Interpretability - Image Explainers.md`,
     `features/onnx/ONNX - Inference on Spark.md`,
     `features/lightgbm/LightGBM - Overview.md`,
     `features/vw/Vowpal Wabbit - Overview.md`,
