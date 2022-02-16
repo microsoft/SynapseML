fbff5e98bc422c9e191fbd1da4beb906e619ca8e
Daniel Ciborowski <dciborow@microsoft.com>
2022-02-16 01:41:33 -0500

    Create SARPlusModel.py

---
+++ core/src/main/python/synapse/ml/recommendation/SARPlusModel.py
@@ -0,0 +1,21 @@
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for information.
+
+
+import sys
+
+if sys.version >= "3":
+    basestring = str
+
+from synapse.ml.core.schema.Utils import *
+from pysarplus import SARPlus
+
+
+@inherit_doc
+class SARPlusModel:
+    
+    def __init__(self, sarplus: SARPlus=None):
+        self.sarplus = sarplus
+
+    def recommendForAllUsers(self, numItems):
+        return self.sarplus.recommend_k_items(top_k=numItems)
