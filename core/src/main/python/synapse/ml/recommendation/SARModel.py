759fdc85c31ca7f472ec1e4b633d7850d198985f
Daniel Ciborowski <dciborow@microsoft.com>
2022-02-16 01:42:18 -0500

    Update SARModel.py

--- core/src/main/python/synapse/ml/recommendation/SARModel.py
+++ core/src/main/python/synapse/ml/recommendation/SARModel.py
@@ -1,21 +1,7 @@
-# Copyright (C) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for information.
-
-
-import sys
-
-if sys.version >= "3":
-    basestring = str
-
-from synapse.ml.core.schema.Utils import *
-from pysarplus import SARPlus
+from synapse.ml.recommendation._SARModel import _SARModel
 
 
 @inherit_doc
-class SARModel:
-    
-    def __init__(self, sarplus: SARPlus=None):
-        self.sarplus = sarplus
-
+class SARModel(_SARModel):
     def recommendForAllUsers(self, numItems):
-        return self.sarplus.recommend_k_items(top_k=numItems)
+        return self._call_java("recommendForAllUsers", numItems)
