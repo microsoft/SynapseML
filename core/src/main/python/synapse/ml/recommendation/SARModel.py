f8e7408cba4918a30794d5728c16e394abc102aa
Daniel Ciborowski <dciborow@microsoft.com>
2022-02-16 01:42:39 -0500

    Update SARModel.py

--- core/src/main/python/synapse/ml/recommendation/SARModel.py
+++ core/src/main/python/synapse/ml/recommendation/SARModel.py
@@ -1,3 +1,13 @@
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for information.
+
+
+import sys
+
+if sys.version >= "3":
+    basestring = str
+
+from synapse.ml.core.schema.Utils import *
 from synapse.ml.recommendation._SARModel import _SARModel
 
 
