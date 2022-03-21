aab8c211dd48ff648cfda0269bf35fc7cffd15d9
Serena Ruan <82044803+serena-ruan@users.noreply.github.com>
2022-03-22 01:29:12 +0800

    fix: expose error message directly if the request is wrong (#1448)

--- cognitive/src/main/scala/com/microsoft/azure/synapse/ml/cognitive/MultivariateAnomalyDetection.scala
+++ cognitive/src/main/scala/com/microsoft/azure/synapse/ml/cognitive/MultivariateAnomalyDetection.scala
@@ -108,7 +108,9 @@ trait MADHttpRequest extends HasURL with HasSubscriptionKey with HasAsyncReply {
           s"Querying for results did not complete within $maxTries tries")
       }
     } else {
-      response
+      val error = IOUtils.toString(response.entity.get.content, "UTF-8")
+        .parseJson.convertTo[DMAError].toJson.compactPrint
+      throw new RuntimeException(s"Caught error: $error")
     }
   }
 }
@@ -464,6 +466,7 @@ class DetectMultivariateAnomaly(override val uid: String) extends Model[DetectMu
 
   protected def prepareUrl: String = getUrl + s"${getModelId}/detect"
 
+  //noinspection ScalaStyle
   override def transform(dataset: Dataset[_]): DataFrame = {
     logTransform[DataFrame] {
 
