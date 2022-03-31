f2ee01b329b32b0b596383bb4fdf34c4d0e91c35
Puneet Pruthi <ppruthi@microsoft.com>
2022-03-31 10:43:48 -0700

    milestone -1

--- tools/docker/demo/init_notebook.py
+++ tools/docker/demo/init_notebook.py
@@ -3,7 +3,7 @@ import os
 spark = SparkSession.builder \
     .master("local[*]") \
     .appName("SynapseML Docker App") \
-    .config("spark.jars.packages", "com.microsoft.azure:synapseml:" + os.environ["MMLSPARK_VERSION"]) \
-    .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven") \
+    .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:" + os.environ["MMLSPARK_VERSION"] + ",org.apache.hadoop:hadoop-azure:3.3.2,com.microsoft.azure:azure-storage:8.6.6") \
+    .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven,https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure,https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage") \
     .getOrCreate()
 sc = spark.sparkContext
