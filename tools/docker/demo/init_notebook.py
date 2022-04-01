e2e630d2725855548a6f6e5e07a5ce6ccad552c3
Puneet Pruthi <ppruthi@microsoft.com>
2022-04-01 12:57:24 -0700

    v0.9.1

--- tools/docker/demo/init_notebook.py
+++ tools/docker/demo/init_notebook.py
@@ -1,9 +1,29 @@
 from pyspark.sql import SparkSession
 import os
+import pyspark
+# spark = SparkSession.builder \
+#     .master("local[*]") \
+#     .appName("SynapseML Docker App") \
+#     .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:" + os.environ["MMLSPARK_VERSION"] + ",org.apache.hadoop:hadoop-azure:3.3.2,com.microsoft.azure:azure-storage:8.6.6") \
+#     .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven,https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure,https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage") \
+#     .getOrCreate()
+#     ("spark.hadoop.fs.wasbs.impl", "org.apache.hadoop.fs.azure.Wasbs"),
+# stop the sparkContext and set new conf
+# spark.sparkContext.stop()
+
+syanpseMLConf = pyspark.SparkConf().setAll([
+    ("spark.hadoop.fs.AbstractFileSystem.wasb.impl", "org.apache.hadoop.fs.azure.Wasb"),
+    ("spark.hadoop.fs.AbstractFileSystem.wasbs.impl", "org.apache.hadoop.fs.azure.Wasbs"),
+    ("spark.hadoop.fs.wasb.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem"),
+    ("spark.hadoop.fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem$Secure"),
+    ("spark.hadoop.fs.viewfs.overload.scheme.target.wasb.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem"),
+    ("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:" + os.environ["MMLSPARK_VERSION"] + ",org.apache.hadoop:hadoop-azure:3.3.2,org.apache.hadoop:hadoop-common:3.3.2,com.microsoft.azure:azure-storage:7.0.1"),
+    ("spark.jars.repositories", "https://mmlspark.azureedge.net/maven,https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure,https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage")
+])
+
 spark = SparkSession.builder \
     .master("local[*]") \
     .appName("SynapseML Docker App") \
-    .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:" + os.environ["MMLSPARK_VERSION"] + ",org.apache.hadoop:hadoop-azure:3.3.2,com.microsoft.azure:azure-storage:8.6.6") \
-    .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven,https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure,https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage") \
+    .config(conf=syanpseMLConf) \
     .getOrCreate()
-sc = spark.sparkContext
+sc = spark.SparkContext
