1b9c0926e3db5f72a6bc2ddde332111173aaa727
Puneet Pruthi <ppruthi@microsoft.com>
2022-04-11 19:55:06 -0700

    prune Dockerfile

--- tools/docker/demo/init_notebook.py
+++ tools/docker/demo/init_notebook.py
@@ -1,23 +1,14 @@
 from pyspark.sql import SparkSession
 import os
 import pyspark
-# spark = SparkSession.builder \
-#     .master("local[*]") \
-#     .appName("SynapseML Docker App") \
-#     .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:" + os.environ["MMLSPARK_VERSION"] + ",org.apache.hadoop:hadoop-azure:3.3.2,com.microsoft.azure:azure-storage:8.6.6") \
-#     .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven,https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure,https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage") \
-#     .getOrCreate()
-#     ("spark.hadoop.fs.wasbs.impl", "org.apache.hadoop.fs.azure.Wasbs"),
-# stop the sparkContext and set new conf
-# spark.sparkContext.stop()
 
 syanpseMLConf = pyspark.SparkConf().setAll([
     ("spark.hadoop.fs.AbstractFileSystem.wasb.impl", "org.apache.hadoop.fs.azure.Wasb"),
     ("spark.hadoop.fs.AbstractFileSystem.wasbs.impl", "org.apache.hadoop.fs.azure.Wasbs"),
     ("spark.hadoop.fs.wasb.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem"),
-    ("spark.hadoop.fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem$Secure"),
+    ("spark.hadoop.fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem"),
     ("spark.hadoop.fs.viewfs.overload.scheme.target.wasb.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem"),
-    ("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:" + os.environ["MMLSPARK_VERSION"] + ",org.apache.hadoop:hadoop-azure:2.7.0,org.apache.hadoop:hadoop-common:2.7.0,com.microsoft.azure:azure-storage:2.0.0"),
+    ("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:" + os.getenv("MMLSPARK_VERSION", "0.9.5") + ",org.apache.hadoop:hadoop-azure:2.7.0,org.apache.hadoop:hadoop-common:2.7.0,com.microsoft.azure:azure-storage:2.0.0"),
     ("spark.jars.repositories", "https://mmlspark.azureedge.net/maven,https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-azure,https://mvnrepository.com/artifact/com.microsoft.azure/azure-storage")
 ])
 
