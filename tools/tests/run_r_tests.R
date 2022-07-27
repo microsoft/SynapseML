145806db7ad26850d6786b43ba0802bf682e009c
Mark <mhamilton723@gmail.com>
2022-07-27 15:08:35 -0400

    chore: bump spark version

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -1,4 +1,4 @@
 library("sparklyr")
-spark_install(version = "3.2.0", hadoop_version = "3.2")
+spark_install(version = "3.2.1", hadoop_version = "3.2")
 options("testthat.output_file" = "../../../../r-test-results.xml")
 devtools::test(reporter = JunitReporter$new())
