d564e421a8537ed73db820e0496748bda0248a8f
Mark Niehaus <manieh@microsoft.com>
2022-08-02 16:03:15 -0700

    revert to spark_install_find

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -1,5 +1,13 @@
 library("sparklyr")
-spark_install(version = "3.2.2", hadoop_version = "3.2")
+print(spark_available_versions(show_hadoop=TRUE, show_minor=TRUE))
+#spark_install(version = "3.2.2", hadoop_version = "3.2")
+tryCatch({
+  spark_install_find(version = "3.2.2", hadoop_version = "3.2")
+},
+error=function(err) {
+  print("Installing ../../../../../../../spark-3.2.2-bin-hadoop3.2.tgz")
+  spark_install_tar("../../../../../../../spark-3.2.2-bin-hadoop3.2.tgz")
+})
 options("testthat.output_file" = "../../../../r-test-results.xml")
 tryCatch({
   print(paste("SPARK_HOME=", Sys.getenv("SPARK_HOME")))
