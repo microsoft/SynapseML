cfd09268c643d62f811cf975394d873d77248071
Mark Niehaus <manieh@microsoft.com>
2022-08-02 15:40:24 -0700

    fiddle with error handling for r tests

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -2,9 +2,10 @@ library("sparklyr")
 spark_install(version = "3.2.2", hadoop_version = "3.2")
 options("testthat.output_file" = "../../../../r-test-results.xml")
 tryCatch({
+  print(paste("SPARK_HOME=", Sys.getenv("SPARK_HOME")))
   devtools::test(reporter = JunitReporter$new())
 },
 error = function(err) {
   print(paste("Error: ", err))
+  stop(err)
 })
-
