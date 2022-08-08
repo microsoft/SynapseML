540de5bffcd925fc23f9a68a9b6e86b057422565
Puneet Pruthi <ppruthi@microsoft.com>
2022-08-08 12:08:58 -0700

    update dotnet package source for ubuntu 22

--- tools/dotnet/dotnetSetup.sh
+++ tools/dotnet/dotnetSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Add Microsoft package signing key and repository
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
