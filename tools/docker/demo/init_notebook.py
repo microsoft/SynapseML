2617ae806ec1725e6b31c8544f05496f1fe38034
Puneet Pruthi <ppruthi@microsoft.com>
2022-08-07 19:52:08 -0700

    use newer version

--- tools/docker/demo/init_notebook.py
+++ tools/docker/demo/init_notebook.py
@@ -26,8 +26,7 @@ syanpseMLConf = pyspark.SparkConf().setAll(
         ),
         (
             "spark.jars.packages",
-            "com.microsoft.azure:synapseml_2.12:"
-            + os.getenv("SYNAPSEML_VERSION", "0.10.0")
+            "com.microsoft.azure:synapseml_2.12:0.10.0-27-8f2bec70-SNAPSHOT"
             + ",org.apache.hadoop:hadoop-azure:2.7.0,org.apache.hadoop:hadoop-common:2.7.0,com.microsoft.azure:azure-storage:2.0.0",
         ),
         (
