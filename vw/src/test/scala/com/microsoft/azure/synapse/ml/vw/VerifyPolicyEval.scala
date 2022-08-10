36fc49695d653be94a029da004f82bc188a843d2
Markus Cozowicz <marcozo@microsoft.com>
2022-08-10 18:13:00 +0200

    fix unit test

--- vw/src/test/scala/com/microsoft/azure/synapse/ml/vw/VerifyPolicyEval.scala
+++ vw/src/test/scala/com/microsoft/azure/synapse/ml/vw/VerifyPolicyEval.scala
@@ -13,45 +13,13 @@ class VerifyPolicyEval extends Benchmarks {
 
   import spark.implicits._
 
-  test("Verify Repartition") {
-//    spark.sparkContext.setCheckpointDir("/tmp/checkpoint-foo/")
-
-    val dataset = Seq(
-      (10, "A", 1),
-      (20, "A", 2),
-      (30, "C", 3),
-      (40, "C", 4),
-      (5,  "A", 5),
-      (15, "A", 6),
-      (25, "C", 7),
-      (35, "C", 8)
-    )
-      .toDF("id", "groupkey", "value")
-//      .filter($"groupKey".isNotNull)
-      .repartition($"groupKey") //, $"id")
-      .cache()
-      // .checkpoint()
-
-    dataset.printSchema()
-
-    for (x <- Seq("A", "C")) {
-      println(s"grouping for $x")
-      val subDF = dataset.filter($"groupKey" === F.lit(x))
-
-      subDF.explain()
-
-      println(s"count: ${subDF.count}")
-      println()
-    }
-  }
-
   test("Verify BanditSnips") {
     val dataset = Seq(
       (0.2, 1, 0.3, 1, "A", 1),
       (0.2, 2, 0.1, 2, "A", 2),
       (0.2, 3, 0.4, 1, "A", 3),
-//      (0.2, 2, 0.1, 2, "C", 1),
-//      (0.2, 3, 0.4, 1, "C", 2)
+      (0.2, 2, 0.1, 2, "C", 1),
+      (0.2, 3, 0.4, 1, "C", 2)
     ).toDF("probLog", "reward", "probPred", "count", "key", "t")
 
     PolicyEvalUDAFUtil.registerUdafs()
@@ -66,8 +34,7 @@ class VerifyPolicyEval extends Benchmarks {
         F.expr("cressieReadInterval(probLog, reward, probPred, count, -100, 100, 0, 100)")
           .as("cressieReadInterval"),
         F.expr("cressieReadIntervalEmpirical(probLog, reward, probPred, count, -100, 100)")
-          .as("cressieReadIntervalEmpirical"),
-        F.expr("bernstein(probLog, reward, probPred, count, 0, 3, t)")
+          .as("cressieReadIntervalEmpirical")
       )
       .withColumn("cressieReadInterval_lower", F.round(F.expr("cressieReadInterval.lower"), 2))
       .withColumn("cressieReadInterval_upper", F.round(F.expr("cressieReadInterval.upper"), 2))
@@ -77,42 +44,12 @@ class VerifyPolicyEval extends Benchmarks {
         F.round(F.expr("cressieReadIntervalEmpirical.upper"), 2))
       .drop("cressieReadInterval", "cressieReadIntervalEmpirical")
 
-    actual.show()
+    val expected = Seq(
+      ("A", 2.1, 2.4, 2.4, 1.03, 33.82, 1.03, 2.73),
+      ("C", 2.7, 2.7, 2.7, 0.98, 56.18, 0.98, 2.98),
+    ).toDF("key", "snips", "ips", "cressieRead", "cressieReadInterval_lower", "cressieReadInterval_upper",
+        "cressieReadIntervalEmpirical_lower", "cressieReadIntervalEmpirical_upper")
 
-//    val expected = Seq(
-//      ("A", 2.1, 2.4, 2.4, 1.03, 33.82, 1.03, 2.73),
-//      ("C", 2.7, 2.7, 2.7, 0.98, 56.18, 0.98, 2.98),
-//    ).toDF("key", "snips", "ips", "cressieRead", "cressieReadInterval_lower", "cressieReadInterval_upper",
-//        "cressieReadIntervalEmpirical_lower", "cressieReadIntervalEmpirical_upper")
-//
-//    verifyResult(expected, actual)
+    verifyResult(expected, actual)
   }
-
-  // TODO: need Paul's help
-  //  test ("Verify Bernstein") {
-  //    import spark.implicits._
-  //
-  //    val dataset = Seq(
-  //      (0.2, 1, 0.1, 1, "A", 1),
-  //      (0.2, 1, 0.1, 2, "A", 2),
-  //      (0.1, 2, 0.1, 2, "A", 3),
-  //      (0.2, 3, 0.4, 1, "C", 1),
-  //      (0.1, 3, 0.4, 1, "C", 2),
-  //      (0.4, 3, 0.4, 1, "C", 3),
-  //      (0.2, 3, 0.4, 1, "C", 4),
-  //    ).toDF("probLog", "reward", "probPred", "count", "key", "t")
-  //
-  //    spark.udf.register("bernstein",
-  //      F.udaf(new EmpiricalBernsteinCS(), Encoders.product[EmpiricalBernsteinCSInput]))
-  //
-  //    val w = Window.partitionBy($"key")
-  //      .orderBy($"t")
-  //      .rowsBetween(Window.unboundedPreceding, Window.currentRow)
-  //
-  //    dataset.agg(
-  //      F.expr("bernstein(probLog, reward, probPred, count, 0, 3)")
-  //    //   .over(w)
-  //    )
-  //      .show()
-  //  }
 }
