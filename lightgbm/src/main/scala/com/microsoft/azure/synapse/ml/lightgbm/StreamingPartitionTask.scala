0382163630dba1913d80dcf3c3b04e04c58a6ea1
Scott Votaw <svotaw@gmail.com>
2022-08-26 13:49:10 -0700

    debugging linux test

--- lightgbm/src/main/scala/com/microsoft/azure/synapse/ml/lightgbm/StreamingPartitionTask.scala
+++ lightgbm/src/main/scala/com/microsoft/azure/synapse/ml/lightgbm/StreamingPartitionTask.scala
@@ -367,7 +367,8 @@ class StreamingPartitionTask extends BasePartitionTask {
       ctx.measures.markSamplingStop()
 
       // Convert byte array to native memory
-      log.info(s"Creating empty training dataset with $numRows rows, config:${ctx.trainingCtx.datasetParams}")
+      log.info(s"Creating empty training dataset with $numRows rows, config:${ctx.trainingCtx.datasetParams}" +
+        s" for ${ctx.executorPartitionCount} threads")
       // Generate the dataset for features
       val datasetVoidPtr = lightgbmlib.voidpp_handle()
       LightGBMUtils.validate(lightgbmlib.LGBM_DatasetCreateFromSampledColumn(
