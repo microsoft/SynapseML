5840fa03314a618fd2c83b6ffe4a25f1d0f33265
Xinyue Ruan <serena.rxy@gmail.com>
2022-08-30 15:05:58 +0800

    fix r test

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -1,8 +1,4 @@
-if (!require("sparklyr")) {
-  packages.install("sparklyr")
-  library("sparklyr")
-}
-
+library("sparklyr")
 spark_install(version = "3.1.3", hadoop_version = "3.1")
 options("testthat.output_file" = "../../../../r-test-results.xml")
 devtools::test(reporter = JunitReporter$new())
