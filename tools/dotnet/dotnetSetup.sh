2ee2f8817496b111dd06d8c1e66c53b92d746d5b
Jessica Wang <jessiwang@microsoft.com>
2022-11-01 16:50:34 -0700

    pump spark version to 3.3.1

--- tools/dotnet/dotnetSetup.sh
+++ tools/dotnet/dotnetSetup.sh
@@ -21,10 +21,10 @@ echo "##vso[task.setvariable variable=DOTNET_WORKER_DIR]$DOTNET_WORKER_DIR"
 dotnet tool install -g sleet
 
 # Install Apache Spark-3.3
-curl https://archive.apache.org/dist/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz -o spark-3.3.0-bin-hadoop3.tgz
+curl https://archive.apache.org/dist/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz -o spark-3.3.1-bin-hadoop3.tgz
 mkdir ~/bin
 tar -xzvf spark-3.3.0-bin-hadoop3.tgz -C ~/bin
-export SPARK_HOME=~/bin/spark-3.3.0-bin-hadoop3/
+export SPARK_HOME=~/bin/spark-3.3.1-bin-hadoop3/
 export PATH=$SPARK_HOME/bin:$PATH
 echo "##vso[task.setvariable variable=SPARK_HOME]$SPARK_HOME"
 echo "##vso[task.setvariable variable=PATH]$SPARK_HOME/bin:$PATH"
