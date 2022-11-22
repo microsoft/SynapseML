c806ba79d17afa6edf53c9ea55e563f6842a6825
Mark Hamilton <mhamilton723@gmail.com>
2022-11-22 00:03:00 +0000

    chore: fix style (#1736)

--- .github/workflows/scripts/clean-acr.py
+++ .github/workflows/scripts/clean-acr.py
@@ -26,29 +26,38 @@ pipeline = "mmlsparkacrexport3"
 
 conn_string = sys.argv[1]
 
-os.system('az extension add --name acrtransfer')
+os.system("az extension add --name acrtransfer")
 
 repos = json.loads(os.popen(f"az acr repository list -n {acr}").read())
 for repo in repos:
-    tags = json.loads(os.popen(
-            f"az acr repository show-tags -n {acr} --repository {repo} --orderby time_desc").read())
+    tags = json.loads(
+        os.popen(
+            f"az acr repository show-tags -n {acr} --repository {repo} --orderby time_desc"
+        ).read()
+    )
 
     for tag in tags:
         target_blob = repo + "/" + tag + ".tar"
         image = repo + ":" + tag
-  
+
         backup_exists = BlobClient.from_connection_string(
-            conn_string, container_name=container, blob_name=target_blob).exists()
+            conn_string, container_name=container, blob_name=target_blob
+        ).exists()
         if not backup_exists:
-            result = os.system(f"az acr pipeline-run create --resource-group {rg} --registry {acr} --pipeline {pipeline} --name {str(abs(hash(target_blob)))} --pipeline-type export --storage-blob {target_blob} -a {image}")
+            result = os.system(
+                f"az acr pipeline-run create --resource-group {rg} --registry {acr} --pipeline {pipeline} --name {str(abs(hash(target_blob)))} --pipeline-type export --storage-blob {target_blob} -a {image}"
+            )
             assert result == 0
             print(f"Transferred {target_blob}")
         else:
             print(f"Skipped existing {image}")
-      
+
         backup_exists = BlobClient.from_connection_string(
-            conn_string, container_name=container, blob_name=target_blob).exists()
+            conn_string, container_name=container, blob_name=target_blob
+        ).exists()
         if backup_exists:
             print(f"Deleting {image}")
-            result = os.system(f"az acr repository delete --name {acr} --image {image} --yes")
+            result = os.system(
+                f"az acr repository delete --name {acr} --image {image} --yes"
+            )
             assert result == 0
