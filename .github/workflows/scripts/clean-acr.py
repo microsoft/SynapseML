ac9dafa4f2b1b68cb2b3970f39f015be669c0e68
Mark Niehaus <manieh@microsoft.com>
2022-11-30 12:51:41 -0800

    fix env get

--- .github/workflows/scripts/clean-acr.py
+++ .github/workflows/scripts/clean-acr.py
@@ -24,7 +24,7 @@ container = "acrbackup"
 rg = "marhamil-mmlspark"
 pipeline = "mmlsparkacrexport3"
 
-conn_string = os.environ("CLEAN_ACR_CONNECTION_STRING")
+conn_string = os.environ["CLEAN_ACR_CONNECTION_STRING"]
 
 os.system("az extension add --name acrtransfer")
 
