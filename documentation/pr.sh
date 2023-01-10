de88473bfb862ef35457fbef42e419646e2bb2eb
Jessica Wang <jessiwang@microsoft.com>
2023-01-10 14:02:17 -0800

    modify document transformer

--- documentation/pr.sh
+++ documentation/pr.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo "Start processing doc"
 # fork repo
-gh.exe repo fork MicrosoftDocs/azure-docs --clone=false
+gh.exe repo fork MicrosoftDocs/azure-docs-pr --clone=false
 # clone a subdir
 git init syncdoc
 cd syncdoc
 # TODO: using team account
-git remote add origin https://github.com/JessicaXYWang/azure-docs.git
+git remote add origin https://github.com/JessicaXYWang/azure-docs-pr.git
 git config core.sparseCheckout true
 git sparse-checkout set articles/synapse-analytics/
 git pull origin main --depth=1
 # generate a notebook
-python ../SynapseML/documentation/convert_notebooks.py --input_path "../SynapseML/notebooks/features/cognitive_services/CognitiveServices - Overview.ipynb" --output_dir ./articles/synapse-analytics --owner microsoft --repo SynapseML --service cognitive-search --mapping_file ../SynapseML/documentation/authors.yml
+python ../SynapseML/documentation/convert_notebooks.py --input_path "../SynapseML/notebooks/features/cognitive_services/CognitiveServices - Overview.ipynb" --output_dir ./articles/synapse-analytics --github_authors JessicaXYWang --service cognitive-search --mapping_file ../SynapseML/documentation/authors.yml --new_title "cognitive-services-with-synapseml-overview"
 # push it to forked repo
 git add .
-git commit -m "update doc"
+git commit -m "update SynapseML doc"
 # PR to azure doc
 git push -u origin HEAD
-gh pr create --title updatedoc --body "update SynapseML documentation" --repo MicrosoftDocs/azure-docs
+gh pr create --title updatedoc --body "update SynapseML documentation" --repo MicrosoftDocs/azure-docs-pr
