1b585d20e47cc4894e5c25c8501cc3756acfdcfd
dylanw-oss <dylanwind@live.com>
2023-02-02 11:04:04 -0800

    Update test to validate ATE value should be positive with the test data

--- core/src/test/scala/com/microsoft/azure/synapse/ml/causal/VerifyDoubleMLEstimator.scala
+++ core/src/test/scala/com/microsoft/azure/synapse/ml/causal/VerifyDoubleMLEstimator.scala
@@ -16,41 +16,41 @@ class VerifyDoubleMLEstimator extends EstimatorFuzzing[DoubleMLEstimator] {
   val dog = "Dog"
   val bird = "Bird"
   private lazy val mockDataset = spark.createDataFrame(Seq(
-    (0, 1, 0.50, 0.60, dog, cat),
-    (1, 0, 0.40, 0.50, cat, dog),
-    (0, 1, 0.78, 0.99, dog, bird),
-    (1, 0, 0.12, 0.34, cat, dog),
-    (0, 1, 0.50, 0.60, dog, bird),
-    (1, 0, 0.40, 0.50, bird, dog),
-    (0, 1, 0.78, 0.99, dog, cat),
-    (1, 0, 0.11, 0.28, cat, bird),
-    (0, 1, 0.40, 0.58, dog, cat),
-    (0, 1, 0.42, 0.53, cat, dog),
-    (1, 0, 0.78, 0.99, dog, bird),
-    (0, 1, 0.12, 0.34, cat, dog),
-    (1, 1, 0.55, 0.69, cat, bird),
-    (0, 1, 0.32, 0.48, cat, dog),
-    (1, 1, 0.62, 0.78, dog, bird),
-    (0, 1, 0.19, 0.48, cat, bird),
-    (0, 1, 0.11, 0.32, cat, bird),
-    (1, 1, 0.43, 0.63, dog, cat),
-    (1, 1, 0.38, 0.73, cat, dog),
-    (1, 0, 0.88, 0.89, dog, bird),
-    (0, 1, 0.22, 0.39, cat, dog),
-    (0, 0, 0.47, 0.72, cat, bird),
-    (1, 0, 0.35, 0.49, cat, dog),
-    (0, 1, 0.66, 0.71, dog, bird),
-    (0, 1, 0.21, 0.45, cat, bird),
-    (1, 1, 0.12, 0.34, cat, dog),
-    (0, 0, 0.50, 0.60, dog, cat),
-    (1, 1, 0.42, 0.51, bird, dog),
-    (0, 0, 0.72, 0.91, cat, bird),
-    (1, 1, 0.13, 0.31, cat, dog),
-    (0, 1, 0.55, 0.69, dog, bird),
-    (1, 0, 0.38, 0.40, bird, dog),
-    (0, 1, 0.69, 0.88, dog, cat),
-    (1, 1, 0.16, 0.35, cat, dog),
-    (0, 0, 0.48, 0.58, dog, cat)))
+    (0, 1, 50, 0.60, dog, cat),
+    (1, 0, 140, 0.50, cat, dog),
+    (0, 1, 78, 0.99, dog, bird),
+    (1, 0, 92, 0.34, cat, dog),
+    (0, 1, 50, 0.60, dog, bird),
+    (1, 0, 80, 0.50, bird, dog),
+    (0, 1, 78, 0.99, dog, cat),
+    (1, 0, 101, 0.28, cat, bird),
+    (0, 1, 40, 0.58, dog, cat),
+    (0, 1, 42, 0.53, cat, dog),
+    (1, 0, 128, 0.99, dog, bird),
+    (0, 1, 12, 0.34, cat, dog),
+    (1, 1, 55, 0.69, cat, bird),
+    (0, 1, 32, 0.48, cat, dog),
+    (1, 1, 62, 0.78, dog, bird),
+    (0, 1, 19, 0.48, cat, bird),
+    (0, 1, 11, 0.32, cat, bird),
+    (1, 1, 43, 0.63, dog, cat),
+    (1, 1, 138, 0.73, cat, dog),
+    (1, 0, 98, 0.89, dog, bird),
+    (0, 1, 22, 0.39, cat, dog),
+    (0, 0, 47, 0.72, cat, bird),
+    (1, 0, 95, 0.49, cat, dog),
+    (0, 1, 66, 0.71, dog, bird),
+    (0, 1, 21, 0.45, cat, bird),
+    (1, 1, 72, 0.34, cat, dog),
+    (0, 0, 50, 0.60, dog, cat),
+    (1, 1, 72, 0.51, bird, dog),
+    (0, 0, 22, 0.91, cat, bird),
+    (1, 1, 133, 0.31, cat, dog),
+    (0, 1, 55, 0.69, dog, bird),
+    (1, 0, 58, 0.40, bird, dog),
+    (0, 1, 69, 0.88, dog, cat),
+    (1, 1, 136, 0.35, cat, dog),
+    (0, 0, 48, 0.58, dog, cat)))
     .toDF(mockLabelColumn, "col1", "col2", "col3", "col4", "col5")
 
 
@@ -78,7 +78,7 @@ class VerifyDoubleMLEstimator extends EstimatorFuzzing[DoubleMLEstimator] {
     assert(ldmlModel.getAvgTreatmentEffect != 0.0)
   }
 
-  test("Get treatment effects and confidence intervals") {
+  test("Get confidence intervals with multiple iterations") {
     val ldml = new DoubleMLEstimator()
       .setTreatmentModel(new LogisticRegression())
       .setTreatmentCol(mockLabelColumn)
@@ -87,10 +87,10 @@ class VerifyDoubleMLEstimator extends EstimatorFuzzing[DoubleMLEstimator] {
       .setMaxIter(10)
 
     val ldmlModel = ldml.fit(mockDataset)
-    assert(ldmlModel.getConfidenceInterval.length == 2
-      && ldmlModel.getConfidenceInterval(0) != ldmlModel.getConfidenceInterval(1))
+    assert(ldmlModel.getConfidenceInterval.length == 2)
+    val (ateLow, ateHigh) = (ldmlModel.getConfidenceInterval(0), ldmlModel.getConfidenceInterval(1))
+    assert(ateLow < ateHigh && ateLow > 0 && ateHigh < 150)
   }
-
 
   override def testObjects(): Seq[TestObject[DoubleMLEstimator]] =
     Seq(new TestObject(new DoubleMLEstimator()
