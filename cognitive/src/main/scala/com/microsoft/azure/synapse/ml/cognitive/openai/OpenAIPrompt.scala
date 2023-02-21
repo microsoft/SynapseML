3f2b01a5bc91c579ab052f5e584f0c90000b952b
Markus Cozowicz <marcozo@microsoft.com>
2023-02-21 12:38:34 +0100

    OpenAIPrompt

---
+++ cognitive/src/main/scala/com/microsoft/azure/synapse/ml/cognitive/openai/OpenAIPrompt.scala
@@ -0,0 +1,104 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in project root for information.
+
+package com.microsoft.azure.synapse.ml.cognitive.openai
+
+import com.microsoft.azure.synapse.ml.codegen.Wrappable
+import com.microsoft.azure.synapse.ml.core.contracts.HasOutputCol
+import com.microsoft.azure.synapse.ml.core.spark.Functions
+import com.microsoft.azure.synapse.ml.logging.SynapseMLLogging
+import com.microsoft.azure.synapse.ml.param.StringStringMapParam
+import org.apache.spark.ml.param.{DoubleParam, FloatParam, Param, ParamMap, StringArrayParam}
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.ml.{ComplexParamsReadable, ComplexParamsWritable, Transformer}
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{Column, DataFrame, Dataset, functions => F, types => T}
+
+object OpenAIPrompt extends ComplexParamsReadable[OpenAIPrompt]
+
+class OpenAIPrompt(override val uid: String) extends OpenAICompletion {
+  logClass()
+
+  def this() = this(Identifiable.randomUID("OpenAIPrompt"))
+
+  override def copy(extra: ParamMap): Transformer = defaultCopy(extra)
+
+  val promptF = new Param[String](
+    this, "promptF", "The prompt. supports string interpolation {col1}: {col2}.")
+
+  def getPromptF: String = $(promptF)
+
+  def setPromptF(value: String): this.type = set(promptF, value)
+
+  val parsedOutputCol = new Param[String](
+    this, "parsedOutputCol", "The parsed output column.")
+
+  def getParsedOutputCol: String = $(parsedOutputCol)
+
+  def setParsedOutputCol(value: String): this.type = set(parsedOutputCol, value)
+
+  val postProcessing = new Param[String](
+    this, "postProcessing", "Post processing options: csv, json, regex")
+
+  def getPostProcessing: String = $(postProcessing)
+
+  def setPostProcessing(value: String): this.type = set(postProcessing, value)
+
+  val postProcessingOptions = new StringStringMapParam(
+    this, "postProcessingOptions", "Options (default): delimiter=',', jsonSchema, regex, regexGroup=0")
+
+  def getPostProcessingOptions: Map[String, String] = $(postProcessingOptions)
+
+  def setPostProcessingOptions(value: Map[String, String]): this.type = set(postProcessingOptions, value)
+
+  setDefault(postProcessing -> "", postProcessingOptions -> Map.empty)
+
+  override def transform(dataset: Dataset[_]): DataFrame = {
+    import com.microsoft.azure.synapse.ml.core.schema.DatasetExtensions._
+
+    logTransform[DataFrame]({
+      val df = dataset.toDF
+
+      val promptColName = df.withDerivativeCol("prompt")
+      setPromptCol(promptColName)
+
+      val dfTemplated = df.withColumn(promptColName, Functions.template(getPromptF))
+
+      // apply template
+      val promptedDF = super.transform(dfTemplated)
+
+      val opts = getPostProcessingOptions
+
+      val parser = getPostProcessing.toLowerCase match {
+        case "csv" => new DelimiterParser(opts.getOrElse("delimiter", ","))
+        case "json" => new JsonParser(opts.get("jsonSchema").get, Map.empty)
+        case "regex" => new RegexParser(opts.get("regex").get, opts.get("regexGroup").get.toInt)
+        case _ => new PassThroughParser()
+      }
+
+      promptedDF.withColumn(getParsedOutputCol,
+        parser.parse(F.element_at(F.col(getOutputCol).getField("choices"), 1)
+          .getField("text")))
+    })
+  }
+}
+
+trait OutputParser {
+  def parse(responseCol: Column): Column
+}
+
+class PassThroughParser extends OutputParser {
+  def parse(responseCol: Column): Column = responseCol
+}
+
+class DelimiterParser(val delimiter: String) extends OutputParser {
+  def parse(responseCol: Column): Column = F.split(F.trim(responseCol), delimiter)
+}
+
+class JsonParser(val schema: String, options: Map[String, String]) extends OutputParser {
+  def parse(responseCol: Column): Column = F.from_json(responseCol, schema, options)
+}
+
+class RegexParser(val regex: String, val groupIdx: Int) extends OutputParser {
+  def parse(responseCol: Column): Column = F.regexp_extract(responseCol, regex, groupIdx)
+}
---
+++ cognitive/src/test/scala/com/microsoft/azure/synapse/ml/cognitive/openai/OpenAIPromptSuite.scala
@@ -0,0 +1,43 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in project root for information.
+
+package com.microsoft.azure.synapse.ml.cognitive.openai
+
+import com.microsoft.azure.synapse.ml.core.test.base.Flaky
+import com.microsoft.azure.synapse.ml.core.test.fuzzing.{TestObject, TransformerFuzzing}
+import org.apache.spark.ml.util.MLReadable
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.ml.linalg.Vector
+
+class OpenAIPromptSuite extends TransformerFuzzing[OpenAIPrompt] with OpenAIAPIKey with Flaky {
+
+  import spark.implicits._
+
+  lazy val prompt: OpenAIPrompt = new OpenAIPrompt()
+    .setSubscriptionKey(openAIAPIKey)
+    .setDeploymentName("text-davinci-001")
+    .setModel("text-davinci-003")
+    .setCustomServiceName(openAIServiceName)
+    .setOutputCol("out")
+    .setParsedOutputCol("outParsed")
+
+  lazy val df: DataFrame = Seq(
+    "I love this",
+    "I'm so confused about the color of the car",
+    "He is very eager to learn and become educated"
+  ).toDF("text")
+
+  test("Basic Usage") {
+    prompt
+      .setPromptF("{text}\nA comma-separated list of matching adjectives: ")
+      .setPostProcessing("csv")
+      .transform(df)
+      .show(5, 200)
+  }
+
+  override def testObjects(): Seq[TestObject[OpenAIPrompt]] =
+    Seq(new TestObject(prompt, df))
+
+  override def reader: MLReadable[_] = OpenAIPrompt
+
+}
