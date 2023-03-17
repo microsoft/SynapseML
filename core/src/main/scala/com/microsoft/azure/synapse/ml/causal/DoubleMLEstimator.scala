9b955556354462a01a2e2d21ec9e87040f45cdaa
dylanw-oss <dylanwind@live.com>
2023-03-17 00:35:52 -0700

    we should support 0/1 integert column with classification model

--- core/src/main/scala/com/microsoft/azure/synapse/ml/causal/DoubleMLEstimator.scala
+++ core/src/main/scala/com/microsoft/azure/synapse/ml/causal/DoubleMLEstimator.scala
@@ -22,6 +22,7 @@ import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.types.{BooleanType, DataType, DoubleType, IntegerType, LongType, StructType}
 import org.apache.commons.math3.stat.inference.TestUtils
+import org.apache.spark.sql.functions.{col, lit, when}
 
 import scala.concurrent.Future
 
@@ -76,10 +77,8 @@ class DoubleMLEstimator(override val uid: String)
   override def fit(dataset: Dataset[_]): DoubleMLModel = {
     logFit({
       require(getMaxIter > 0, "maxIter should be larger than 0!")
-      val treatmentColType = dataset.schema(getTreatmentCol).dataType
-      validateColTypeWithModel(treatmentColType, getTreatmentCol, getTreatmentModel)
-      val outcomeColType = dataset.schema(getOutcomeCol).dataType
-      validateColTypeWithModel(outcomeColType, getOutcomeCol, getOutcomeModel)
+      validateColTypeWithModel(dataset, getTreatmentCol, getTreatmentModel)
+      validateColTypeWithModel(dataset, getOutcomeCol, getOutcomeModel)
 
       if (get(weightCol).isDefined) {
         getTreatmentModel match {
@@ -262,16 +261,28 @@ class DoubleMLEstimator(override val uid: String)
     DoubleMLEstimator.validateTransformSchema(schema)
   }
 
-  protected def validateColTypeWithModel(colType: DataType, colName: String, model: Estimator[_]): Unit = {
+  protected def validateColTypeWithModel(dataset: Dataset[_], colName: String, model: Estimator[_]): Unit = {
+    val colType = dataset.schema(colName).dataType
     val modelType = getDoubleMLModelType(model)
     colType match {
+      case IntegerType =>
+        // If column is integer with value 0 or 1, it can be used with classification model
+        // If column has normal integer values, it can be used with regression model
+        // If user set to use classification model, verify if all values in (0, 1)
+        if (modelType == DoubleMLModelTypes.Binary){
+          val hasInvalidValues = dataset.filter(!col(colName).isin(0, 1)).count() > 0
+          if (hasInvalidValues)
+            throw new Exception(s"column '$colName' in dataset is integer data type and " +
+              "you set to use a classification model for it, " +
+              "its all values must be either 0 or 1, but it has other values.")
+        }
       case BooleanType =>
         if (modelType == DoubleMLModelTypes.Continuous)
           throw new Exception(s"column '$colName' in dataset is boolean data type, " +
-            s"but you set to use a regression model for it.")
-      case IntegerType | DoubleType | LongType  =>
+            "but you set to use a regression model for it.")
+      case DoubleType | LongType  =>
         if (modelType == DoubleMLModelTypes.Binary)
-          throw new Exception(s"column '$colName' in dataset is integer, double or long data type, " +
+          throw new Exception(s"column '$colName' in dataset is double or long data type, " +
             "but you set to use a classification model for it.")
       case _ =>
         throw new Exception(s"column '$colName' must be of type DoubleType, LongType, " +
