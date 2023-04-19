bc761c8bea88714dc778ed4b17f0eaf096feec76
Mark Niehaus <manieh@microsoft.com>
2023-04-19 14:21:26 -0700

    try 3.3.2 again with hadoop 3

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -3,6 +3,8 @@ if (!require("sparklyr")) {
   library("sparklyr")
 }
 
-spark_install(version = "3.2.4", hadoop_version = "2.7", logging="INFO", verbose=T)
+#spark_available_versions(show_hadoop=TRUE, show_minor=TRUE)
+
+spark_install(version = "3.3.2", hadoop_version = "3", logging="INFO", verbose=T)
 options("testthat.output_file" = "../../../../r-test-results.xml")
 devtools::test(reporter = JunitReporter$new())
