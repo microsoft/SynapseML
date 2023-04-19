c058bd83c20fcfc106431b82c1ce01b6b6c53bd1
Mark Niehaus <manieh@microsoft.com>
2023-04-19 14:48:09 -0700

    use sparklyr 1.8.1

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -3,8 +3,6 @@ if (!require("sparklyr")) {
   library("sparklyr")
 }
 
-#spark_available_versions(show_hadoop=TRUE, show_minor=TRUE)
-
 spark_install(version = "3.3.2", hadoop_version = "3", logging="INFO", verbose=T)
 options("testthat.output_file" = "../../../../r-test-results.xml")
 devtools::test(reporter = JunitReporter$new())
