dc8b997678120976227bf0cbfe70ae950c06ec95
sherylZhaoCode <xuazhao@microsoft.com>
2023-04-19 00:50:19 -0700

    added the langchain transformer test

--- cognitive/src/test/python/synapsemltest/cognitive/test_LangchainTransform.py
+++ cognitive/src/test/python/synapsemltest/cognitive/test_LangchainTransform.py
@@ -1,37 +1,82 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in project root for information.
 
-# Prepare training and test data.
 import unittest
-
-from synapse.ml.io.http import *
+from synapse.ml.cognitive.langchain.LangchainTransform import LangchainTransformer
 from synapsemltest.spark import *
-from pyspark.sql.functions import struct
-from pyspark.sql.types import *
 
 
-class SimpleHTTPTransformerSmokeTest(unittest.TestCase):
-    def test_simple(self):
-        df = spark.createDataFrame([("foo",) for x in range(20)], ["data"]).withColumn(
-            "inputs",
-            struct("data"),
+class LangchainTransformTest(unittest.TestCase):
+    def test_langchainTransform(self):
+        # construction of Chain
+        # It is a very simple chain, basically just
+        # expand the input column and then summarize to the output column
+        # output column should be very similar to input column,
+        # and should contain the words input column
+
+        from langchain.chains import LLMChain
+        from langchain.chains import SimpleSequentialChain
+        from langchain.prompts import PromptTemplate
+        from langchain.llms import AzureOpenAI
+
+        llm = AzureOpenAI(
+            deployment_name="text-davinci-003",
+            model_name="text-davinci-003",
+            temperature=0,
+            verbose=False,
         )
 
-        response_schema = (
-            StructType().add("status", StringType()).add("message", StringType())
+        expand_prompt = PromptTemplate(
+            input_variables=["technology"],
+            template="Write a paragraph explaining the following technology: {technology}",
         )
 
-        client = (
-            SimpleHTTPTransformer()
-            .setInputCol("inputs")
-            .setInputParser(JSONInputParser())
-            .setOutputParser(JSONOutputParser().setDataType(response_schema))
-            .setOutputCol("results")
-            .setUrl("https://dog.ceo/api/breeds/image/random")
+        expand_chain = LLMChain(llm=llm, prompt=expand_prompt)
+
+        summarize_prompt = PromptTemplate(
+            input_variables=["paragraph"],
+            template="Name the technology explained in the following paragraph: {paragraph}",
         )
 
-        responses = client.transform(df)
-        responses.select("results").show(truncate=False)
+        summarize_chain = LLMChain(llm=llm, prompt=summarize_prompt)
+
+        chain = SimpleSequentialChain(
+            chains=[expand_chain, summarize_chain], verbose=True
+        )
+
+        # construction of test dataframe
+        sentenceDataFrame = spark.createDataFrame(
+            [(0, "docker"), (0, "spark"), (1, "python")], ["label", "technology"]
+        )
+
+        # fetching openai_api_key
+        import subprocess
+        import json
+
+        secretJson = subprocess.check_output(
+            "az keyvault secret show --vault-name mmlspark-build-keys --name openai-api-key",
+            shell=True,
+        )
+        openai_api_key = json.loads(secretJson)["value"]
+
+        # construct langchain transformer using the chain defined above. And test if the generated
+        # column has the expected result.
+        langchainTransformer = (
+            LangchainTransformer()
+            .setInputCol("technology")
+            .setOutputCol("copied_technology")
+            .setChain(chain)
+            .set_api_key(openai_api_key)
+        )
+
+        transformed_df = langchainTransformer.transform(sentenceDataFrame)
+        input_col_values = [row.technology for row in transformed_df.collect()]
+        output_col_values = [row.copied_technology for row in transformed_df.collect()]
+
+        for i in range(len(input_col_values)):
+            assert (
+                input_col_values[i] in output_col_values[i].lower()
+            ), f"output column value {output_col_values[i]} doesn't contain input column value {input_col_values[i]}"
 
 
 if __name__ == "__main__":
