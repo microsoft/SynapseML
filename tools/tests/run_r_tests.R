a39307e8fec81f9e0751830e80e62f34f7fc2f2e
Mark Niehaus <manieh@microsoft.com>
2023-04-19 08:57:44 -0700

    try 3.2.4

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -3,6 +3,6 @@ if (!require("sparklyr")) {
   library("sparklyr")
 }
 
-spark_install(version = "3.3.2", hadoop_version = "3")
+spark_install(version = "3.2.4", hadoop_version = "2.7", logging="INFO", verbose=T)
 options("testthat.output_file" = "../../../../r-test-results.xml")
 devtools::test(reporter = JunitReporter$new())
