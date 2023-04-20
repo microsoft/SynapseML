ef6b626cae2c799f88f812ffc960e084ab071f8f
sherylZhaoCode <xuazhao@microsoft.com>
2023-04-20 09:48:56 -0700

    adding environmental variables back

--- cognitive/src/test/python/synapsemltest/cognitive/test_LangchainTransform.py
+++ cognitive/src/test/python/synapsemltest/cognitive/test_LangchainTransform.py
@@ -1,23 +1,38 @@
 # Copyright (C) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in project root for information.
 
-import unittest
+import os, json, subprocess, unittest
+from langchain.chains import LLMChain
+from langchain.chains import SimpleSequentialChain
+from langchain.prompts import PromptTemplate
+from langchain.llms import AzureOpenAI
 from synapse.ml.cognitive.langchain.LangchainTransform import LangchainTransformer
 from synapsemltest.spark import *
 
 
 class LangchainTransformTest(unittest.TestCase):
     def test_langchainTransform(self):
+        # fetching openai_api_key
+
+        secretJson = subprocess.check_output(
+            "az keyvault secret show --vault-name mmlspark-build-keys --name openai-api-key",
+            shell=True,
+        )
+        openai_api_key = json.loads(secretJson)["value"]
+        openai_api_base = "https://synapseml-openai.openai.azure.com/"
+        openai_api_version = "2022-12-01"
+        openai_api_type = "azure"
+
         # construction of Chain
         # It is a very simple chain, basically just
         # expand the input column and then summarize to the output column
         # output column should be very similar to input column,
         # and should contain the words input column
 
-        from langchain.chains import LLMChain
-        from langchain.chains import SimpleSequentialChain
-        from langchain.prompts import PromptTemplate
-        from langchain.llms import AzureOpenAI
+        os.environ["OPENAI_API_TYPE"] = openai_api_type
+        os.environ["OPENAI_API_VERSION"] = openai_api_version
+        os.environ["OPENAI_API_BASE"] = openai_api_base
+        os.environ["OPENAI_API_KEY"] = openai_api_key
 
         llm = AzureOpenAI(
             deployment_name="text-davinci-003",
@@ -49,16 +64,6 @@ class LangchainTransformTest(unittest.TestCase):
             [(0, "docker"), (0, "spark"), (1, "python")], ["label", "technology"]
         )
 
-        # fetching openai_api_key
-        import subprocess
-        import json
-
-        secretJson = subprocess.check_output(
-            "az keyvault secret show --vault-name mmlspark-build-keys --name openai-api-key",
-            shell=True,
-        )
-        openai_api_key = json.loads(secretJson)["value"]
-
         # construct langchain transformer using the chain defined above. And test if the generated
         # column has the expected result.
         langchainTransformer = (
@@ -67,7 +72,7 @@ class LangchainTransformTest(unittest.TestCase):
             .setOutputCol("copied_technology")
             .setChain(chain)
             .setApiKey(openai_api_key)
-            .setApiBase("https://synapseml-openai.openai.azure.com/")
+            .setApiBase(openai_api_base)
         )
 
         transformed_df = langchainTransformer.transform(sentenceDataFrame)
