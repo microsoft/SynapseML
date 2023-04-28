ec17d1b0971b62335e7bed2ba0197dd427d47fcd
sherylZhaoCode <xuazhao@microsoft.com>
2023-04-27 23:50:31 -0700

    added the save and load method to the transformer

--- cognitive/src/main/python/synapse/ml/cognitive/langchain/LangchainTransform.py
+++ cognitive/src/main/python/synapse/ml/cognitive/langchain/LangchainTransform.py
@@ -10,30 +10,95 @@ Then this transformer will perform the operation defined in the
 Langchain chain to transform the input Column and save it to the
 OutputColumn.
 Example Usage:
-    >>> langchainTransformer = LangchainTransformer()
+    >>> transformer = LangchainTransformer()
     ...                       .setInputCol("input_column_name")
     ...                       .setOutputCol("output_column_name")
     ...                       .setChain(pre_defined_chain)
-    ...                       .set_api_key(OPENAI_API_KEY)
-    >>> langchainTransformer.transform(sentenceDataFrame)
+    ...                       .setSubscriptionKey(OPENAI_API_KEY)
+    ...                       .setUrl(baseURL)
+    >>> transformer.transform(sentenceDataFrame)
 
+If the chain does not have memory, you can also save and load the
+Langchain Transformer.
+Example Usage:
+    >>> transformer.save(path)
+    >>> loaded_transformer = LangchainTransformer.load(path)
 """
 
+import json
+from langchain.chains.loading import load_chain_from_config
 from pyspark import keyword_only
 from pyspark.ml import Transformer
 from pyspark.ml.param.shared import (
     HasInputCol,
     HasOutputCol,
     Param,
-    Params,
-    TypeConverters,
 )
-from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.ml.util import (
+    DefaultParamsReadable,
+    DefaultParamsWritable,
+    DefaultParamsReader,
+    DefaultParamsWriter,
+)
 from pyspark.sql.functions import udf
+from typing import cast, Optional, TypeVar, Type
 
-# Default Values
-OPENAI_API_BASE = "https://scusopenai.openai.azure.com/"
 OPENAI_API_VERSION = "2022-12-01"
+RL = TypeVar("RL", bound="MLReadable")
+
+
+class LangchainTransformerParamsWriter(DefaultParamsWriter):
+    @staticmethod
+    def _chain_serializer(chain) -> Optional[str]:
+        if chain.memory is not None:
+            raise NotImplementedError(
+                "Memory saving is not currently supported in Langchain. "
+                "Therefore, it is not possible to save this LangchainTransformer object, "
+                "as its chain contains memory."
+            )
+        return json.dumps(chain.dict())
+
+    def saveImpl(self, path: str) -> None:
+        params = self.instance._paramMap
+        modifiedParamMap = {}
+        for p in params:
+            if "chain" in p.name:
+                # For parameter chain, we need to first
+                # serialize it, and then pass it to
+                # parameter map.
+                param_value = self._chain_serializer(params[p])
+            else:
+                param_value = params[p]
+            modifiedParamMap[p.name] = param_value
+        DefaultParamsWriter.saveMetadata(
+            self.instance, path, self.sc, paramMap=modifiedParamMap
+        )
+
+
+class LangchainTransformerParamsReader(DefaultParamsReader):
+    @staticmethod
+    def __get_class(clazz: str) -> Type[RL]:
+        """
+        Loads Python class from its name.
+        """
+        parts = clazz.split(".")
+        module = ".".join(parts[:-1])
+        m = __import__(module, fromlist=[parts[-1]])
+        return getattr(m, parts[-1])
+
+    def load(self, path: str) -> RL:
+        metadata = LangchainTransformerParamsReader.loadMetadata(path, self.sc)
+        py_type: Type[RL] = LangchainTransformerParamsReader.__get_class(
+            metadata["class"]
+        )
+        instance = py_type()
+        cast("Params", instance)._resetUid(metadata["uid"])
+        # deserialize the chain before setting Params
+        metadata["paramMap"]["chain"] = load_chain_from_config(
+            json.loads(metadata["paramMap"]["chain"])
+        )
+        LangchainTransformerParamsReader.getAndSetParams(instance, metadata)
+        return instance
 
 
 class LangchainTransformer(
@@ -45,22 +110,26 @@ class LangchainTransformer(
         inputCol=None,
         outputCol=None,
         chain=None,
-        api_key=None,
-        api_base=OPENAI_API_BASE,
-        api_version=OPENAI_API_VERSION,
+        subscriptionKey=None,
+        url=None,
+        apiVersion=OPENAI_API_VERSION,
     ):
         super(LangchainTransformer, self).__init__()
-        self.chain = Param(self, "chain", "Langchain chain")
-        self.api_key = Param(self, "api_key", "openai api key")
-        self.api_base = Param(self, "api_base", "openai api base")
-        self.api_version = Param(self, "api_version", "openai api version")
+        self.chain = Param(
+            self,
+            "chain",
+            "Langchain chain",
+        )
+        self.subscriptionKey = Param(self, "subscriptionKey", "openai api key")
+        self.url = Param(self, "url", "openai api base")
+        self.apiVersion = Param(self, "apiVersion", "openai api version")
         kwargs = self._input_kwargs
-        if api_key:
-            kwargs["api_key"] = api_key
-        if api_base:
-            kwargs["api_base"] = api_base
-        if api_version:
-            kwargs["api_version"] = api_version
+        if subscriptionKey:
+            kwargs["subscriptionKey"] = subscriptionKey
+        if url:
+            kwargs["url"] = url
+        if apiVersion:
+            kwargs["apiVersion"] = apiVersion
         self.setParams(**kwargs)
 
     @keyword_only
@@ -69,9 +138,9 @@ class LangchainTransformer(
         inputCol=None,
         outputCol=None,
         chain=None,
-        api_key=None,
-        api_base=OPENAI_API_BASE,
-        api_version=OPENAI_API_VERSION,
+        subscriptionKey=None,
+        url=None,
+        apiVersion=OPENAI_API_VERSION,
     ):
         kwargs = self._input_kwargs
         return self._set(**kwargs)
@@ -82,26 +151,26 @@ class LangchainTransformer(
     def getChain(self):
         return self.getOrDefault(self.chain)
 
-    def setApiKey(self, value: str):
+    def setSubscriptionKey(self, value: str):
         """
         set the openAI api key
         """
-        return self._set(api_key=value)
+        return self._set(subscriptionKey=value)
 
-    def getApiKey(self):
-        return self.getOrDefault(self.api_key)
+    def getSubscriptionKey(self):
+        return self.getOrDefault(self.subscriptionKey)
 
-    def setApiBase(self, value: str):
-        return self._set(api_base=value)
+    def setUrl(self, value: str):
+        return self._set(url=value)
 
-    def getApiBase(self):
-        return self.getOrDefault(self.api_base)
+    def getUrl(self):
+        return self.getOrDefault(self.url)
 
     def setApiVersion(self, value: str):
-        return self._set(api_version=value)
+        return self._set(apiVersion=value)
 
     def getApiVersion(self):
-        return self.getOrDefault(self.api_version)
+        return self.getOrDefault(self.apiVersion)
 
     def setInputCol(self, value: str):
         """
@@ -121,17 +190,27 @@ class LangchainTransformer(
         and save the transformed values to the output column.
         """
 
-        def f(x):
+        @udf
+        def udfFunction(x):
             import openai
 
             openai.api_type = "azure"
-            openai.api_key = self.getApiKey()
-            openai.api_base = self.getApiBase()
+            openai.api_key = self.getSubscriptionKey()
+            openai.api_base = self.getUrl()
             openai.api_version = self.getApiVersion()
             return self.getChain().run(x)
 
-        udf_function = udf(lambda x: f(x))
+        outCol = self.getOutputCol()
+        inCol = dataset[self.getInputCol()]
+        return dataset.withColumn(outCol, udfFunction(inCol))
 
-        out_col = self.getOutputCol()
-        in_col = dataset[self.getInputCol()]
-        return dataset.withColumn(out_col, udf_function(in_col))
+    def write(self) -> LangchainTransformerParamsWriter:
+        writer = LangchainTransformerParamsWriter(instance=self)
+        # set the should overwriter in writer as True
+        writer = writer.overwrite()
+        return writer
+
+    @classmethod
+    def read(cls) -> "LangchainTransformerParamsReader[RL]":
+        """Returns a LangchainTransformerParamsReader instance for this class."""
+        return LangchainTransformerParamsReader(cls)
--- cognitive/src/test/python/synapsemltest/cognitive/test_LangchainTransform.py
+++ cognitive/src/test/python/synapsemltest/cognitive/test_LangchainTransform.py
@@ -3,7 +3,6 @@
 
 import os, json, subprocess, unittest
 from langchain.chains import LLMChain
-from langchain.chains import SimpleSequentialChain
 from langchain.prompts import PromptTemplate
 from langchain.llms import AzureOpenAI
 from synapse.ml.cognitive.langchain.LangchainTransform import LangchainTransformer
@@ -11,9 +10,9 @@ from synapsemltest.spark import *
 
 
 class LangchainTransformTest(unittest.TestCase):
-    def test_langchainTransform(self):
+    def __init__(self, *args, **kwargs):
+        super(LangchainTransformTest, self).__init__(*args, **kwargs)
         # fetching openai_api_key
-
         secretJson = subprocess.check_output(
             "az keyvault secret show --vault-name mmlspark-build-keys --name openai-api-key",
             shell=True,
@@ -23,17 +22,15 @@ class LangchainTransformTest(unittest.TestCase):
         openai_api_version = "2022-12-01"
         openai_api_type = "azure"
 
-        # construction of Chain
-        # It is a very simple chain, basically just
-        # expand the input column and then summarize to the output column
-        # output column should be very similar to input column,
-        # and should contain the words input column
-
         os.environ["OPENAI_API_TYPE"] = openai_api_type
         os.environ["OPENAI_API_VERSION"] = openai_api_version
         os.environ["OPENAI_API_BASE"] = openai_api_base
         os.environ["OPENAI_API_KEY"] = openai_api_key
 
+        self.subscriptionKey = openai_api_key
+        self.url = openai_api_base
+
+        # construction of llm
         llm = AzureOpenAI(
             deployment_name="text-davinci-003",
             model_name="text-davinci-003",
@@ -41,41 +38,25 @@ class LangchainTransformTest(unittest.TestCase):
             verbose=False,
         )
 
-        expand_prompt = PromptTemplate(
+        # construction of Chain
+        # It is a very simple chain, basically just
+        # expand the input column and then summarize to the output column
+        # output column should be very similar to input column,
+        # and should contain the words input column
+        copy_prompt = PromptTemplate(
             input_variables=["technology"],
-            template="Write a paragraph explaining the following technology: {technology}",
+            template="Copy the following word: {technology}",
         )
 
-        expand_chain = LLMChain(llm=llm, prompt=expand_prompt)
-
-        summarize_prompt = PromptTemplate(
-            input_variables=["paragraph"],
-            template="Name the technology explained in the following paragraph: {paragraph}",
-        )
-
-        summarize_chain = LLMChain(llm=llm, prompt=summarize_prompt)
-
-        chain = SimpleSequentialChain(
-            chains=[expand_chain, summarize_chain], verbose=True
-        )
+        self.chain = LLMChain(llm=llm, prompt=copy_prompt)
 
         # construction of test dataframe
-        sentenceDataFrame = spark.createDataFrame(
+        self.sentenceDataFrame = spark.createDataFrame(
             [(0, "docker"), (0, "spark"), (1, "python")], ["label", "technology"]
         )
 
-        # construct langchain transformer using the chain defined above. And test if the generated
-        # column has the expected result.
-        langchainTransformer = (
-            LangchainTransformer()
-            .setInputCol("technology")
-            .setOutputCol("copied_technology")
-            .setChain(chain)
-            .setApiKey(openai_api_key)
-            .setApiBase(openai_api_base)
-        )
-
-        transformed_df = langchainTransformer.transform(sentenceDataFrame)
+    def _assert_chain_output(self, transformer):
+        transformed_df = transformer.transform(self.sentenceDataFrame)
         input_col_values = [row.technology for row in transformed_df.collect()]
         output_col_values = [row.copied_technology for row in transformed_df.collect()]
 
@@ -83,6 +64,29 @@ class LangchainTransformTest(unittest.TestCase):
             assert (
                 input_col_values[i] in output_col_values[i].lower()
             ), f"output column value {output_col_values[i]} doesn't contain input column value {input_col_values[i]}"
+
+    def test_langchainTransform(self):
+        # construct langchain transformer using the chain defined above. And test if the generated
+        # column has the expected result.
+
+        self.langchainTransformer = (
+            LangchainTransformer()
+            .setInputCol("technology")
+            .setOutputCol("copied_technology")
+            .setChain(self.chain)
+            .setSubscriptionKey(self.subscriptionKey)
+            .setUrl(self.url)
+        )
+
+        self._assert_chain_output(self.langchainTransformer)
+
+    def test_save_load(self):
+        temp_dir = "tmp"
+        os.mkdir(temp_dir)
+        path = os.path.join(temp_dir, "langchainTransformer")
+        self.langchainTransformer.save(path)
+        loaded_transformer = LangchainTransformer.load(path)
+        self._assert_chain_output(loaded_transformer)
 
 
 if __name__ == "__main__":
