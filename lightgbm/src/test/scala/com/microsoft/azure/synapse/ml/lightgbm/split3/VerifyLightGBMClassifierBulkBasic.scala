3274b076598751419d31832122b77bd8d410dac1
Scott Votaw <svotaw@gmail.com>
2023-06-06 12:19:49 -0700

    fix benchmark file

--- lightgbm/src/test/scala/com/microsoft/azure/synapse/ml/lightgbm/split3/VerifyLightGBMClassifierBulkBasic.scala
+++ lightgbm/src/test/scala/com/microsoft/azure/synapse/ml/lightgbm/split3/VerifyLightGBMClassifierBulkBasic.scala
@@ -4,10 +4,10 @@
 package com.microsoft.azure.synapse.ml.lightgbm.split3
 
 import com.microsoft.azure.synapse.ml.lightgbm._
-import com.microsoft.azure.synapse.ml.lightgbm.split4.VerifyLightGBMClassifierStream
+import com.microsoft.azure.synapse.ml.lightgbm.split1.VerifyLightGBMClassifierStreamBasic
 
 /** Tests to validate the functionality of LightGBM module. */
-class VerifyLightGBMClassifierBulkBasic extends VerifyLightGBMClassifierStream {
+class VerifyLightGBMClassifierBulkBasic extends VerifyLightGBMClassifierStreamBasic {
   override lazy val executionMode: String = LightGBMConstants.BulkDataTransferMode
   override def ignoreSerializationFuzzing: Boolean = true
   override def ignoreExperimentFuzzing: Boolean = true
