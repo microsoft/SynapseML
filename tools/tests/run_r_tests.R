1f495b4ad51fb9ead68e4cad88787b225bf93bdf
Mark Hamilton <mhamilton723@gmail.com>
2023-06-27 16:27:55 +0100

    chore: fix spark_install for R tests (#1994)
    * chore: figure out issue with sparklyr install
    
    * revert
    
    * make spark install from a tar
    
    * make spark install from a tar
    
    * make spark install from a tar
    
    * make spark install from a tar

--- tools/tests/run_r_tests.R
+++ tools/tests/run_r_tests.R
@@ -3,9 +3,7 @@ if (!require("sparklyr")) {
   library("sparklyr")
 }
 
-spark_install(version = "3.2.4", hadoop_version = "3.2")
-spark_install(version = "3.2.4", hadoop_version = "3.2")
-spark_install(version = "3.2.4", hadoop_version = "3.2")
+spark_install_tar(paste(getwd(), "/../../../../../../spark-3.2.4-bin-hadoop3.2.tgz", sep = ""))
 
 options("testthat.output_file" = "../../../../r-test-results.xml")
 devtools::test(reporter = JunitReporter$new())
