f7de78159594c009bd04ca853359836b64e0f927
Jessica Wang <jessiwang@microsoft.com>
2023-07-14 04:18:15 -0700

    add get prereq from notebook

--- docs/python/documentprojection/channels/fabric.py
+++ docs/python/documentprojection/channels/fabric.py
@@ -1,9 +1,9 @@
+import difflib
 import os
 import re
 import warnings
 from datetime import datetime
 
-import difflib
 import nbformat
 import requests
 from github import Github
@@ -33,22 +33,22 @@ class FabricPublisher(Publisher):
         return self.config
     
     def publish(self, document: Document) -> bool:
-        if not document.metadata['is_active']:
+        if not document.metadata["is_active"]:
             return False
         channel_config = self.get_config()
         # set is_testing in channel config and test_doc path in manifest file
         # to compare generated doc with a given .md file
-        if ('is_testing' in channel_config) and channel_config['is_testing']:
-            if 'repo_doc' in document.metadata:
-                file_path = document.metadata['repo_doc']
-                md_content = read_github_file(repo_owner, repo_name, file_path, token)
+        if ("is_testing" in channel_config) and channel_config["is_testing"]:
+            if "repo_doc" in document.metadata:
+                file_path = document.metadata["repo_doc"]
+                md_content = read_github_file(repo_owner, repo_name, file_path)
                 print(compare_doc(md_content, document.content))
         else:
-            output_dir = channel_config['output_dir']
+            output_dir = channel_config["output_dir"]
             make_dir(output_dir)
-            filename = document.metadata['filename']
+            filename = document.metadata["filename"]
             if not filename.endswith(".md"):
-                filename += '.md'
+                filename += ".md"
             with open(os.path.join(output_dir, filename), "w", encoding="utf-8") as f:
                 f.write(document.content)
         return True
@@ -58,19 +58,31 @@ class FabricChannel(Channel):
         self.formatter = FabricFormatter(config)
         self.publisher = FabricPublisher(config)
 
-class FabricFormatter(MarkdownFormatter):
+class FabricFormatter:
     def __init__(self, config: ChannelMetadata):
         self.config = config
 
-    def _to_markdown(notebook: Notebook, hide_tag="hide-synapse-internal") -> str:
+    def _add_header(markdown: str, header: str):
+        content = f"{header}\n{markdown}"
+        return content
+    
+    def to_markdown(self, notebook: Notebook) -> str:
         # add prerequisites from manifest file
-        if 'prerequisites_position' in notebook.metadata:
-            position = notebook.metadata['prerequisites_position']
+        if "prerequisites_position" in notebook.metadata:
+            position = notebook.metadata["prerequisites_position"]
         else:
-            position = 2
-        if 'prerequisites' in notebook.metadata:
-            new_cell = new_markdown_cell(notebook.metadata['prerequisites'])
+            position = 1
+        if "prerequisites" in notebook.metadata:
+            new_cell = new_markdown_cell(notebook.metadata["prerequisites"])
             notebook.data.cells.insert(position, new_cell)
+        if "prerequisites_notebooks" in notebook.metadata:
+            combined_prerequisites = self.get_prerequisites_from_notebooks(notebook.metadata["prerequisites_notebooks"])
+            new_cell = new_markdown_cell(source=combined_prerequisites)
+            notebook.data.cells.insert(position, new_cell)
+        if "hide_tag" in notebook.metadata:
+            hide_tag = notebook.metadata["hide_tag"]
+        else:
+            hide_tag = "hide-synapse-internal"
         c = Config()
         c.TagRemovePreprocessor.remove_cell_tags = (hide_tag,)
         c.TagRemovePreprocessor.enabled = True
@@ -83,7 +95,7 @@ class FabricFormatter(MarkdownFormatter):
         return self.config
     
     def get_header(self, notebook: Notebook) -> str:
-        fabric_metadata = self.get_metadata(notebook)['metadata']
+        fabric_metadata = self.get_metadata(notebook)["metadata"]
         self.check_required_metadata(fabric_metadata)
         generated_metadata = self.generate_metadata(fabric_metadata)
         return generated_metadata
@@ -95,7 +107,7 @@ class FabricFormatter(MarkdownFormatter):
         return notebook.metadata
     
     def clean_markdown(self, markdown: str, metadata) -> str:
-        filename = metadata['filename']
+        filename = metadata["filename"]
         try:
             alt_texts = metadata["alt_texts"]
         except KeyError:
@@ -112,17 +124,17 @@ class FabricFormatter(MarkdownFormatter):
             next_steps = metadata["next_steps"]
         except KeyError:
             next_steps = None
-        media_dir = self.get_config()['media_dir']
-        output_dir = self.get_config()['output_dir']
+        media_dir = self.get_config()["media_dir"]
+        output_dir = self.get_config()["output_dir"]
         markdown = process_img(markdown, filename, output_dir, media_dir, alt_texts)
         markdown = self.combine_documentation(markdown, footer, manifest_mapping, next_steps)
         return markdown
     
     def format(self, notebook: Notebook) -> Document:
         metadata = self.get_metadata(notebook)
-        if ('is_active' in metadata) and (not metadata['is_active']):
+        if ("is_active" in metadata) and (not metadata["is_active"]):
             return Document("", self.get_metadata(notebook))
-        markdown = FabricFormatter._to_markdown(notebook)
+        markdown = self.to_markdown(notebook)
         markdown = self.clean_markdown(markdown, metadata)
         markdown = FabricFormatter._add_header(markdown, self.get_header(notebook))
         return Document(markdown, self.get_metadata(notebook))
@@ -184,6 +196,20 @@ class FabricFormatter(MarkdownFormatter):
         generated_doc = "".join([body, end])
         generated_doc = remove_replace_content(generated_doc, manifest_mapping)
         return generated_doc
+    
+    def get_prerequisites_from_notebooks(self, prerequisites_notebooks):
+        cells_with_tag = []
+        for notebook_path in prerequisites_notebooks:
+            with open(notebook_path, "r", encoding="utf-8") as f:
+                notebook = nbformat.read(f, nbformat.NO_CONVERT)
+            tags = prerequisites_notebooks[notebook_path]
+            for cell in notebook.cells:
+                if "tags" in cell.metadata:
+                    cell_tags = cell.metadata["tags"]
+                    if any(tag in cell_tags for tag in tags):
+                        cells_with_tag.append(cell)
+        combined_content = "\n".join(cell.source for cell in cells_with_tag)
+        return "## Prerequisites\n" + combined_content
 
 
 def download_image(image_url, image_path):
@@ -251,13 +277,13 @@ def remove_replace_content(text, manifest_mapping):
 def compare_doc(md_content, generated):
     differ = difflib.Differ()
     diff = differ.compare(md_content.splitlines(), generated.splitlines())
-    diff_with_row_numbers = [(line[0], line[2:]) for line in diff if line.startswith('+') or line.startswith('-')]
+    diff_with_row_numbers = [(line[0], line[2:]) for line in diff if line.startswith("+") or line.startswith("-")]
     diff_with_row_numbers = [(line[0], line[1], index + 1) for index, line in enumerate(diff_with_row_numbers)]
-    return '\n'.join(f'{symbol} {line} (row {row_num})' for symbol, line, row_num in diff_with_row_numbers)
+    return "\n".join(f"{symbol} {line} (row {row_num})" for symbol, line, row_num in diff_with_row_numbers)
 
 def read_github_file(repo_owner, repo_name, file_path, token):
     g = Github(token)
-    repo = g.get_repo('{repo_owner}/{repo_name}'.format(repo_owner=repo_owner, repo_name=repo_name))
+    repo = g.get_repo("{repo_owner}/{repo_name}".format(repo_owner=repo_owner, repo_name=repo_name))
     file_contents = repo.get_contents(file_path)
-    md_content = file_contents.decoded_content.decode('utf-8')
+    md_content = file_contents.decoded_content.decode("utf-8")
     return md_content
