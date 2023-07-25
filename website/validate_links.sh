7165855c3b7b661e8d69b769cea4ea7689ebc3c9
Mark <mhamilton723@gmail.com>
2023-07-25 15:53:47 +0100

    fixes

--- website/validate_links.sh
+++ website/validate_links.sh
@@ -1,43 +1,8 @@
 #!/bin/bash
 
-# Function to check if the website is up and running
-check_website_status() {
-  max_retries=30
-  retries=0
-  while [ $retries -lt $max_retries ]; do
-   response=$(curl -s http://localhost:3000/SynapseML)
-    if [[ $response == *"Docusaurus"* ]]; then
-      echo "Website is up and running!"
-      break
-    else
-      echo "Waiting for the website to deploy... (Attempt $((retries+1)) of $max_retries)"
-      sleep 5 # Adjust the sleep duration as needed
-      retries=$((retries+1))
-    fi
-  done
-
-  if [ $retries -eq $max_retries ]; then
-    echo "Website deployment failed. Exiting."
-    exit 1
-  fi
-}
-
-# Function to crawl the website and check for dead links
-crawl_for_deadlinks() {
-  echo "Crawling the website to check for dead links..."
-  wget --spider -r http://localhost:3000/SynapseML 2>&1 | grep -B 2 '404 Not Found'
-}
-
-# Start the website
 echo "Starting the website..."
 yarn start &
-
-# Wait for the website to deploy
-check_website_status
-
-# Crawl the website for dead links
-crawl_for_deadlinks
-
-# Stop the server after the crawl is complete (you may need to adjust this command based on your specific setup)
-# For example, if you are using a different web server like Node.js' http-server, replace the command accordingly.
+sleep 60
+echo "Crawling the website to check for dead links..."
+wget --spider -r http://localhost:3000/SynapseML 2>&1 | grep -B 2 '404 Not Found'
 pkill -f "yarn start"
