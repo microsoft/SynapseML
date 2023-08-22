29c94c1f9627da87143410c076c0b4a2f6faaaa8
NORTHAMERICA\keerthiyanda <keerthiyanda@microsoft.com>
2023-08-22 13:31:04 -0700

    Remove scalactic exclusion

--- core/src/test/scala/com/microsoft/azure/synapse/ml/nbtest/SynapseUtilities.scala
+++ core/src/test/scala/com/microsoft/azure/synapse/ml/nbtest/SynapseUtilities.scala
@@ -184,7 +184,6 @@ object SynapseUtilities {
     val excludes: String = Seq(
       "org.scala-lang:scala-reflect",
       "org.apache.spark:spark-tags_2.12",
-      "org.scalactic:scalactic_2.12",
       "org.scalatest:scalatest_2.12",
       "org.slf4j:slf4j-api").mkString(",")
     val runName = abfssPath.split('/').last.replace(".py", "")
