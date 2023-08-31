dc1b33387735ed5c98a380041dba9a8f0efea3c0
NORTHAMERICA\keerthiyanda <keerthiyanda@microsoft.com>
2023-08-31 07:28:33 -0700

    Adding Reccomendations fix

--- core/src/test/scala/com/microsoft/azure/synapse/ml/recommendation/RankingAdapterSpec.scala
+++ core/src/test/scala/com/microsoft/azure/synapse/ml/recommendation/RankingAdapterSpec.scala
@@ -5,6 +5,8 @@ package com.microsoft.azure.synapse.ml.recommendation
 
 import com.microsoft.azure.synapse.ml.core.test.fuzzing.{EstimatorFuzzing, TestObject, TransformerFuzzing}
 import org.apache.spark.ml.util.MLReadable
+import org.apache.spark.sql.DataFrame
+import org.scalactic.Equality
 
 class RankingAdapterSpec extends RankingTestBase with EstimatorFuzzing[RankingAdapter] {
   override def testObjects(): Seq[TestObject[RankingAdapter]] = {
@@ -15,6 +17,19 @@ class RankingAdapterSpec extends RankingTestBase with EstimatorFuzzing[RankingAd
 
   override def modelReader: MLReadable[_] = RankingAdapterModel
 
+  override def assertDFEq(df1: DataFrame, df2: DataFrame)(implicit eq: Equality[DataFrame]): Unit = {
+    def prep(df: DataFrame) = {
+      // sort rows and round decimals before compare two dataframes
+      import org.apache.spark.sql.functions._
+      val roundListDecimals: Seq[Float] => Seq[Float] = _.map { value =>
+        BigDecimal(value.toDouble).setScale(6, BigDecimal.RoundingMode.HALF_UP).toFloat
+      }
+      val castListToIntUDF = udf(roundListDecimals)
+      val sortedDF = df.orderBy(col("prediction"))
+      val updatedDF: DataFrame = sortedDF.withColumn("label", castListToIntUDF(col("label")))
+      updatedDF
+    }
+    super.assertDFEq(prep(df1), prep(df2))(eq)}
 }
 
 class RankingAdapterModelSpec extends RankingTestBase with TransformerFuzzing[RankingAdapterModel] {
