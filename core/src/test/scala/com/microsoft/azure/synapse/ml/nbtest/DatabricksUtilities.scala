78c79c54e01dd57b48fda03c6bc8c1d927fce7ab
NORTHAMERICA\keerthiyanda <keerthiyanda@microsoft.com>
2023-08-30 19:14:24 -0700

    updating torchvision

--- core/src/test/scala/com/microsoft/azure/synapse/ml/nbtest/DatabricksUtilities.scala
+++ core/src/test/scala/com/microsoft/azure/synapse/ml/nbtest/DatabricksUtilities.scala
@@ -75,7 +75,7 @@ object DatabricksUtilities {
   val GPULibraries: String = List(
     Map("maven" -> Map("coordinates" -> PackageMavenCoordinate, "repo" -> PackageRepository)),
     Map("pypi" -> Map("package" -> "pytorch-lightning==1.5.0")),
-    Map("pypi" -> Map("package" -> "torchvision==0.12.0")),
+    Map("pypi" -> Map("package" -> "torchvision==0.14.1")),
     Map("pypi" -> Map("package" -> "transformers==4.32.1")),
     Map("pypi" -> Map("package" -> "petastorm==0.12.0")),
     Map("pypi" -> Map("package" -> "protobuf==3.20.3"))
