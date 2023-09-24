01a63014afd12ffaba8f626028b8200824fe7d51
Sailesh Baidya <34048254+saileshbaidya@users.noreply.github.com>
2023-09-23 22:09:07 -0700

    Update core/src/main/scala/com/microsoft/azure/synapse/ml/logging/Usage/FabricTokenServiceClient.scala
    Co-authored-by: Mark Hamilton <mhamilton723@gmail.com>

--- core/src/main/scala/com/microsoft/azure/synapse/ml/logging/Usage/FabricTokenServiceClient.scala
+++ core/src/main/scala/com/microsoft/azure/synapse/ml/logging/Usage/FabricTokenServiceClient.scala
@@ -51,7 +51,6 @@ class FabricTokenServiceClient {
     )
     val url = s"$synapseTokenServiceEndpoint/api/v1/proxy${targetUrl.getPath}/access?resource=$resource"
 
-    val response: JsValue = CommonUtils.requestGet(url, headers, "content")
-    response.toString().getBytes("UTF-8").toString
+    CommonUtils.requestGet(url, headers, "content").toString().getBytes("UTF-8").toString
   }
 }
