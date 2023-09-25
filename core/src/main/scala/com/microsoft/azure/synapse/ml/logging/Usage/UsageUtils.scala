14cf6647d7aaa54074d95614c5ffb9b23a8ed4d9
Sailesh Baidya <saibai@microsoft.com>
2023-09-25 15:42:00 -0700

    Removing some class and associated test. Turning some variable lazy.

--- core/src/main/scala/com/microsoft/azure/synapse/ml/logging/Usage/UsageUtils.scala
+++ core/src/main/scala/com/microsoft/azure/synapse/ml/logging/Usage/UsageUtils.scala
@@ -7,7 +7,6 @@ import com.microsoft.azure.synapse.ml.logging.common.WebUtils
 import com.microsoft.azure.synapse.ml.logging.SynapseMLLogging
 import com.microsoft.azure.synapse.ml.logging.Usage.HostEndpointUtils._
 import com.microsoft.azure.synapse.ml.logging.Usage.TokenUtils.getAccessToken
-import com.microsoft.azure.synapse.ml.logging.common.SparkHadoopUtils
 
 import java.time.Instant
 import java.util.UUID
@@ -16,13 +15,13 @@ import spray.json._
 import spray.json.DefaultJsonProtocol._
 import spray.json.DefaultJsonProtocol.StringJsonFormat
 
-object UsageTelemetry extends FabricConstants with SparkHadoopUtils with WebUtils {
+object UsageTelemetry extends FabricConstants with WebUtils {
   private val SC = SparkSession.builder().getOrCreate().sparkContext
-  private val CapacityId = getHadoopConfig("trident.capacity.id", Some(SC))
-  private val WorkspaceId: String = getHadoopConfig("trident.artifact.workspace.id", Some(SC))
-  private val PbiEnv = SC.getConf.get("spark.trident.pbienv", "").toLowerCase()
+  private lazy val CapacityId = SC.hadoopConfiguration.get("trident.capacity.id")
+  private lazy val WorkspaceId: String = SC.hadoopConfiguration.get("trident.artifact.workspace.id")
+  private lazy val PbiEnv = SC.getConf.get("spark.trident.pbienv", "").toLowerCase()
 
-  private val SharedHost = getMlflowSharedHost(PbiEnv)
+  private lazy val SharedHost = getMlflowSharedHost(PbiEnv)
   private val SharedEndpoint = f"{SharedHost}/metadata/workspaces/{WorkspaceId}/artifacts"
   private val WlHost = getMlflowWorkloadHost(PbiEnv, CapacityId, WorkspaceId, Some(SharedHost))
 
