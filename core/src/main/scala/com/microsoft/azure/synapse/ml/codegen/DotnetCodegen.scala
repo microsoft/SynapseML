35eaf03145459dd277cf254fb0b374d24c014e1c
Mark Hamilton <marhamil@microsoft.com>
2023-10-26 14:10:37 -0400

    bump versions

--- core/src/main/scala/com/microsoft/azure/synapse/ml/codegen/DotnetCodegen.scala
+++ core/src/main/scala/com/microsoft/azure/synapse/ml/codegen/DotnetCodegen.scala
@@ -53,7 +53,7 @@ object DotnetCodegen {
          |
          |  <ItemGroup>
          |    <PackageReference Include="Microsoft.Spark" Version="2.1.1" />
-         |    <PackageReference Include="SynapseML.DotnetBase" Version="0.11.3" />
+         |    <PackageReference Include="SynapseML.DotnetBase" Version="0.11.4" />
          |    <PackageReference Include="IgnoresAccessChecksToGenerator" Version="0.4.0" PrivateAssets="All" />
          |    $newtonsoftDep
          |  </ItemGroup>
