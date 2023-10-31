39b14d270ea68be096c9b7fc0271d0ad0099032a
Mark Hamilton <marhamil@microsoft.com>
2023-10-31 12:39:20 -0400

    fix import

--- cognitive/src/main/python/synapse/ml/cognitive/__init__.py
+++ cognitive/src/main/python/synapse/ml/cognitive/__init__.py
@@ -6,13 +6,17 @@ warnings.warn(
     "The cognitive namespace has been deprecated. Please change "
     "synapse.ml.cognitive to synapse.ml.services",
 )
-import synapse.ml.services
-from synapse.ml.services import *
 
-# Create a proxy module to handle attribute access
+# Custom module proxy
 class CognitiveModuleProxy(types.ModuleType):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Explicitly import synapse.ml.services to run its __init__.py logic
+        import synapse.ml.services
+
     def __getattr__(self, name):
         # Redirect attribute access to synapse.ml.services
+        import synapse.ml.services
         return getattr(synapse.ml.services, name)
 
 # Replace the synapse.ml.cognitive entry in sys.modules with the proxy module
