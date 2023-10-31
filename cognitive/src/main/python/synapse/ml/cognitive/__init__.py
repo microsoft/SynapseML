88242724ca9bba46763a71216b59302d754c3e8c
Mark Hamilton <marhamil@microsoft.com>
2023-10-31 18:44:41 -0400

    fixes

--- cognitive/src/main/python/synapse/ml/cognitive/__init__.py
+++ cognitive/src/main/python/synapse/ml/cognitive/__init__.py
@@ -2,12 +2,22 @@ import warnings
 import sys
 from synapse.ml.services import *
 
+# Define the __all__ attribute based on the modules and attributes in synapse.ml.services
+__all__ = [
+    name for name in dir(sys.modules["synapse.ml.services"]) if not name.startswith("_")
+]
+
+
 class CognitiveModuleRedirector:
     def __getattr__(self, name):
+        warnings.simplefilter(action="always", category=DeprecationWarning)
         # Raise a deprecation warning
-        warnings.simplefilter(action='always', category=DeprecationWarning)
-        warnings.warn(f"Importing from 'synapse.ml.cognitive' is deprecated. Use 'synapse.ml.services' instead.", DeprecationWarning)
+        warnings.warn(
+            f"Importing from 'synapse.ml.cognitive' is deprecated. Use 'synapse.ml.services' instead.",
+            DeprecationWarning,
+        )
         # Redirect the import to synapse.ml.services
-        return getattr(sys.modules['synapse.ml.services'], name)
+        return getattr(sys.modules["synapse.ml.services"], name)
+
 
 sys.modules[__name__] = CognitiveModuleRedirector()
