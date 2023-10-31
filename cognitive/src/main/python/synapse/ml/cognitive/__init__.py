9641e6255ca4a1d962196ec64dededd698b887d5
Mark Hamilton <marhamil@microsoft.com>
2023-10-31 18:52:29 -0400

    fix style

--- cognitive/src/main/python/synapse/ml/cognitive/__init__.py
+++ cognitive/src/main/python/synapse/ml/cognitive/__init__.py
@@ -5,7 +5,11 @@ from synapse.ml.services import *
 warnings.simplefilter(action="always", category=DeprecationWarning)
 
 # Copy attributes from synapse.ml.services to synapse.ml.cognitive
-for attr_name in dir(sys.modules['synapse.ml.services']):
-    if not attr_name.startswith('_'):
-        globals()[attr_name] = getattr(sys.modules['synapse.ml.services'], attr_name)
-        warnings.warn(f"Importing '{attr_name}' from 'synapse.ml.cognitive' is deprecated. Use 'synapse.ml.services' instead.", DeprecationWarning, stacklevel=2)
+for attr_name in dir(sys.modules["synapse.ml.services"]):
+    if not attr_name.startswith("_"):
+        globals()[attr_name] = getattr(sys.modules["synapse.ml.services"], attr_name)
+        warnings.warn(
+            f"Importing '{attr_name}' from 'synapse.ml.cognitive' is deprecated. Use 'synapse.ml.services' instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
