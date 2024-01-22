da1dafd3e5cf951144b66bd91b43854da24ca5bb
cruise <cruiseli@microsoft.com>
2024-01-22 18:16:21 +0800

    add unified logging for python

---
+++ core/src/test/python/synapsemltest/core/test_logging.py
@@ -0,0 +1,44 @@
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+
+# Prepare training and test data.
+import unittest
+
+import logging
+from synapse.ml.core.logging.SynapseMLLogger import SynapseMLLogger
+
+
+class SampleTransformer(SynapseMLLogger):
+    def __init__(self):
+        super().__init__(log_level=logging.DEBUG)
+        self.log_class('SampleTransformer')
+
+    @SynapseMLLogger.log_transform()
+    def transform(self, df):
+        return True
+
+    @SynapseMLLogger.log_fit()
+    def fit(self, df):
+        return True
+
+    @SynapseMLLogger.log_verb()
+    def test_throw(self):
+        raise Exception("test exception")
+
+
+class LoggingTest(unittest.TestCase):
+    def test_logging_smoke(self):
+        t = SampleTransformer()
+        data = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
+        columns = ["name", "age"]
+        df = sc.createDataFrame(data, columns)
+        t.transform(df)
+        t.fit(df)
+        try:
+            t.test_throw()
+        except Exception as e:
+            assert f'{e}' == "test exception"
+
+
+if __name__ == "__main__":
+    result = unittest.main()
