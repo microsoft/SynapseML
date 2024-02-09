2836cf3ebb06b827b7d04b64ddecaed15b542d08
Alexander Spiridonov <aspiridonov@yahoo.com>
2024-02-09 08:34:39 -0800

    feat: OpenAI embeddings with GPU based KNN (#2157)
    * OpenAI embeddings with GPU based KNN
    
    Added OpenAI embeddings with GPU based KNN using NVIDIA Rapids
    
    * Added databricks rapids ml init script
    
    Added init script to install repids ml using cuda 11.8
    
    * No outputs
    
    Removed outputs
    
    * Added testing code
    
    With GPU KNN notebook test code
    
    * Added GPU test code
    
    Added GPU test code to OpenAI with KNN notebook
    
    * Fixed extra bracket
    
    * Removed extra bracket
    
    * Update DatabricksUtilities.scala
    
    Corrected brackets
    
    * Update SynapseTests.scala
    
    Removed tab
    
    * Update SynapseTests.scala
    
    Removed whitespace on the end
    
    * Corrected Style errors
    
    * Corrected init script location
    
    * Removed invalid esc char
    
    Fixed style errors
    
    * Removed Fine-tune
    
    Suggested by Mark
    
    * removed init script text
    
    * Update core/src/test/scala/com/microsoft/azure/synapse/ml/nbtest/DatabricksUtilities.scala
    
    Added "Fine-tune" again
    
    Co-authored-by: Mark Hamilton <mhamilton723@gmail.com>
    
    * Update DatabricksUtilities.scala
    
    Added Fine-tune back
    
    * Reverse style changes
    
    * Corrected style
    
    * Added GPUInitScript to createClusterInPool
    
    Create cluster using init script
    
    * Corrected to have a separate Rapids test
    
    * Update DatabricksRapidsTests.scala
    
    Corrected parameters
    
    * Update DatabricksUtilities.scala
    
    Added Rapids cluster name
    
    * Update DatabricksRapidsTests.scala
    
    Reduced number of nodes to 1
    
    * Update DatabricksRapidsTests.scala
    
    Fixed imports
    
    ---------
    
    Co-authored-by: Alexander Spiridonov <b-aleksandrs@microsoft.com>
    Co-authored-by: Mark Hamilton <mhamilton723@gmail.com>
    Co-authored-by: Alexander <157773158+bvonodiripsa@users.noreply.github.com>

---
+++ core/src/test/scala/com/microsoft/azure/synapse/ml/nbtest/DatabricksRapidsTests.scala
@@ -0,0 +1,26 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in project root for information.
+
+package com.microsoft.azure.synapse.ml.nbtest
+
+import com.microsoft.azure.synapse.ml.nbtest.DatabricksUtilities._
+
+import com.microsoft.azure.synapse.ml.build.BuildInfo
+import com.microsoft.azure.synapse.ml.core.env.FileUtilities
+import com.microsoft.azure.synapse.ml.nbtest.DatabricksUtilities._
+
+import java.io.File
+import scala.collection.mutable.ListBuffer
+
+class DatabricksRapidsTests extends DatabricksTestHelper {
+
+  val clusterId: String = createClusterInPool(GPUClusterName, AdbGpuRuntime, 1, GpuPoolId, RapidsInitScripts)
+  val jobIdsToCancel: ListBuffer[Long] = databricksTestHelper(
+    clusterId, GPULibraries, RapidsNotebooks)
+
+  protected override def afterAll(): Unit = {
+    afterAllHelper(jobIdsToCancel, clusterId, RapidsClusterName)
+    super.afterAll()
+  }
+
+}
