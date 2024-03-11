d33485781a81f5b2f3f28d2b75e4cdee119e512d
JessicaXYWang <108437381+JessicaXYWang@users.noreply.github.com>
2024-03-10 21:33:01 -0700

    add doc link in error message

--- core/src/main/scala/com/microsoft/azure/synapse/ml/fabric/OpenAIFabricSetting.scala
+++ core/src/main/scala/com/microsoft/azure/synapse/ml/fabric/OpenAIFabricSetting.scala
@@ -40,7 +40,9 @@ trait OpenAIFabricSetting extends RESTUtils {
         s"Or you can set your Azure OpenAI credentials.")
       case "DisallowedForCrossGeo" => throw new RuntimeException(s"Default OpenAI model ${modelName} is Disallowed " +
         s"for Cross Geo, please contact your admin if you want to use default fabric LLM model. " +
-        s"Or you can set your Azure OpenAI credentials.")
+        s"Or you can set your Azure OpenAI credentials." +
+        s"Refer to https://learn.microsoft.com/en-us/fabric/data-science/ai-services/ai-services-overview " +
+        s"for more detials")
       case "ModelNotFound" => throw new RuntimeException(s"Default OpenAI model ${modelName} not found, " +
         s"please check your deployment name. " +
         s"Refer to https://learn.microsoft.com/en-us/fabric/data-science/ai-services/ai-services-overview " +
