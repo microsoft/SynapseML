5c3734280dd364743ab47f61fcb54914e35951b5
JessicaXYWang <108437381+JessicaXYWang@users.noreply.github.com>
2024-05-20 11:21:43 -0700

    chore: update resource location (#2225)
    * update resource location
    
    * fix typo
    
    * update openai resources
    
    * fix bug
    
    * update nasaearth data
    
    * update openai resource name in notebooks
    
    * clean up

--- cognitive/src/test/scala/com/microsoft/azure/synapse/ml/services/openai/OpenAICompletionSuite.scala
+++ cognitive/src/test/scala/com/microsoft/azure/synapse/ml/services/openai/OpenAICompletionSuite.scala
@@ -12,12 +12,10 @@ import org.apache.spark.sql.{DataFrame, Row}
 import org.scalactic.Equality
 
 trait OpenAIAPIKey {
-  lazy val openAIAPIKey: String = sys.env.getOrElse("OPENAI_API_KEY", Secrets.OpenAIApiKey)
-  lazy val openAIServiceName: String = sys.env.getOrElse("OPENAI_SERVICE_NAME", "synapseml-openai")
+  lazy val openAIAPIKey: String = sys.env.getOrElse("OPENAI_API_KEY_2", Secrets.OpenAIApiKey)
+  lazy val openAIServiceName: String = sys.env.getOrElse("OPENAI_SERVICE_NAME_2", "synapseml-openai-2")
   lazy val deploymentName: String = "gpt-35-turbo"
   lazy val modelName: String = "gpt-35-turbo"
-  lazy val openAIAPIKeyGpt4: String = sys.env.getOrElse("OPENAI_API_KEY_2", Secrets.OpenAIApiKeyGpt4)
-  lazy val openAIServiceNameGpt4: String = sys.env.getOrElse("OPENAI_SERVICE_NAME_2", "synapseml-openai-2")
   lazy val deploymentNameGpt4: String = "gpt-4"
   lazy val modelNameGpt4: String = "gpt-4"
 }
