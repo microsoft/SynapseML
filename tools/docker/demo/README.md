00ba7850c36f7bd7f159b4e7e4761153fecf0a13
Mark Hamilton <mhamilton723@gmail.com>
2024-10-10 19:23:46 -0400

    chore: bump synapseml to 1.0.6  (#2295)
    * chore: bump to version 1.0.6
    
    * bump website

--- tools/docker/demo/README.md
+++ tools/docker/demo/README.md
@@ -15,9 +15,9 @@ docker build . --build-arg SYNAPSEML_VERSION=<YOUR-VERSION-HERE> -f tools/docker
 
 eg.
 
-For building image with SynapseML version 1.0.5, run:
+For building image with SynapseML version 1.0.6, run:
 ```
-docker build . --build-arg SYNAPSEML_VERSION=1.0.5 -f tools/docker/demo/Dockerfile -t synapseml:1.0.5
+docker build . --build-arg SYNAPSEML_VERSION=1.0.6 -f tools/docker/demo/Dockerfile -t synapseml:1.0.6
 ```
 
 # Run the image
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,7 +1,7 @@
 const math = require('remark-math')
 const katex = require('rehype-katex')
 const path = require('path');
-let version = "1.0.5";
+let version = "1.0.6";
 
 module.exports = {
     title: 'SynapseML',
@@ -13,7 +13,7 @@ module.exports = {
     projectName: 'SynapseML',
     trailingSlash: true,
     customFields: {
-        version: "1.0.5",
+        version: "1.0.6",
     },
     stylesheets: [
         {
@@ -88,11 +88,11 @@ module.exports = {
                         },
                         {
                             label: 'Python API Reference',
-                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.5/pyspark/index.html',
+                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.6/pyspark/index.html',
                         },
                         {
                             label: 'Scala API Reference',
-                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.5/scala/index.html',
+                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.6/scala/index.html',
                         },
                     ],
                 },
