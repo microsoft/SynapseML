72aa18e414aed5ceafdacc26ffafe76cd22defa9
JessicaXYWang <108437381+JessicaXYWang@users.noreply.github.com>
2025-03-04 15:39:39 -0800

    test

--- core/src/main/python/synapse/ml/llm/HuggingFaceCausallmTransform.py
+++ core/src/main/python/synapse/ml/llm/HuggingFaceCausallmTransform.py
@@ -95,230 +95,230 @@ class ComputableObject:
         if self.model_path:
             self.load_model()
  
-class HuggingFaceCausalLM(
-    Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable
-):
+# class HuggingFaceCausalLM(
+#     Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable
+# ):
 
-    modelName = Param(
-        Params._dummy(),
-        "modelName",
-        "model name",
-        typeConverter=TypeConverters.toString,
-    )
-    inputCol = Param(
-        Params._dummy(),
-        "inputCol",
-        "input column",
-        typeConverter=TypeConverters.toString,
-    )
-    outputCol = Param(
-        Params._dummy(),
-        "outputCol",
-        "output column",
-        typeConverter=TypeConverters.toString,
-    )
-    modelParam = Param(
-        Params._dummy(),
-        "modelParam",
-        "Model Parameters, passed to .generate(). For more details, check https://huggingface.co/docs/transformers/en/main_classes/text_generation#transformers.GenerationConfig",
-    )
-    modelConfig = Param(
-        Params._dummy(),
-        "modelConfig",
-        "Model configuration, passed to AutoModelForCausalLM.from_pretrained(). For more details, check https://huggingface.co/docs/transformers/en/model_doc/auto#transformers.AutoModelForCausalLM",
-    )
-    cachePath = Param(
-        Params._dummy(),
-        "cachePath",
-        "cache path for the model. could be a lakehouse path",
-        typeConverter=TypeConverters.toString,
-    )
-    deviceMap = Param(
-        Params._dummy(),
-        "deviceMap",
-        "Specifies a model parameter for the device Map. For GPU usage with models such as Phi 3, set it to 'cuda'.",
-        typeConverter=TypeConverters.toString,
-    )
-    torchDtype = Param(
-        Params._dummy(),
-        "torchDtype",
-        "Specifies a model parameter for the torch dtype. For GPU usage with models such as Phi 3, set it to 'auto'.",
-        typeConverter=TypeConverters.toString,
-    )
+#     modelName = Param(
+#         Params._dummy(),
+#         "modelName",
+#         "model name",
+#         typeConverter=TypeConverters.toString,
+#     )
+#     inputCol = Param(
+#         Params._dummy(),
+#         "inputCol",
+#         "input column",
+#         typeConverter=TypeConverters.toString,
+#     )
+#     outputCol = Param(
+#         Params._dummy(),
+#         "outputCol",
+#         "output column",
+#         typeConverter=TypeConverters.toString,
+#     )
+#     modelParam = Param(
+#         Params._dummy(),
+#         "modelParam",
+#         "Model Parameters, passed to .generate(). For more details, check https://huggingface.co/docs/transformers/en/main_classes/text_generation#transformers.GenerationConfig",
+#     )
+#     modelConfig = Param(
+#         Params._dummy(),
+#         "modelConfig",
+#         "Model configuration, passed to AutoModelForCausalLM.from_pretrained(). For more details, check https://huggingface.co/docs/transformers/en/model_doc/auto#transformers.AutoModelForCausalLM",
+#     )
+#     cachePath = Param(
+#         Params._dummy(),
+#         "cachePath",
+#         "cache path for the model. could be a lakehouse path",
+#         typeConverter=TypeConverters.toString,
+#     )
+#     deviceMap = Param(
+#         Params._dummy(),
+#         "deviceMap",
+#         "Specifies a model parameter for the device Map. For GPU usage with models such as Phi 3, set it to 'cuda'.",
+#         typeConverter=TypeConverters.toString,
+#     )
+#     torchDtype = Param(
+#         Params._dummy(),
+#         "torchDtype",
+#         "Specifies a model parameter for the torch dtype. For GPU usage with models such as Phi 3, set it to 'auto'.",
+#         typeConverter=TypeConverters.toString,
+#     )
 
-    @keyword_only
-    def __init__(
-        self,
-        modelName=None,
-        inputCol=None,
-        outputCol=None,
-        cachePath=None,
-        deviceMap=None,
-        torchDtype=None,
-    ):
-        super(HuggingFaceCausalLM, self).__init__()
-        self._setDefault(
-            modelName=modelName,
-            inputCol=inputCol,
-            outputCol=outputCol,
-            modelParam=_ModelParam(),
-            modelConfig=_ModelConfig(),
-            cachePath=None,
-            deviceMap=None,
-            torchDtype=None,
-        )
-        kwargs = self._input_kwargs
-        self.setParams(**kwargs)
+#     @keyword_only
+#     def __init__(
+#         self,
+#         modelName=None,
+#         inputCol=None,
+#         outputCol=None,
+#         cachePath=None,
+#         deviceMap=None,
+#         torchDtype=None,
+#     ):
+#         super(HuggingFaceCausalLM, self).__init__()
+#         self._setDefault(
+#             modelName=modelName,
+#             inputCol=inputCol,
+#             outputCol=outputCol,
+#             modelParam=_ModelParam(),
+#             modelConfig=_ModelConfig(),
+#             cachePath=None,
+#             deviceMap=None,
+#             torchDtype=None,
+#         )
+#         kwargs = self._input_kwargs
+#         self.setParams(**kwargs)
 
-        if self.getCachePath():
-            bc_computable = ComputableObject(self.getCachePath(), self.getModelConfig())
-            sc = SparkSession.builder.getOrCreate().sparkContext
-            self.bcObject = sc.broadcast(bc_computable)
-        else:
-            self.bcObject = None
+#         if self.getCachePath():
+#             bc_computable = ComputableObject(self.getCachePath(), self.getModelConfig())
+#             sc = SparkSession.builder.getOrCreate().sparkContext
+#             self.bcObject = sc.broadcast(bc_computable)
+#         else:
+#             self.bcObject = None
 
-    @keyword_only
-    def setParams(self):
-        kwargs = self._input_kwargs
-        return self._set(**kwargs)
+#     @keyword_only
+#     def setParams(self):
+#         kwargs = self._input_kwargs
+#         return self._set(**kwargs)
 
-    def setModelName(self, value):
-        return self._set(modelName=value)
+#     def setModelName(self, value):
+#         return self._set(modelName=value)
 
-    def getModelName(self):
-        return self.getOrDefault(self.modelName)
+#     def getModelName(self):
+#         return self.getOrDefault(self.modelName)
 
-    def setInputCol(self, value):
-        return self._set(inputCol=value)
+#     def setInputCol(self, value):
+#         return self._set(inputCol=value)
 
-    def getInputCol(self):
-        return self.getOrDefault(self.inputCol)
+#     def getInputCol(self):
+#         return self.getOrDefault(self.inputCol)
 
-    def setOutputCol(self, value):
-        return self._set(outputCol=value)
+#     def setOutputCol(self, value):
+#         return self._set(outputCol=value)
 
-    def getOutputCol(self):
-        return self.getOrDefault(self.outputCol)
+#     def getOutputCol(self):
+#         return self.getOrDefault(self.outputCol)
 
-    def setModelParam(self, **kwargs):
-        param = _ModelParam(**kwargs)
-        return self._set(modelParam=param)
+#     def setModelParam(self, **kwargs):
+#         param = _ModelParam(**kwargs)
+#         return self._set(modelParam=param)
 
-    def getModelParam(self):
-        return self.getOrDefault(self.modelParam)
+#     def getModelParam(self):
+#         return self.getOrDefault(self.modelParam)
 
-    def setModelConfig(self, **kwargs):
-        config = _ModelConfig(**kwargs)
-        return self._set(modelConfig=config)
+#     def setModelConfig(self, **kwargs):
+#         config = _ModelConfig(**kwargs)
+#         return self._set(modelConfig=config)
 
-    def getModelConfig(self):
-        return self.getOrDefault(self.modelConfig)
+#     def getModelConfig(self):
+#         return self.getOrDefault(self.modelConfig)
 
-    def setCachePath(self, value):
-        return self._set(cachePath=value)
+#     def setCachePath(self, value):
+#         return self._set(cachePath=value)
 
-    # def getCachePath(self):
-    #     return self.getOrDefault(self.cachePath)
-    def setCachePath(self, value, model_config):
-        ret = self._set(cachePath=value)
-        if value is not None:
-            bc_computable = ComputableObject(value, model_config)
-            sc = SparkSession.builder.getOrCreate().sparkContext
-            self.bcObject = sc.broadcast(bc_computable)
-        else:
-            self.bcObject = None
-        return ret
+#     # def getCachePath(self):
+#     #     return self.getOrDefault(self.cachePath)
+#     def setCachePath(self, value, model_config):
+#         ret = self._set(cachePath=value)
+#         if value is not None:
+#             bc_computable = ComputableObject(value, model_config)
+#             sc = SparkSession.builder.getOrCreate().sparkContext
+#             self.bcObject = sc.broadcast(bc_computable)
+#         else:
+#             self.bcObject = None
+#         return ret
 
-    def setDeviceMap(self, value):
-        return self._set(deviceMap=value)
+#     def setDeviceMap(self, value):
+#         return self._set(deviceMap=value)
 
-    def getDeviceMap(self):
-        return self.getOrDefault(self.deviceMap)
+#     def getDeviceMap(self):
+#         return self.getOrDefault(self.deviceMap)
 
-    def setTorchDtype(self, value):
-        return self._set(torchDtype=value)
+#     def setTorchDtype(self, value):
+#         return self._set(torchDtype=value)
 
-    def getTorchDtype(self):
-        return self.getOrDefault(self.torchDtype)
+#     def getTorchDtype(self):
+#         return self.getOrDefault(self.torchDtype)
 
-    def getBCObject(self):
-        return self.bcObject
+#     def getBCObject(self):
+#         return self.bcObject
 
-    def _predict_single_complete(self, prompt, model, tokenizer):
-        param = self.getModelParam().get_param()
-        inputs = tokenizer(prompt, return_tensors="pt").input_ids
-        outputs = model.generate(inputs, **param)
-        decoded_output = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-        return decoded_output
+#     def _predict_single_complete(self, prompt, model, tokenizer):
+#         param = self.getModelParam().get_param()
+#         inputs = tokenizer(prompt, return_tensors="pt").input_ids
+#         outputs = model.generate(inputs, **param)
+#         decoded_output = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+#         return decoded_output
 
-    def _predict_single_chat(self, prompt, model, tokenizer):
-        param = self.getModelParam().get_param()
-        if isinstance(prompt, list):
-            chat = prompt
-        else:
-            chat = [{"role": "user", "content": prompt}]
-        formatted_chat = tokenizer.apply_chat_template(
-            chat, tokenize=False, add_generation_prompt=True
-        )
-        tokenized_chat = tokenizer(
-            formatted_chat, return_tensors="pt", add_special_tokens=False
-        )
-        inputs = {
-            key: tensor.to(model.device) for key, tensor in tokenized_chat.items()
-        }
-        merged_inputs = {**inputs, **param}
-        outputs = model.generate(**merged_inputs)
-        decoded_output = tokenizer.decode(
-            outputs[0][inputs["input_ids"].size(1) :], skip_special_tokens=True
-        )
-        return decoded_output
+#     def _predict_single_chat(self, prompt, model, tokenizer):
+#         param = self.getModelParam().get_param()
+#         if isinstance(prompt, list):
+#             chat = prompt
+#         else:
+#             chat = [{"role": "user", "content": prompt}]
+#         formatted_chat = tokenizer.apply_chat_template(
+#             chat, tokenize=False, add_generation_prompt=True
+#         )
+#         tokenized_chat = tokenizer(
+#             formatted_chat, return_tensors="pt", add_special_tokens=False
+#         )
+#         inputs = {
+#             key: tensor.to(model.device) for key, tensor in tokenized_chat.items()
+#         }
+#         merged_inputs = {**inputs, **param}
+#         outputs = model.generate(**merged_inputs)
+#         decoded_output = tokenizer.decode(
+#             outputs[0][inputs["input_ids"].size(1) :], skip_special_tokens=True
+#         )
+#         return decoded_output
 
-    def _process_partition(self, iterator, task, bc_object):
-        """Process each partition of the data."""
-        peekable_iterator = _PeekableIterator(iterator)
-        try:
-            first_row = peekable_iterator.peek()
-        except StopIteration:
-            return None
+#     def _process_partition(self, iterator, task, bc_object):
+#         """Process each partition of the data."""
+#         peekable_iterator = _PeekableIterator(iterator)
+#         try:
+#             first_row = peekable_iterator.peek()
+#         except StopIteration:
+#             return None
         
-        if bc_object:
-            lc_object = bc_object.value
-            model = lc_object.model
-            tokenizer = lc_object.tokenizer
-        else:
-            model = AutoModelForCausalLM.from_pretrained(model_name, **model_config)
-            tokenizer = AutoTokenizer.from_pretrained(model_name)
+#         if bc_object:
+#             lc_object = bc_object.value
+#             model = lc_object.model
+#             tokenizer = lc_object.tokenizer
+#         else:
+#             model = AutoModelForCausalLM.from_pretrained(model_name, **model_config)
+#             tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-        for row in peekable_iterator:
-            prompt = row[self.getInputCol()]
-            if task == "chat":
-                result = self._predict_single_chat(prompt, model, tokenizer)
-            elif task == "complete":
-                result = self._predict_single_complete(prompt, model, tokenizer)
-            row_dict = row.asDict()
-            row_dict[self.getOutputCol()] = result
-            yield Row(**row_dict)
+#         for row in peekable_iterator:
+#             prompt = row[self.getInputCol()]
+#             if task == "chat":
+#                 result = self._predict_single_chat(prompt, model, tokenizer)
+#             elif task == "complete":
+#                 result = self._predict_single_complete(prompt, model, tokenizer)
+#             row_dict = row.asDict()
+#             row_dict[self.getOutputCol()] = result
+#             yield Row(**row_dict)
 
-    def _transform(self, dataset):
-        bc_object = self.getBCObject()
-        input_schema = dataset.schema
-        output_schema = StructType(
-            input_schema.fields + [StructField(self.getOutputCol(), StringType(), True)]
-        )
-        result_rdd = dataset.rdd.mapPartitions(
-            lambda partition: self._process_partition(partition, "chat", bc_object)
-        )
-        result_df = result_rdd.toDF(output_schema)
-        return result_df
+#     def _transform(self, dataset):
+#         bc_object = self.getBCObject()
+#         input_schema = dataset.schema
+#         output_schema = StructType(
+#             input_schema.fields + [StructField(self.getOutputCol(), StringType(), True)]
+#         )
+#         result_rdd = dataset.rdd.mapPartitions(
+#             lambda partition: self._process_partition(partition, "chat", bc_object)
+#         )
+#         result_df = result_rdd.toDF(output_schema)
+#         return result_df
 
-    def complete(self, dataset):
-        bc_object = self.getBCObject()
-        input_schema = dataset.schema
-        output_schema = StructType(
-            input_schema.fields + [StructField(self.getOutputCol(), StringType(), True)]
-        )
-        result_rdd = dataset.rdd.mapPartitions(
-            lambda partition: self._process_partition(partition, "complete", bc_object)
-        )
-        result_df = result_rdd.toDF(output_schema)
-        return result_df
+#     def complete(self, dataset):
+#         bc_object = self.getBCObject()
+#         input_schema = dataset.schema
+#         output_schema = StructType(
+#             input_schema.fields + [StructField(self.getOutputCol(), StringType(), True)]
+#         )
+#         result_rdd = dataset.rdd.mapPartitions(
+#             lambda partition: self._process_partition(partition, "complete", bc_object)
+#         )
+#         result_df = result_rdd.toDF(output_schema)
+#         return result_df
