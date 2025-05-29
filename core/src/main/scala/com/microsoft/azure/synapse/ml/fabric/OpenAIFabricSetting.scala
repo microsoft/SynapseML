941f3e8355c357fb8897eb670271694e05f366d4
Mark Hamilton <marhamil@microsoft.com>
2025-05-29 12:10:50 -0400

    chore: fix style

--- core/src/main/scala/com/microsoft/azure/synapse/ml/fabric/OpenAIFabricSetting.scala
+++ core/src/main/scala/com/microsoft/azure/synapse/ml/fabric/OpenAIFabricSetting.scala
@@ -25,39 +25,42 @@ trait OpenAIFabricSetting extends RESTUtils {
 
     val mlWorkloadEndpointML = FabricClient.MLWorkloadEndpointML
     val url = mlWorkloadEndpointML + "cognitive/openai/tenantsetting"
-    val modelStatus = try {
-      usagePost(url, payload).asJsObject.fields.get(modelName.toLowerCase).get
+    val modelStatusOpt: Option[JsValue] = try {
+      Some(usagePost(url, payload).asJsObject.fields.get(modelName.toLowerCase).get)
     } catch {
       case e: Throwable =>
         println(
           "Could not get model status, you are likely running in the system context of Fabric", e)
-        return ()
+        None
     }
 
-    // Allowed, Disallowed, DisallowedForCrossGeo, ModelNotFound, InvalidResult
-    val resultString: String = modelStatus match {
-      case JsString(value) => value
-      case _ => throw new RuntimeException("Unexpected result from type conversion " +
-        "when checking the fabric tenant settings API.")
+    modelStatusOpt.foreach { modelStatus =>
+      // Allowed, Disallowed, DisallowedForCrossGeo, ModelNotFound, InvalidResult
+      val resultString: String = modelStatus match {
+        case JsString(value) => value
+        case _ => throw new RuntimeException("Unexpected result from type conversion " +
+          "when checking the fabric tenant settings API.")
+      }
+
+      resultString match {
+        case "Disallowed" => throw new RuntimeException(s"Default OpenAI model ${modelName} is Disallowed, " +
+          s"please contact your admin if you want to use default fabric LLM model. " +
+          s"Or you can set your Azure OpenAI credentials.")
+        case "DisallowedForCrossGeo" => throw new RuntimeException(s"Default OpenAI model ${modelName} is Disallowed " +
+          s"for Cross Geo, please contact your admin if you want to use default fabric LLM model. " +
+          s"Or you can set your Azure OpenAI credentials." +
+          s"Refer to https://learn.microsoft.com/en-us/fabric/data-science/ai-services/ai-services-overview " +
+          s"for more detials")
+        case "ModelNotFound" => throw new RuntimeException(s"Default OpenAI model ${modelName} not found, " +
+          s"please check your deployment name. " +
+          s"Refer to https://learn.microsoft.com/en-us/fabric/data-science/ai-services/ai-services-overview " +
+          s"for the models available.")
+        case "InvalidResult" => throw new RuntimeException("Cannot get tenant admin setting status correctly")
+        case "Allowed" => ()
+        case _ => throw new RuntimeException("Unexpected result from checking the Fabric tenant settings API.")
+      }
     }
 
-    resultString match {
-      case "Disallowed" => throw new RuntimeException(s"Default OpenAI model ${modelName} is Disallowed, " +
-        s"please contact your admin if you want to use default fabric LLM model. " +
-        s"Or you can set your Azure OpenAI credentials.")
-      case "DisallowedForCrossGeo" => throw new RuntimeException(s"Default OpenAI model ${modelName} is Disallowed " +
-        s"for Cross Geo, please contact your admin if you want to use default fabric LLM model. " +
-        s"Or you can set your Azure OpenAI credentials." +
-        s"Refer to https://learn.microsoft.com/en-us/fabric/data-science/ai-services/ai-services-overview " +
-        s"for more detials")
-      case "ModelNotFound" => throw new RuntimeException(s"Default OpenAI model ${modelName} not found, " +
-        s"please check your deployment name. " +
-        s"Refer to https://learn.microsoft.com/en-us/fabric/data-science/ai-services/ai-services-overview " +
-        s"for the models available.")
-      case "InvalidResult" => throw new RuntimeException("Cannot get tenant admin setting status correctly")
-      case "Allowed" => ()
-      case _ => throw new RuntimeException("Unexpected result from checking the Fabric tenant settings API.")
-    }
   }
 
 }
