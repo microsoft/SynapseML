9c911488b74340d022f919ce14a946c8ac75b0b4
Mark Hamilton <mhamilton723@gmail.com>
2025-07-03 16:30:21 -0400

    chore: bump to v1.0.12 (#2397)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,7 +1,7 @@
 const math = require('remark-math')
 const katex = require('rehype-katex')
 const path = require('path');
-let version = "1.0.11";
+let version = "1.0.12";
 
 module.exports = {
     title: 'SynapseML',
@@ -13,7 +13,7 @@ module.exports = {
     projectName: 'SynapseML',
     trailingSlash: true,
     customFields: {
-        version: "1.0.11",
+        version: "1.0.12",
     },
     stylesheets: [
         {
@@ -88,11 +88,11 @@ module.exports = {
                         },
                         {
                             label: 'Python API Reference',
-                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.11/pyspark/index.html',
+                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.12/pyspark/index.html',
                         },
                         {
                             label: 'Scala API Reference',
-                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.11/scala/index.html',
+                            to: 'https://mmlspark.blob.core.windows.net/docs/1.0.12/scala/index.html',
                         },
                     ],
                 },
