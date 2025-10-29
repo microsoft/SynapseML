f474513c6c8964ea2a4aed08b80f163a386c7c69
Brendan Walsh <brwals@microsoft.com>
2025-10-29 00:02:14 -0700

    chore: bump version to v1.1.0

--- tools/docker/demo/README.md
+++ tools/docker/demo/README.md
@@ -15,9 +15,9 @@ docker build . --build-arg SYNAPSEML_VERSION=<YOUR-VERSION-HERE> -f tools/docker
 
 eg.
 
-For building image with SynapseML version 1.0.15, run:
+For building image with SynapseML version 1.1.0, run:
 ```
-docker build . --build-arg SYNAPSEML_VERSION=1.0.15 -f tools/docker/demo/Dockerfile -t synapseml:1.0.15
+docker build . --build-arg SYNAPSEML_VERSION=1.1.0 -f tools/docker/demo/Dockerfile -t synapseml:1.1.0
 ```
 
 # Run the image
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -262,13 +262,13 @@ function Home() {
               >
                 <TabItem value="Synapse">
                   <p>SynapseML can be installed on Synapse adding the following to the first cell of a notebook:</p>
-                  For Spark3.4 pools:
+                  For Spark3.5 pools:
                   <CodeSnippet
                     snippet={`%%configure -f
 {
   "name": "synapseml",
   "conf": {
-      "spark.jars.packages": "com.microsoft.azure:synapseml_2.12:1.0.15",
+      "spark.jars.packages": "com.microsoft.azure:synapseml_2.12:1.1.0",
       "spark.jars.repositories": "https://mmlspark.azureedge.net/maven",
       "spark.jars.excludes": "org.scala-lang:scala-reflect,org.apache.spark:spark-tags_2.12,org.scalactic:scalactic_2.12,org.scalatest:scalatest_2.12,com.fasterxml.jackson.core:jackson-databind",
       "spark.yarn.user.classpath.first": "true",
@@ -277,13 +277,13 @@ function Home() {
 }`}
                     lang="bash"
                   ></CodeSnippet>
-                  For Spark3.3 pools:
+                  For Spark3.4 pools:
                   <CodeSnippet
                     snippet={`%%configure -f
 {
   "name": "synapseml",
   "conf": {
-      "spark.jars.packages": "com.microsoft.azure:synapseml_2.12:0.11.4-spark3.3",
+      "spark.jars.packages": "com.microsoft.azure:synapseml_2.12:1.0.15",
       "spark.jars.repositories": "https://mmlspark.azureedge.net/maven",
       "spark.jars.excludes": "org.scala-lang:scala-reflect,org.apache.spark:spark-tags_2.12,org.scalactic:scalactic_2.12,org.scalatest:scalatest_2.12,com.fasterxml.jackson.core:jackson-databind",
       "spark.yarn.user.classpath.first": "true",
@@ -314,9 +314,9 @@ function Home() {
                   SynapseML can be conveniently installed on existing Spark
                   clusters via the --packages option, examples:
                   <CodeSnippet
-                    snippet={`spark-shell --packages com.microsoft.azure:synapseml_2.12:1.0.15 # Please use 1.0.15 version for Spark3.4 and 0.11.4-spark3.3 version for Spark3.3
-pyspark --packages com.microsoft.azure:synapseml_2.12:1.0.15
-spark-submit --packages com.microsoft.azure:synapseml_2.12:1.0.15 MyApp.jar `}
+                    snippet={`spark-shell --packages com.microsoft.azure:synapseml_2.12:1.1.0 # Please use 1.1.0 version for Spark3.5 and 1.1.0 version for Spark3.4
+pyspark --packages com.microsoft.azure:synapseml_2.12:1.1.0
+spark-submit --packages com.microsoft.azure:synapseml_2.12:1.1.0 MyApp.jar `}
                     lang="bash"
                   ></CodeSnippet>
                   This can be used in other Spark contexts too. For example, you
@@ -342,14 +342,14 @@ spark-submit --packages com.microsoft.azure:synapseml_2.12:1.0.15 MyApp.jar `}
                   </p>
                   <p>
                     <p>For the coordinates:</p>
+                    Spark 3.5 Cluster:
+                    <CodeSnippet
+                      snippet={`com.microsoft.azure:synapseml_2.12:1.1.0`}
+                      lang="bash"
+                    ></CodeSnippet>
                     Spark 3.4 Cluster:
                     <CodeSnippet
                       snippet={`com.microsoft.azure:synapseml_2.12:1.0.15`}
-                      lang="bash"
-                    ></CodeSnippet>
-                    Spark 3.3 Cluster:
-                    <CodeSnippet
-                      snippet={`com.microsoft.azure:synapseml_2.12:0.11.4-spark3.3`}
                       lang="bash"
                     ></CodeSnippet>
                     with the resolver:
@@ -367,7 +367,7 @@ spark-submit --packages com.microsoft.azure:synapseml_2.12:1.0.15 MyApp.jar `}
                   notebooks. To get started with our example notebooks import
                   the following databricks archive:
                   <CodeSnippet
-                    snippet={`https://mmlspark.blob.core.windows.net/dbcs/SynapseMLExamplesv1.0.15.dbc`}
+                    snippet={`https://mmlspark.blob.core.windows.net/dbcs/SynapseMLExamplesv1.1.0.dbc`}
                     lang="bash"
                   ></CodeSnippet>
                 </TabItem>
@@ -405,7 +405,7 @@ spark-submit --packages com.microsoft.azure:synapseml_2.12:1.0.15 MyApp.jar `}
                   <CodeSnippet
                     snippet={`import pyspark
 spark = (pyspark.sql.SparkSession.builder.appName("MyApp")
-        .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:1.0.15") # Please use 1.0.15 version for Spark3.4 and 0.11.4-spark3.3 version for Spark3.3
+        .config("spark.jars.packages", "com.microsoft.azure:synapseml_2.12:1.1.0") # Please use 1.1.0 version for Spark3.5 and 1.1.0 version for Spark3.4
         .config("spark.jars.repositories", "https://mmlspark.azureedge.net/maven")
         .getOrCreate())
 import synapse.ml`}
@@ -417,7 +417,7 @@ import synapse.ml`}
                   following lines to your build.sbt:
                   <CodeSnippet
                     snippet={`resolvers += "SynapseML" at "https://mmlspark.azureedge.net/maven"
-libraryDependencies += "com.microsoft.azure" %% "synapseml_2.12" % "1.0.15" // Please use 1.0.15 version for Spark3.2 and 1.0.15-spark3.3 version for Spark3.3`}
+libraryDependencies += "com.microsoft.azure" %% "synapseml_2.12" % "1.1.0" // Please use 1.1.0 version for Spark3.2 and 1.0.15-spark3.3 version for Spark3.3`}
                     lang="jsx"
                   ></CodeSnippet>
                 </TabItem>
